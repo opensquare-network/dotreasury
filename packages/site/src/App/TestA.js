8e0cc4ae8c30fa572f7250a54c9a78dc932e6448
qiyisi <qiyisi@qq.com>
2020-12-21 14:22:50 +0800

    init router

---
+++ packages/site/src/App/TestA.js
@@ -0,0 +1,3 @@
+export default function TestA() {
+  return <>TestA</>;
+}
---
+++ packages/site/src/pages/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+// import styled from "styled-components"
+
+const HeaderExamplePage = () => (
+  <div>
+    <Header>HEADER</Header>
+  </div>
+);
+
+export default HeaderExamplePage;
