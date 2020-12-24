997fda3145e50bd6ff60ef983badfe45bc47cbc5
Yongfeng LI <wliyongfeng@gmail.com>
2020-12-24 18:37:28 +0800

    Use polkadot identicon as the user icon

--- packages/site/src/components/User/Index.js
+++ packages/site/src/components/User/Index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { Image } from "semantic-ui-react";
 
 import Username from "./Username";
+import Avatar from "./Avatar";
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,17 +11,10 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
-const UserImage = styled(Image)`
-  border-radius: 50%;
-  border: 1px solid #e1e1e1;
-  width: 24px;
-  height: 24px;
-`;
-
 const User = ({ name, address, src }) => {
   return (
     <Wrapper>
-      <UserImage src={src ? src : "./imgs/avatar.png"} />
+      <Avatar address="EGVQCe73TpFyAZx5uKfE1222XfkT3BSKozjgcqzLBnc5eYo" />
       <Username name={name} address={address} />
     </Wrapper>
   );
