95fca2f74183b417eb1a6ed5bec8880aa3e61313
Yongfeng LI <wliyongfeng@gmail.com>
2020-12-25 23:40:17 +0800

    Fix tips table token name

--- packages/site/src/components/Balance.js
+++ packages/site/src/components/Balance.js
@@ -3,9 +3,9 @@ import React from "react";
 import PairText from "./PairText";
 import { toPrecision } from "../utils";
 
-const Balance = ({ value = 0, currency = "OSN" }) => {
+const Balance = ({ value = 0, currency = "KSM" }) => {
   if (value === null || value === undefined) value = 0;
-  const precision = toPrecision(value, 8, false);
+  const precision = toPrecision(value, 12, false);
   return <PairText value={precision} unit={currency} />;
 };
 
