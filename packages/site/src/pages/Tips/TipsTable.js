c95f6f28fa497e900d621a07b25f08c666963bf4
qiyisi <qiyisi@qq.com>
2020-12-25 14:12:12 +0800

    Add tips table loading

--- packages/site/src/pages/Tips/TipsTable.js
+++ packages/site/src/pages/Tips/TipsTable.js
@@ -8,7 +8,6 @@ import User from "../../components/User/Index";
 import Balance from "../../components/Balance";
 import RightButton from "../../components/RightButton";
 import PairTextVertical from "../../components/PairTextVertical";
-import Status from "./Status";
 import ReasonText from "./ReasonText";
 
 const Wrapper = styled.div`
@@ -25,14 +24,6 @@ const Wrapper = styled.div`
   }
 `;
 
-const TableWrapper = styled.div``;
-
-// const Gap = styled.div`
-//   @media screen and (max-width: 1140px) {
-//     flex: 0 0 16px;
-//   }
-// `;
-
 const TipsTable = ({ data }) => {
   const history = useHistory();
 
@@ -44,64 +35,58 @@ const TipsTable = ({ data }) => {
 
   return (
     <Wrapper>
-      <TableWrapper>
-        <Table striped selectable unstackable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell textAlign={"center"}>
-                Beneficiary
-              </Table.HeaderCell>
-              <Table.HeaderCell textAlign={"center"} className="hidden">
-                Finder
-              </Table.HeaderCell>
-              <Table.HeaderCell>Reason</Table.HeaderCell>
-              <Table.HeaderCell textAlign={"right"}>Value</Table.HeaderCell>
-              <Table.HeaderCell textAlign={"right"}>Status</Table.HeaderCell>
-              <Table.HeaderCell className="hidden"></Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {data &&
-              data.map((item, index) => (
-                <Table.Row key={index} onClick={onClickRow}>
-                  <Table.Cell className="user-cell">
-                    <User
-                      name={item.beneficiary.name}
-                      address={item.beneficiary.address}
-                    />
-                  </Table.Cell>
-                  <Table.Cell className="user-cell hidden">
-                    <User
-                      name={item.finder.name}
-                      address={item.finder.address}
-                    />
-                  </Table.Cell>
-                  <Table.Cell>
-                    <ReasonText>{item.reason}</ReasonText>
-                  </Table.Cell>
-                  <Table.Cell className="balance-cell" textAlign={"right"}>
-                    <Balance
-                      value={item.balance.value}
-                      currency={item.balance.currency}
-                    />
-                  </Table.Cell>
-                  <Table.Cell className="status-cell" textAlign={"right"}>
-                    <PairTextVertical
-                      value={item.status.status}
-                      detail={item.status.time}
-                    />
-                  </Table.Cell>
-                  <Table.Cell className="link-cell hidden">
-                    <NavLink to="/detail">
-                      <RightButton />
-                    </NavLink>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table>
-        {/* <Gap /> */}
-      </TableWrapper>
+      <Table striped selectable unstackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell textAlign={"center"}>
+              Beneficiary
+            </Table.HeaderCell>
+            <Table.HeaderCell textAlign={"center"} className="hidden">
+              Finder
+            </Table.HeaderCell>
+            <Table.HeaderCell>Reason</Table.HeaderCell>
+            <Table.HeaderCell textAlign={"right"}>Value</Table.HeaderCell>
+            <Table.HeaderCell textAlign={"right"}>Status</Table.HeaderCell>
+            <Table.HeaderCell className="hidden"></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data &&
+            data.map((item, index) => (
+              <Table.Row key={index} onClick={onClickRow}>
+                <Table.Cell className="user-cell">
+                  <User
+                    name={item.beneficiary.name}
+                    address={item.beneficiary.address}
+                  />
+                </Table.Cell>
+                <Table.Cell className="user-cell hidden">
+                  <User name={item.finder.name} address={item.finder.address} />
+                </Table.Cell>
+                <Table.Cell>
+                  <ReasonText>{item.reason}</ReasonText>
+                </Table.Cell>
+                <Table.Cell className="balance-cell" textAlign={"right"}>
+                  <Balance
+                    value={item.balance.value}
+                    currency={item.balance.currency}
+                  />
+                </Table.Cell>
+                <Table.Cell className="status-cell" textAlign={"right"}>
+                  <PairTextVertical
+                    value={item.status.status}
+                    detail={item.status.time}
+                  />
+                </Table.Cell>
+                <Table.Cell className="link-cell hidden">
+                  <NavLink to="/detail">
+                    <RightButton />
+                  </NavLink>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table>
     </Wrapper>
   );
 };
