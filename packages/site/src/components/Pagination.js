1ffc010d98cd8107259ee24dff3dbb22b464b853
Yongfeng LI <wliyongfeng@gmail.com>
2020-12-25 16:38:44 +0800

    Fix pagination boundaryRange to 1

--- packages/site/src/components/Pagination.js
+++ packages/site/src/components/Pagination.js
@@ -12,7 +12,7 @@ const CustomPagination = (props) => {
   return (
     <Wrapper>
       <Pagination
-        boundaryRange={0}
+        boundaryRange={1}
         siblingRange={1}
         ellipsisItem={{ content: <Icon name='ellipsis horizontal' />, icon: true }}
         firstItem={{ content: <Icon name='angle double left' />, icon: true }}
