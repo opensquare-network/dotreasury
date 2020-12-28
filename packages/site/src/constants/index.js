8668a6e72e32faa1e4e077891c75799befbf6314
chaojun <chaojun@opensquare.network>
2020-12-28 16:22:50 +0800

    Show "--" as value for the retracted tips

--- packages/site/src/constants/index.js
+++ packages/site/src/constants/index.js
@@ -2,3 +2,10 @@ export const TEXT_DARK_MAJOR = "#1D253C";
 export const TEXT_DARK_MINOR = "rgba(29, 37, 60, 0.64)";
 export const PRIMARY_THEME_COLOR = "#DF405D";
 export const SECONDARY_THEME_COLOR = "#FFEEF1";
+
+export const TipStatus = {
+  Tipping: "Tipping",
+  Closing: "Closing",
+  Closed: "Closed",
+  Retracted: "Retracted",
+};
