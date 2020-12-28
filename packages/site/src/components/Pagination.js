85dc79623b0845bb15e24a2c446863c9c749a3d0
qiyisi <qiyisi@qq.com>
2020-12-28 14:11:33 +0800

    Update pagination

--- packages/site/src/components/Pagination.js
+++ packages/site/src/components/Pagination.js
@@ -3,14 +3,20 @@ import { Icon, Pagination } from "semantic-ui-react";
 import styled from "styled-components";
 
 import {PRIMARY_THEME_COLOR} from "../constants"
+import {SECONDARY_THEME_COLOR} from "../constants"
 
 const Wrapper = styled.div`
   margin: 10px 0;
   display: flex;
   justify-content: flex-end;
 
-  a:focus {
-    outline-color: ${PRIMARY_THEME_COLOR} !important;
+  a {
+    font-family: Inter !important;
+    outline: none !important;
+    &.active {
+      color: ${PRIMARY_THEME_COLOR} !important;
+      background: ${SECONDARY_THEME_COLOR} !important;
+    }
   }
 `;
 
@@ -19,13 +25,15 @@ const CustomPagination = (props) => {
     <Wrapper>
       <Pagination
         boundaryRange={1}
-        siblingRange={1}
+        siblingRange={0}
         ellipsisItem={{
           content: <Icon name="ellipsis horizontal" />,
           icon: true,
         }}
-        firstItem={{ content: <Icon name="angle double left" />, icon: true }}
-        lastItem={{ content: <Icon name="angle double right" />, icon: true }}
+        // firstItem={{ content: <Icon name="angle double left" />, icon: true }}
+        // lastItem={{ content: <Icon name="angle double right" />, icon: true }}
+        firstItem={null}
+        lastItem={null}
         {...props}
       />
     </Wrapper>
