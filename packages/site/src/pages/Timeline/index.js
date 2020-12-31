540cef2b8e8ba2914abf7d3959cb150bced81982
qiyisi <qiyisi@qq.com>
2020-12-31 10:15:07 +0800

    Update timeline loading

--- packages/site/src/pages/Timeline/index.js
+++ packages/site/src/pages/Timeline/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import {Image} from "semantic-ui-react";
 
 import Item from "./Item";
 // import FoldableItem from "./FoldableItem";
@@ -13,15 +14,24 @@ const Header = styled(SubTitle)`
   margin-bottom: 20px;
 `;
 
+const LoadingWrapper = styled.div`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 const ItemList = styled.div``;
 
-const Timeline = ({ data, polkassembly }) => {
+const Timeline = ({ data, polkassembly, loading }) => {
   return (
     <Wrapper>
       <Header>Timeline</Header>
+      { (loading && <LoadingWrapper><Image src="/imgs/loading.svg" /></LoadingWrapper>) ||
       <ItemList>
         { (data || []).map((item, index) => <Item key={index} data={item} polkassembly={polkassembly} />) }
       </ItemList>
+      }
     </Wrapper>
   );
 };
