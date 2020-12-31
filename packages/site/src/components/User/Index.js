137172c81726bc45b89360672b4759ed25ab6b0e
qiyisi <qiyisi@qq.com>
2020-12-31 14:35:19 +0800

    Fix username error warning

--- packages/site/src/components/User/Index.js
+++ packages/site/src/components/User/Index.js
@@ -25,23 +25,31 @@ const User = ({ address, ellipsis = true, popup = true }) => {
   const [name, setName] = useState(null)
   const [badgeData, setBadgeData] = useState(null)
   useEffect(() => {
+    let isMounted = true;
     const fetchIdentity = async () => {
       const identity = await getIndentity(address);
       if (identity && identity.display) {
-        setName(identity.displayParent ? `${identity.displayParent}/${identity.display}` : identity.display)
-        setBadgeData({
-          isNull: false,
-          hasParent: !!identity.displayParent,
-          isJuge: identity.judgements?.length > 0
-        })
+        if (isMounted) {
+          setName(identity.displayParent ? `${identity.displayParent}/${identity.display}` : identity.display)
+          setBadgeData({
+            isNull: false,
+            hasParent: !!identity.displayParent,
+            isJuge: identity.judgements?.length > 0
+          })
+        }
       } else {
-        setName(null)
-        setBadgeData(null)
+        if (isMounted) {
+          setName(null)
+          setBadgeData(null)
+        }
       }
     }
     setName(null)
     setBadgeData(null)
     fetchIdentity()
+    return () => {
+      isMounted = false;
+    }
   }, [address])
   return (
     <Wrapper>
