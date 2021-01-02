771065ae54977a3da8a87452e876e19dfec81219
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-02 17:55:03 +0800

    Set default value for REACT_APP_SOCKET_IO_URL

--- packages/site/src/services/websocket.js
+++ packages/site/src/services/websocket.js
@@ -3,7 +3,7 @@ import store from "../store";
 import { setScanHeight } from "../store/reducers/chainSlice";
 
 const chainStatusRoom = "CHAIN_STATUS_ROOM";
-const socket = io(process.env.REACT_APP_SOCKET_IO_URL);
+const socket = io(process.env.REACT_APP_SOCKET_IO_URL || "api.dotreasury.com");
 socket.connect();
 
 socket.on("connect", () => {
