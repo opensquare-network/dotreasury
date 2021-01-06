9d81f611db656af04d3a894fa66a28287c954751
qiyisi <qiyisi@qq.com>
2021-01-06 10:42:50 +0800

    Update bounty list

--- packages/site/src/pages/Bounties/index.js
+++ packages/site/src/pages/Bounties/index.js
@@ -1,49 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import Title from "../../components/Title";
 import ResponsivePagination from "../../components/ResponsivePagination";
 import BountiesTable from "./BountiesTable";
+import { useDispatch, useSelector } from "react-redux";
+import { useQuery } from "../../utils/hooks";
+import { useHistory } from "react-router";
+
+import {
+  fetchBounties,
+  loadingSelector,
+  bountyListSelector,
+} from "../../store/reducers/bountySlice";
 
 const Header = styled(Title)`
   margin-bottom: 20px;
 `;
 
+const DEFAULT_PAGE_SIZE = 20;
+const DEDAULT_QUERY_PAGE = 1;
+
 const Bounties = () => {
-  const testData = [
-    {
-      creator: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
-      beneficiary: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
-      title: "Kusama network UI Bounty",
-      update: {
-        time: "29 d 23 h",
-        blocks: 51767899
-      },
-      payout: "-",
-      value: 1000000000000,
-      status: "Active"
-    },
-    {
-      creator: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
-      beneficiary: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
-      title: "Kusama network UI Bounty",
-      update: {
-        time: "29 d 23 h",
-        blocks: 51767899
-      },
-      payout: "-",
-      value: 1000000000000,
-      status: "Active"
-    }
-  ]
+  // const testData = [
+  //   {
+  //     creator: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
+  //     beneficiary: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
+  //     title: "Kusama network UI Bounty",
+  //     update: {
+  //       time: "29 d 23 h",
+  //       blocks: 51767899
+  //     },
+  //     payout: "-",
+  //     value: 1000000000000,
+  //     status: "Active"
+  //   },
+  //   {
+  //     creator: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
+  //     beneficiary: "Gfazop6biSFdPJtCxGeVyJzTYprB1Us77Lx8Jww51fyTq5a",
+  //     title: "Kusama network UI Bounty",
+  //     update: {
+  //       time: "29 d 23 h",
+  //       blocks: 51767899
+  //     },
+  //     payout: "-",
+  //     value: 1000000000000,
+  //     status: "Active"
+  //   }
+  // ]
+
+  const searchPage = parseInt(useQuery().get("page"));
+  const queryPage = searchPage && !isNaN(searchPage) && searchPage > 0 ? searchPage : DEDAULT_QUERY_PAGE;
+  const [tablePage, setTablePage] = useState(queryPage);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { items: bounties, total } = useSelector(bountyListSelector);
+  const loading = useSelector(loadingSelector);
+
+  useEffect(() => {
+    dispatch(fetchBounties(tablePage - 1, DEFAULT_PAGE_SIZE));
+  }, [dispatch, tablePage]);
+
+  const totalPages = Math.ceil(total / DEFAULT_PAGE_SIZE);
 
   return (
     <>
       <Header>Bounties</Header>
-      <BountiesTable data={testData} />
+      <BountiesTable data={bounties} loading={loading} />
       <ResponsivePagination
-        activePage={1}
-        totalPages={3} />
+        activePage={tablePage}
+        totalPages={totalPages}
+        onPageChange={(_, { activePage }) => {
+          history.push({
+            search: activePage === DEDAULT_QUERY_PAGE ? null : `?page=${activePage}`
+          });
+          setTablePage(activePage);
+        }}
+      />
     </>
   );
 };
