dd97a85ba0b3e92edb13010b91cfbd26a12ce0d9
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-07 19:46:55 +0800

    Remove legacy code, #175

--- packages/scan/src/clean/index.js
+++ packages/scan/src/clean/index.js
@@ -1,23 +1,11 @@
-const { getBlockCollection } = require("../mongo");
-const { getExtrinsicCollection } = require("../mongo");
 const { getTipCollection } = require("../mongo");
 const { getBountyCollection } = require("../mongo");
-const { getBountyTimelineCollection } = require("../mongo");
 const { getProposalCollection } = require("../mongo");
 
 async function deleteDataFrom(blockHeight) {
-  const blockCol = await getBlockCollection();
-  await blockCol.deleteMany({ "header.number": { $gte: blockHeight } });
-
   await deleteTipFrom(blockHeight);
   await deleteBountyFrom(blockHeight);
-  await deleteBountyTimelineFrom(blockHeight);
   await deleteProposalFrom(blockHeight);
-}
-
-async function deleteExtrinsicsFrom(blockHeight) {
-  const col = await getExtrinsicCollection();
-  await col.deleteMany({ "indexer.blockHeight": { $gte: blockHeight } });
 }
 
 async function deleteTipFrom(blockHeight) {
@@ -27,11 +15,6 @@ async function deleteTipFrom(blockHeight) {
 
 async function deleteBountyFrom(blockHeight) {
   const col = await getBountyCollection();
-  await col.deleteMany({ "indexer.blockHeight": { $gte: blockHeight } });
-}
-
-async function deleteBountyTimelineFrom(blockHeight) {
-  const col = await getBountyTimelineCollection();
   await col.deleteMany({ "indexer.blockHeight": { $gte: blockHeight } });
 }
 
--- packages/scan/src/mongo/index.js
+++ packages/scan/src/mongo/index.js
@@ -3,13 +3,9 @@ const { MongoClient } = require("mongodb");
 const dbName = process.env.MONGO_DB_NAME || "dotreasury";
 
 const statusCollectionName = "status";
-const blockCollectionName = "block";
-const eventCollectionName = "event";
-const extrinsicCollectionName = "extrinsic";
 const tipCollectionName = "tip";
 const motionCollectionName = "motion";
 const bountyCollectionName = "bounty";
-const bountyTimelineCollectionName = "bountyTimeline";
 const proposalCollectionName = "proposal";
 const burntCollectionName = "burnt";
 
@@ -18,12 +14,8 @@ let db = null;
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017";
 let statusCol = null;
-let blockCol = null;
-let eventCol = null;
-let extrinsicCol = null;
 let tipCol = null;
 let bountyCol = null;
-let bountyTimelineCol = null;
 let proposalCol = null;
 let motionCol = null;
 let burntCol = null;
@@ -35,12 +27,8 @@ async function initDb() {
 
   db = client.db(dbName);
   statusCol = db.collection(statusCollectionName);
-  blockCol = db.collection(blockCollectionName);
-  eventCol = db.collection(eventCollectionName);
-  extrinsicCol = db.collection(extrinsicCollectionName);
   tipCol = db.collection(tipCollectionName);
   bountyCol = db.collection(bountyCollectionName);
-  bountyTimelineCol = db.collection(bountyTimelineCollectionName);
   proposalCol = db.collection(proposalCollectionName);
   motionCol = db.collection(motionCollectionName);
   burntCol = db.collection(burntCollectionName);
@@ -73,21 +61,6 @@ async function getStatusCollection() {
   return statusCol;
 }
 
-async function getBlockCollection() {
-  await tryInit(blockCol);
-  return blockCol;
-}
-
-async function getExtrinsicCollection() {
-  await tryInit(extrinsicCol);
-  return extrinsicCol;
-}
-
-async function getEventCollection() {
-  await tryInit(eventCol);
-  return eventCol;
-}
-
 async function getTipCollection() {
   await tryInit(tipCol);
   return tipCol;
@@ -96,11 +69,6 @@ async function getTipCollection() {
 async function getBountyCollection() {
   await tryInit(bountyCol);
   return bountyCol;
-}
-
-async function getBountyTimelineCollection() {
-  await tryInit(bountyTimelineCol);
-  return bountyTimelineCol;
 }
 
 async function getProposalCollection() {
@@ -115,12 +83,8 @@ async function getBurntCollection() {
 
 module.exports = {
   getStatusCollection,
-  getBlockCollection,
-  getExtrinsicCollection,
-  getEventCollection,
   getTipCollection,
   getBountyCollection,
-  getBountyTimelineCollection,
   getProposalCollection,
   getMotionCollection,
   getBurntCollection,
