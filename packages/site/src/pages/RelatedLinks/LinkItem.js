a9b8fa3d48a760c304828d7f3ce795c8ba6acdcf
chaojun <chaojun@opensquare.network>
2021-01-08 12:14:04 +0800

    Add github twiiter logos

--- packages/site/src/pages/RelatedLinks/LinkItem.js
+++ packages/site/src/pages/RelatedLinks/LinkItem.js
@@ -32,20 +32,30 @@ const Link = ({ link, text }) => {
   const url = new URL(link);
 
   let src = "";
-  if (url.host.endsWith("youtube.com")) {
+  let name = "";
+  if (url.host.endsWith("youtube.com") || url.host.endsWith("youtu.be")) {
     src = "/imgs/youtube-logo.svg";
-  } else if (url.host.endsWith("github.com")) {
-    src = "/imgs/youtube-logo.svg";
+    name = "YouTube";
+  } else if (url.host.endsWith("github.com") || url.host.endsWith("github.io")) {
+    src = "/imgs/github-logo.svg";
+    name = "GitHub";
   } else if (url.host.endsWith("medium.com")) {
     src = "/imgs/medium-logo.svg";
+    name = "Medium";
   } else if (url.host.endsWith("polkassembly.io")) {
     src = "/imgs/polkassembly-logo.svg";
+    name = "Polkassembly";
+  } else if (url.host.endsWith("twitter.com")) {
+    src = "/imgs/twitter-logo.svg";
+    name = "Twitter";
   } else {
     src = "/imgs/youtube-logo.svg";
   }
 
-  let [, name] = url.host.match(/([^.]*)\.[a-z]+$/);
-  name = stringUpperFirst(name);
+  if (!name) {
+    [, name] = url.host.match(/([^.]*)\.[a-z]+$/);
+    name = stringUpperFirst(name);
+  }
 
   return (
     <Wrapper>
