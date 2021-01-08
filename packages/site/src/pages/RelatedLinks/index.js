ee97b9e93a2cd7cbc8cc05e3303121cf7badc094
qiyisi <qiyisi@qq.com>
2021-01-08 18:36:42 +0800

    Hide related links

--- packages/site/src/pages/RelatedLinks/index.js
+++ packages/site/src/pages/RelatedLinks/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Icon, Modal, Form } from "semantic-ui-react";
+import { Button, Icon, Modal, Form, Divider } from "semantic-ui-react";
 import queryString from 'query-string';
 import { useLocation } from "react-router-dom"
 
@@ -69,76 +69,80 @@ const RelatedLinks = ({ type, index }) => {
   const q = queryString.parse(location.search);
   const isAdmin = q.admin === "true";
 
-  return (
-    <Wrapper>
-      <AdminLogin />
+  if (isAdmin || (links && links.length > 0)) {
+    return (
+      <Wrapper>
+        <AdminLogin />
 
-      <SubTitle>Ralated Links
-        { isAdmin
-            && <IconButton name="plus" onClick={() => setOpenAddLinkModal(true)} />
-        }
-      </SubTitle>
-      <LinksWrapper>
-        {
-          links.map((link, linkIndex) => <LinkWrapper key={linkIndex}>
-            { isAdmin
-                && <IconButton name="minus" onClick={() => {
-                      setLinkIndex(linkIndex);
-                      setOpenRemoveLinkModal(true);
-                    }} />
-            }
-            <LinkItem text={link.description} link={link.link} />
-          </LinkWrapper>)
-        }
-      </LinksWrapper>
+        <SubTitle>Ralated Links
+          { isAdmin
+              && <IconButton name="plus" onClick={() => setOpenAddLinkModal(true)} />
+          }
+        </SubTitle>
+        <LinksWrapper>
+          {
+            links.map((link, linkIndex) => <LinkWrapper key={linkIndex}>
+              { isAdmin
+                  && <IconButton name="minus" onClick={() => {
+                        setLinkIndex(linkIndex);
+                        setOpenRemoveLinkModal(true);
+                      }} />
+              }
+              <LinkItem text={link.description} link={link.link} />
+            </LinkWrapper>)
+          }
+        </LinksWrapper>
 
-      <Modal
-        size="small"
-        open={openAddLinkModal}
-        onClose={() => setOpenAddLinkModal(false)}
-      >
-        <Modal.Header>Add Link</Modal.Header>
-        <Modal.Content>
-          <Form>
-            <Form.Input fluid label='Link' placeholder='https://'
-              onChange={(_, { value }) => setLink(value)}
-            />
-            <Form.Input fluid label='Description' placeholder='The description of the link'
-              onChange={(_, { value }) => setDescription(value)}
-            />
-          </Form>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={() => setOpenAddLinkModal(false)}>
-            Cancel
-          </Button>
-          <Button onClick={() => addRelatedLink(link, description)}>
-            OK
-          </Button>
-        </Modal.Actions>
-      </Modal>
+        <Modal
+          size="small"
+          open={openAddLinkModal}
+          onClose={() => setOpenAddLinkModal(false)}
+        >
+          <Modal.Header>Add Link</Modal.Header>
+          <Modal.Content>
+            <Form>
+              <Form.Input fluid label='Link' placeholder='https://'
+                onChange={(_, { value }) => setLink(value)}
+              />
+              <Form.Input fluid label='Description' placeholder='The description of the link'
+                onChange={(_, { value }) => setDescription(value)}
+              />
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button onClick={() => setOpenAddLinkModal(false)}>
+              Cancel
+            </Button>
+            <Button onClick={() => addRelatedLink(link, description)}>
+              OK
+            </Button>
+          </Modal.Actions>
+        </Modal>
 
-      <Modal
-        size="mini"
-        open={openRemoveLinkModal}
-        onClose={() => setOpenRemoveLinkModal(false)}
-      >
-        <Modal.Header>Remove Link</Modal.Header>
-        <Modal.Content>
-          Are you sure want to remove this link?
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={() => setOpenRemoveLinkModal(false)}>
-            Cancel
-          </Button>
-          <Button onClick={() => removeRelatedLink(linkIndex)}>
-            OK
-          </Button>
-        </Modal.Actions>
-      </Modal>
-
-    </Wrapper>
-  );
+        <Modal
+          size="mini"
+          open={openRemoveLinkModal}
+          onClose={() => setOpenRemoveLinkModal(false)}
+        >
+          <Modal.Header>Remove Link</Modal.Header>
+          <Modal.Content>
+            Are you sure want to remove this link?
+          </Modal.Content>
+          <Modal.Actions>
+            <Button onClick={() => setOpenRemoveLinkModal(false)}>
+              Cancel
+            </Button>
+            <Button onClick={() => removeRelatedLink(linkIndex)}>
+              OK
+            </Button>
+          </Modal.Actions>
+        </Modal>
+        <Divider />
+      </Wrapper>
+    );
+  } else {
+    return null
+  }
 };
 
 export default RelatedLinks;
