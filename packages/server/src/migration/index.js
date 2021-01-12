ae6e19ce6717385cf04bcb9c3f5435cf7eee7409
qiyisi <qiyisi@qq.com>
2021-01-12 11:40:57 +0800

    init migration

---
+++ packages/server/src/migration/index.js
@@ -0,0 +1,25 @@
+const { getProposalCollection } = require("../mongo");
+const { getLinkCollection } = require("../mongo-admin");
+
+const main = async () => {
+  try {
+    console.log("migration start");
+    const proposalCol = await getProposalCollection();
+    const linkCol = await getLinkCollection();
+    const proposalLinks = await linkCol.find({type: 'proposal'}).toArray();
+    await Promise.all(proposalLinks.map(async item => {
+      await proposalCol.updateOne({
+          proposalIndex: item.indexer
+        }, {
+          "$set": { links: item.links }
+      });
+    }));
+    console.log("migration success")
+    process.exit(0);
+  } catch (err) {
+    console.log("migration error", err)
+    process.exit(1);
+  }
+}
+
+main();
