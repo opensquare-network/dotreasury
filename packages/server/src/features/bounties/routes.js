8fd50289c13952eb105369d6c940a7ca527d4cb2
chaojun <chaojun@opensquare.network>
2021-01-12 13:16:17 +0800

    Bounty related links

--- packages/server/src/features/bounties/routes.js
+++ packages/server/src/features/bounties/routes.js
@@ -7,4 +7,8 @@ router.get("/bounties", bountiesController.getBounties);
 router.get("/bounties/count", bountiesController.getBountiesCount);
 router.get("/bounties/:bountyIndex", bountiesController.getBountyDetail);
 
+router.get("/bounties/:bountyIndex/links", bountiesController.getBountyLinks);
+router.post("/bounties/:bountyIndex/links", bountiesController.createBountyLink);
+router.delete("/bounties/:bountyIndex/links/:linkIndex", bountiesController.deleteBountyLink);
+
 module.exports = router;
