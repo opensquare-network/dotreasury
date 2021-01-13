df318e183d5c77d39ee076e1a005b9b16ba97853
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-13 20:30:14 +0800

    Fix votes for early motions

--- packages/site/src/pages/ProposalDetail/index.js
+++ packages/site/src/pages/ProposalDetail/index.js
@@ -9,7 +9,10 @@ import {
   proposalDetailSelector,
   loadingProposalDetailSelector,
 } from "../../store/reducers/proposalSlice";
-import { fetchCurrentBlockHeight, currentBlockHeightSelector } from "../../store/reducers/chainSlice";
+import {
+  fetchCurrentBlockHeight,
+  currentBlockHeightSelector,
+} from "../../store/reducers/chainSlice";
 
 import InformationTable from "./InformationTable";
 import Timeline from "../Timeline";
@@ -46,65 +49,119 @@ const TableWrapper = styled.div`
 `;
 
 function processTimeline(proposalDetail, currentBlockHeight) {
-  return [{
-    name: "Proposed",
-    extrinsicIndexer: proposalDetail.indexer || {},
-    fields: [{
-      title: "Proposer",
-      value: <User address={proposalDetail.proposer} />
-    }, {
-      title: "Beneficiary",
-      value: <User address={proposalDetail.beneficiary} />
-    }, {
-      title: "Value",
-      value: <Balance value={proposalDetail.value} />
-    }]
-  },
-  ...(proposalDetail.motions || []).map(motion => ({
+  return [
+    {
+      name: "Proposed",
+      extrinsicIndexer: proposalDetail.indexer || {},
+      fields: [
+        {
+          title: "Proposer",
+          value: <User address={proposalDetail.proposer} />,
+        },
+        {
+          title: "Beneficiary",
+          value: <User address={proposalDetail.beneficiary} />,
+        },
+        {
+          title: "Value",
+          value: <Balance value={proposalDetail.value} />,
+        },
+      ],
+    },
+    ...(proposalDetail.motions || []).map((motion) => ({
       index: motion.index,
       polkassembly: polkassemblyApi.getMotionUrl(motion.index),
       defaultUnfold: !motion.result && motion.voting?.end >= currentBlockHeight,
       // FIXME: && motion.treasuryProposalId !== 15
-      expired: !motion.result && motion.voting?.end < currentBlockHeight && motion.treasuryProposalId !== 15,
+      expired:
+        !motion.result &&
+        motion.voting?.end < currentBlockHeight &&
+        motion.treasuryProposalId !== 15,
       end: motion.voting?.end,
-      subTimeline: (motion.timeline || []).map(item => ({
-        name: (item.action === "Propose" ? `Motion #${motion.index}` : item.action),
+      subTimeline: (motion.timeline || []).map((item) => ({
+        name:
+          item.action === "Propose" ? `Motion #${motion.index}` : item.action,
         extrinsicIndexer: item.extrinsic.extrinsicIndexer,
         fields: (() => {
           if (item.action === "Propose") {
             const [proposer, , , threshold] = item.eventData;
-            const ayes = motion.voting?.ayes?.length || 0;
-            const nays = motion.voting?.nays?.length || 0;
-            return [{
-              value: <Proposer address={proposer} agree={motion.result && motion.result === "Approved"} value={motion.method} threshold={threshold} ayes={ayes} nays={nays} />
-            }]
+            let ayes, nays;
+            if (motion.voting) {
+              ayes = motion.voting?.ayes?.length;
+              nays = motion.voting?.nays?.length || 0;
+            } else {
+              const votes = motion.timeline.filter(
+                (item) => item.action === "Vote"
+              );
+              const map = votes.reduce(
+                (result, { eventData: [voter, _, aye] }) => {
+                  result[voter] = aye;
+                  return result;
+                },
+                {}
+              );
+              const values = Object.values(map);
+              ayes = values.filter((v) => v).length;
+              nays = values.length - ayes;
+            }
+            return [
+              {
+                value: (
+                  <Proposer
+                    address={proposer}
+                    agree={motion.result && motion.result === "Approved"}
+                    value={motion.method}
+                    threshold={threshold}
+                    ayes={ayes}
+                    nays={nays}
+                  />
+                ),
+              },
+            ];
           } else if (item.action === "Vote") {
             const [voter, , agree] = item.eventData;
-            return [{
-              value: <Voter address={voter} agree={agree} value={agree ? "Aye" : "Nay"} />
-            }]
+            return [
+              {
+                value: (
+                  <Voter
+                    address={voter}
+                    agree={agree}
+                    value={agree ? "Aye" : "Nay"}
+                  />
+                ),
+              },
+            ];
           } else if (item.action === "Close") {
-            return [{
-              title: motion.result
-            }]
+            return [
+              {
+                title: motion.result,
+              },
+            ];
           } else {
             return [];
           }
         })(),
-      }))
+      })),
     })),
-    ...(proposalDetail.latestState?.state === "Awarded" ? [{
-      name: "Awarded",
-      eventIndexer: proposalDetail.latestState?.indexer || {},
-      fields: [{
-        title: "Beneficiary",
-        value: <User address={proposalDetail.latestState?.data[2]} />
-      }, {
-        title: "Balance",
-        value: <Balance value={proposalDetail.latestState?.data[1]} />
-      }]
-    }] : [])
-  ]
+    ...(proposalDetail.latestState?.state === "Awarded"
+      ? [
+          {
+            name: "Awarded",
+            eventIndexer: proposalDetail.latestState?.indexer || {},
+            fields: [
+              {
+                title: "Beneficiary",
+                value: <User address={proposalDetail.latestState?.data[2]} />,
+              },
+              {
+                title: "Balance",
+                value: <Balance value={proposalDetail.latestState?.data[1]} />,
+              },
+            ],
+          },
+        ]
+      : []),
+  ];
 }
 
 const ProposalDetail = () => {
@@ -140,10 +197,7 @@ const ProposalDetail = () => {
       </TableWrapper>
       <RelatedLinks type="proposals" index={parseInt(proposalIndex)} />
       <TimelineCommentWrapper>
-        <Timeline
-          data={timelineData}
-          loading={loadingProposalDetail}
-        />
+        <Timeline data={timelineData} loading={loadingProposalDetail} />
         <Comment />
       </TimelineCommentWrapper>
     </>
