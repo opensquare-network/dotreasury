a755b0b163d75efb2111e5e4b8bd00035a9f4752
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-13 21:05:05 +0800

    Fix links migration

--- packages/server/src/migration/index.js
+++ packages/server/src/migration/index.js
@@ -6,20 +6,25 @@ const main = async () => {
     console.log("migration start");
     const proposalCol = await getProposalCollection();
     const linkCol = await getLinkCollection();
-    const proposalLinks = await linkCol.find({type: 'proposal'}).toArray();
-    await Promise.all(proposalLinks.map(async item => {
-      await proposalCol.updateOne({
-          proposalIndex: item.indexer
-        }, {
-          "$set": { links: item.links }
-      });
-    }));
-    console.log("migration success")
+    const proposalLinks = await linkCol.find({ type: "proposals" }).toArray();
+    await Promise.all(
+      proposalLinks.map(async (item) => {
+        await proposalCol.updateOne(
+          {
+            proposalIndex: item.indexer,
+          },
+          {
+            $set: { links: item.links },
+          }
+        );
+      })
+    );
+    console.log("migration success");
     process.exit(0);
   } catch (err) {
-    console.log("migration error", err)
+    console.log("migration error", err);
     process.exit(1);
   }
-}
+};
 
 main();
