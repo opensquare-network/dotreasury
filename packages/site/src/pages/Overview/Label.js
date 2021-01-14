3faa4a668b685600db38598b23dc8a11ecd07ae4
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-14 18:38:47 +0800

    Remove useless imports

--- packages/site/src/pages/Overview/Label.js
+++ packages/site/src/pages/Overview/Label.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled, { css } from "styled-components";
-import Balance from "../../components/Balance"
-import {Popup} from "semantic-ui-react";
+import { Popup } from "semantic-ui-react";
 
 import Text from "../../components/Text";
 
 const Wrapper = styled.div`
-  background: #FBFBFB;
+  background: #fbfbfb;
   padding: 4px 16px;
   display: flex;
   align-items: center;
@@ -15,29 +14,33 @@ const Wrapper = styled.div`
   :hover {
     cursor: pointer;
   }
-`
+`;
 
 const Circle = styled.div`
   width: 16px;
   height: 16px;
-  border: 4px solid ${p => p.disabled ? "rgba(29, 37, 60, 0.24)" : p.color};
+  border: 4px solid ${(p) => (p.disabled ? "rgba(29, 37, 60, 0.24)" : p.color)};
   border-radius: 50%;
-`
+`;
 
 const LabelText = styled(Text)`
   font-weight: 500;
-  ${p => p.disabled&&css`
-    color: rgba(29, 37, 60, 0.24);
-  `}
-`
+  ${(p) =>
+    p.disabled &&
+    css`
+      color: rgba(29, 37, 60, 0.24);
+    `}
+`;
 
 const ValueText = styled(Text)`
   font-weight: 300;
   font-size: 0.8em;
-  ${p => p.disabled&&css`
-    color: rgba(29, 37, 60, 0.24);
-  `}
-`
+  ${(p) =>
+    p.disabled &&
+    css`
+      color: rgba(29, 37, 60, 0.24);
+    `}
+`;
 
 const Label = ({ name, value, color, disabled, onToggleDisabled }) => {
   return (
@@ -46,11 +49,15 @@ const Label = ({ name, value, color, disabled, onToggleDisabled }) => {
       <LabelText disabled={disabled}>{name}</LabelText>
       <Popup
         content={`${value} KSM`}
-        size='mini'
-        trigger={<ValueText disabled={disabled}>{`${parseInt(value) === value ? "" : "≈ "}${parseInt(value)} KSM`}</ValueText>}
-        />
+        size="mini"
+        trigger={
+          <ValueText disabled={disabled}>{`${
+            parseInt(value) === value ? "" : "≈ "
+          }${parseInt(value)} KSM`}</ValueText>
+        }
+      />
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Label;
