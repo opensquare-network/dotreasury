38fca45d482858b44b7f3b131e20f070e17d8185
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-19 18:39:58 +0800

    Add staging scan pm2 app

--- packages/scan/ecosystem.config.js
+++ packages/scan/ecosystem.config.js
@@ -1,15 +1,23 @@
+const commonPart = {
+  script: "src/index.js",
+  log_date_format: "YYYY-MM-DD HH:mm Z",
+  env: {
+    NODE_ENV: "development",
+  },
+  env_production: {
+    NODE_ENV: "production",
+  },
+};
+
 module.exports = {
   apps: [
     {
-      name: "doTreasury scan",
-      script: "src/index.js",
-      log_date_format: "YYYY-MM-DD HH:mm Z",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
+      name: "doTreasury-scan",
+      ...commonPart,
+    },
+    {
+      name: "doTreasury-scan-staging",
+      ...commonPart,
     },
   ],
 };
