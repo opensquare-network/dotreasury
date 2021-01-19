8089e7142a6a4c8169957901fd9439d24cad4a16
chaojun <chaojun@opensquare.network>
2021-01-19 14:59:49 +0800

    Show update due as time instead of blocks

--- packages/site/src/components/BlocksTime.js
+++ packages/site/src/components/BlocksTime.js
@@ -1,25 +1,23 @@
-import React, { useEffect, useState } from "react";
-import TimePeriod from "./TimePeriod";
-import { estimateBlocksTime } from "../services/chainApi";
-import { useIsMounted } from "../utils/hooks"
+import React from "react";
+import BlocksTimeDefaultUnit from "./BlocksTimeDefaultUnit";
 
-export default function BlocksTime({ blocks, unitMapper = {}, pluralUnitMapper = {}, ...others }) {
-  const [time, setTime] = useState(0);
-  const isMounted = useIsMounted();
-
-  useEffect(() => {
-    estimateBlocksTime(blocks).then(blocksTime => {
-      if (isMounted.current) {
-        setTime(blocksTime);
-      }
-    });
-  }, [blocks, isMounted]);
-
+export default function BlocksTime({
+  blocks,
+  unitMapper = {},
+  pluralUnitMapper = {},
+  ...others
+}) {
   return (
-    <TimePeriod
-      time={time}
-      unitMapper={Object.assign({ y: "yr", d: "day", h: "hr", s: "sec" }, unitMapper)}
-      pluralUnitMapper={Object.assign({ y: "yrs", mon: "mons", d: "days", h: "hrs", min: "mins", s: "secs" }, pluralUnitMapper)}
+    <BlocksTimeDefaultUnit
+      blocks={blocks}
+      unitMapper={Object.assign(
+        { y: "yr", d: "day", h: "hr", s: "sec" },
+        unitMapper
+      )}
+      pluralUnitMapper={Object.assign(
+        { y: "yrs", mon: "mons", d: "days", h: "hrs", min: "mins", s: "secs" },
+        pluralUnitMapper
+      )}
       {...others}
     />
   );
