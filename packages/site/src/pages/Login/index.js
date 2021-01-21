14fc78d7db159f7e1a3a1fe3a4f0e6612df154e6
qiyisi <qiyisi@qq.com>
2021-01-21 17:07:07 +0800

    Update login and register

--- packages/site/src/pages/Login/index.js
+++ packages/site/src/pages/Login/index.js
@@ -3,16 +3,45 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect } from 'react-router-dom';
 import queryString from 'query-string';
 import { useForm } from 'react-hook-form';
+import styled from "styled-components";
+import { Button, Form } from "semantic-ui-react";
 
 import { loggedInUserSelector, setLoggedInUser } from "../../store/reducers/userSlice";
 import scanApi from "../../services/scanApi";
-import { useIsMounted } from "../../utils/hooks"
+import Card from "../../components/Card";
+import Text from "../../components/Text";
+import { TEXT_DARK_MAJOR, PRIMARY_THEME_COLOR } from "../../constants";
 
+const CardWrapper = styled(Card)`
+  max-width: 450px;
+  margin: auto;
+  padding: 20px;
+  .ui.form input:focus {
+    border-color: ${PRIMARY_THEME_COLOR} !important;
+  }
+  label {
+    color: ${TEXT_DARK_MAJOR} !important;
+  }
+`
+
+const Header = styled(Text)`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 30px;
+`
+
+const StyledLink = styled(Link)`
+  display: block;
+  color: ${PRIMARY_THEME_COLOR};
+  :hover {
+    color: ${PRIMARY_THEME_COLOR};
+  }
+  margin-bottom: 20px;
+`
 
 function Login({ location }) {
   const loggedInUser = useSelector(loggedInUserSelector);
   const { register, handleSubmit, errors } = useForm();
-  const isMounted = useIsMounted();
   const dispatch = useDispatch();
 
   // Redirect out of here if user has already logged in
@@ -24,17 +53,14 @@ function Login({ location }) {
   // Do login
   const onSubmit = async (formData) => {
     const loginResult = await scanApi.login(formData.username, formData.password);
-    if (isMounted.current) {
-      dispatch(setLoggedInUser(loginResult));
-    }
+    dispatch(setLoggedInUser(loginResult));
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <div>
+    <CardWrapper>
+      <Header>Login</Header>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Field>
             <label htmlFor="username">Username
               { errors.username
                 && <span>
@@ -43,10 +69,8 @@ function Login({ location }) {
               }
             </label>
             <input name="username" type="text" ref={register({required: true})} />
-          </div>
-        </div>
-        <div>
-          <div>
+          </Form.Field>
+          <Form.Field>
             <label htmlFor="password">Password
               { errors.password
                 && <span>
@@ -55,15 +79,11 @@ function Login({ location }) {
               }
             </label>
             <input name="password" type="password" ref={register({required: true})} />
-          </div>
-        </div>
-        <div>
-          <div>
-            <input type="submit" value="Login" />/<Link to="/register">Register new user</Link>
-          </div>
-        </div>
-      </form>
-    </div>
+          </Form.Field>
+        <StyledLink to="/register">Register new user</StyledLink>
+        <Button primary type="submit">Login</Button>
+      </Form>
+    </CardWrapper>
   );
 }
 
