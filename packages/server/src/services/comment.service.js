2706e31ab80024d4c04a78e55ab1fc6991057144
chaojun <chaojun@opensquare.network>
2021-01-25 11:50:38 +0800

    Add comment reaction api

--- packages/server/src/services/comment.service.js
+++ packages/server/src/services/comment.service.js
@@ -6,55 +6,60 @@ class CommentService {
   async getComments(indexer, validateBeforeCreate) {
     const discuCol = await getDiscussionCollection();
 
-    let discussion = await discuCol.aggregate([
-      { $match: { indexer } },
-      { $unwind: "$comments" },
-      {
-        $lookup: {
-          from: "user",
-          localField: "comments.authorId",
-          foreignField: "_id",
-          as: "comments.author"
-        }
-      },
-      {
-        $addFields: {
-          "comments.author": { $arrayElemAt: [
-            {
-              $map: {
-                input: "$comments.author",
-                in: {
-                  username: "$$this.username",
-                }
-              }
-            }, 0]
+    let discussion = await discuCol
+      .aggregate([
+        { $match: { indexer } },
+        { $unwind: "$comments" },
+        {
+          $lookup: {
+            from: "user",
+            localField: "comments.authorId",
+            foreignField: "_id",
+            as: "comments.author",
           },
-        }
-      },
-      {
-        $project: {
-          "comments.authorId": 0,
-        }
-      },
-      {
-        $group: {
-          _id: {
-            _id: "$_id",
-            indexer: "$indexer",
+        },
+        {
+          $addFields: {
+            "comments.author": {
+              $arrayElemAt: [
+                {
+                  $map: {
+                    input: "$comments.author",
+                    in: {
+                      username: "$$this.username",
+                    },
+                  },
+                },
+                0,
+              ],
+            },
           },
-          comments: {
-            $push: "$comments"
-          }
-        }
-      },
-      {
-        $project: {
-          _id: "$_id._id",
-          indexer: "$_id.indexer",
-          comments: "$comments"
-        }
-      }
-    ]).toArray();
+        },
+        {
+          $project: {
+            "comments.authorId": 0,
+          },
+        },
+        {
+          $group: {
+            _id: {
+              _id: "$_id",
+              indexer: "$indexer",
+            },
+            comments: {
+              $push: "$comments",
+            },
+          },
+        },
+        {
+          $project: {
+            _id: "$_id._id",
+            indexer: "$_id.indexer",
+            comments: "$comments",
+          },
+        },
+      ])
+      .toArray();
 
     if (discussion[0]) {
       return discussion[0];
@@ -82,17 +87,20 @@ class CommentService {
     const discuCol = await getDiscussionCollection();
 
     const now = new Date();
-    const result = await discuCol.updateOne({ indexer }, {
-      $push: {
-        comments: {
-          commentId: new ObjectId(),
-          authorId,
-          content,
-          createdAt: now,
-          updatedAt: now,
-        }
+    const result = await discuCol.updateOne(
+      { indexer },
+      {
+        $push: {
+          comments: {
+            commentId: new ObjectId(),
+            authorId,
+            content,
+            createdAt: now,
+            updatedAt: now,
+          },
+        },
       }
-    });
+    );
 
     if (!result.result.ok) {
       throw new HttpError(500, "Post comment error.");
@@ -109,20 +117,23 @@ class CommentService {
     const discuCol = await getDiscussionCollection();
 
     const now = new Date();
-    const result = await discuCol.updateOne({
-      indexer,
-      comments: {
-        $elemMatch: {
-          commentId: ObjectId(commentId),
-          authorId,
-        }
+    const result = await discuCol.updateOne(
+      {
+        indexer,
+        comments: {
+          $elemMatch: {
+            commentId: ObjectId(commentId),
+            authorId,
+          },
+        },
+      },
+      {
+        $set: {
+          "comments.$.content": newContent,
+          "comments.$.updatedAt": now,
+        },
       }
-    }, {
-      $set: {
-        "comments.$.content": newContent,
-        "comments.$.updatedAt": now,
-      }
-    });
+    );
 
     if (!result.result.ok) {
       throw new HttpError(500, "Update comment error.");
@@ -137,19 +148,17 @@ class CommentService {
 
   async deleteComment(indexer, commentId, authorId) {
     const discuCol = await getDiscussionCollection();
-    let result = await discuCol.updateOne({
-      indexer,
-      comments: {
-        $elemMatch: {
-          commentId: ObjectId(commentId),
-          authorId,
-        }
+    let result = await discuCol.updateOne(
+      { indexer },
+      {
+        $pull: {
+          comments: {
+            commentId: ObjectId(commentId),
+            authorId,
+          },
+        },
       }
-    }, {
-      $unset: {
-        "comments.$": true
-      }
-    });
+    );
 
     if (!result.result.ok) {
       throw new HttpError(500, "Delete comment error.");
@@ -159,18 +168,106 @@ class CommentService {
       return false;
     }
 
-    result = await discuCol.updateOne({ indexer }, {
-      $pull: {
-        comments: null
+    return true;
+  }
+
+  async unsetCommentReaction(indexer, commentId, userId) {
+    const discuCol = await getDiscussionCollection();
+
+    const result = await discuCol.updateOne(
+      {
+        indexer,
+        "comments.commentId": ObjectId(commentId),
+      },
+      {
+        $pull: {
+          "comments.$.reactions": {
+            userId,
+          },
+        },
       }
-    });
+    );
 
     if (!result.result.ok) {
-      throw new HttpError(500, "Clean up empty comment error.");
+      throw new HttpError(500, "Db error, clean reaction.");
+    }
+
+    if (result.result.nModified === 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  async setCommentReaction(indexer, commentId, reaction, userId) {
+    const discuCol = await getDiscussionCollection();
+
+    const now = new Date();
+    let result = await discuCol.updateOne(
+      {
+        indexer,
+        comments: {
+          $elemMatch: {
+            commentId: ObjectId(commentId),
+            "reactions.userId": userId,
+          },
+        },
+      },
+      {
+        $set: {
+          "comments.$.reactions.$[reaction].reaction": reaction,
+          "comments.$.reactions.$[reaction].updatedAt": now,
+        },
+      },
+      {
+        arrayFilters: [
+          {
+            "reaction.userId": userId,
+          },
+        ],
+      }
+    );
+
+    if (!result.result.ok) {
+      throw new HttpError(500, "Db error, update reaction.");
+    }
+
+    if (result.result.nModified > 0) {
+      return true;
+    }
+
+    result = await discuCol.updateOne(
+      {
+        indexer,
+        comments: {
+          $elemMatch: {
+            commentId: ObjectId(commentId),
+            authorId: { $ne: userId },
+          },
+        },
+      },
+      {
+        $push: {
+          "comments.$.reactions": {
+            userId,
+            reaction,
+            createdAt: now,
+            updatedAt: now,
+          },
+        },
+      }
+    );
+
+    if (!result.result.ok) {
+      throw new HttpError(500, "Db error, add reaction.");
+    }
+
+    if (result.result.nModified === 0) {
+      return false;
     }
 
     return true;
   }
 }
 
-module.exports = new CommentService;
+module.exports = new CommentService();
