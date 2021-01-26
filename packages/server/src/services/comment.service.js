608c5bd10cf07b7680ec0622c7dab386dee7500e
chaojun <chaojun@opensquare.network>
2021-01-26 09:35:58 +0800

    Updatee reaction user

--- packages/server/src/services/comment.service.js
+++ packages/server/src/services/comment.service.js
@@ -1,85 +1,66 @@
 const { ObjectId } = require("mongodb");
-const { getDiscussionCollection } = require("../mongo-admin");
+const {
+  getDiscussionCollection,
+  getUserCollection,
+} = require("../mongo-admin");
 const { HttpError } = require("../exc");
 
 class CommentService {
   async getComments(indexer, validateBeforeCreate) {
     const discuCol = await getDiscussionCollection();
+    let discussion = await discuCol.findOne({ indexer });
 
-    let discussion = await discuCol
-      .aggregate([
-        { $match: { indexer } },
-        { $unwind: "$comments" },
-        {
-          $lookup: {
-            from: "user",
-            localField: "comments.authorId",
-            foreignField: "_id",
-            as: "comments.author",
-          },
-        },
-        {
-          $addFields: {
-            "comments.author": {
-              $arrayElemAt: [
-                {
-                  $map: {
-                    input: "$comments.author",
-                    in: {
-                      username: "$$this.username",
-                    },
-                  },
-                },
-                0,
-              ],
-            },
-          },
-        },
-        {
-          $project: {
-            "comments.authorId": 0,
-          },
-        },
-        {
-          $group: {
-            _id: {
-              _id: "$_id",
-              indexer: "$indexer",
-            },
-            comments: {
-              $push: "$comments",
-            },
-          },
-        },
-        {
-          $project: {
-            _id: "$_id._id",
-            indexer: "$_id.indexer",
-            comments: "$comments",
-          },
-        },
-      ])
-      .toArray();
+    if (discussion) {
+      const userIds = new Set();
+      discussion.comments?.forEach((comment) => {
+        userIds.add(comment.authorId.toString());
+        comment.reactions?.forEach((reaction) => {
+          userIds.add(reaction.userId.toString());
+        });
+      });
 
-    if (discussion[0]) {
-      return discussion[0];
+      const userCol = await getUserCollection();
+      const users = await userCol
+        .find(
+          { _id: { $in: Array.from(userIds).map(ObjectId) } },
+          { projection: { username: 1 } }
+        )
+        .toArray();
+      const userMap = {};
+      users.forEach((user) => {
+        userMap[user._id.toString()] = user;
+      });
+
+      discussion.comments?.forEach((comment) => {
+        comment.author = userMap[comment.authorId.toString()];
+        delete comment.authorId;
+        comment.reactions?.forEach((reaction) => {
+          reaction.user = userMap[reaction.userId.toString()];
+          delete reaction.userId;
+        });
+      });
+
+      users.forEach((user) => {
+        delete user._id;
+      });
+    } else {
+      // If the discussion record is not created yet, then validate and create
+      if (validateBeforeCreate) {
+        await validateBeforeCreate();
+      }
+
+      const result = await discuCol.insertOne({
+        indexer,
+        comments: [],
+      });
+
+      if (!result.result.ok) {
+        throw new HttpError(500, "Cannot initialize discussion.");
+      }
+
+      discussion = result.ops[0];
     }
 
-    // If the discussion record is not created yet, then validate and create
-    if (validateBeforeCreate) {
-      await validateBeforeCreate();
-    }
-
-    const result = await discuCol.insertOne({
-      indexer,
-      comments: [],
-    });
-
-    if (!result.result.ok) {
-      throw new HttpError(500, "Cannot initialize discussion.");
-    }
-
-    discussion = result.ops[0];
     return discussion;
   }
 
