26a1aff2fc5ac96f443fd7f87b5b3ffac93995ae
chaojun <chaojun@opensquare.network>
2021-01-26 14:30:44 +0800

    Implement web3 address login

--- packages/site/src/components/AccountSelector.js
+++ packages/site/src/components/AccountSelector.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled, { css } from "styled-components";
-import { Dropdown, Image } from 'semantic-ui-react';
+import { Dropdown, Image } from "semantic-ui-react";
 
 import Text from "./Text";
 import TextMinor from "./TextMinor";
 import { PRIMARY_THEME_COLOR } from "../constants";
 
+import Address from "./Address";
+
 const Wrapper = styled.div`
   margin-bottom: 24px;
-`
+`;
 
 const Label = styled(Text)`
   font-weight: 500;
   margin-bottom: 8px;
-`
+`;
 
 const DropdownWrapper = styled.div`
   position: relative;
-`
+`;
 
 const StyledDropdown = styled(Dropdown)`
   width: 100%;
   height: 64px;
-  :active, :focus {
+  :active,
+  :focus {
     border-color: ${PRIMARY_THEME_COLOR} !important;
   }
-  &.active, & .menu {
+  &.active,
+  & .menu {
     border-color: ${PRIMARY_THEME_COLOR} !important;
   }
   &.ui.dropdown .menu > .item {
@@ -36,11 +40,11 @@ const StyledDropdown = styled(Dropdown)`
     transform: translate(0, -9px) !important;
     opacity: 0.24 !important;
   }
-`
+`;
 
 const StyledDropdownItem = styled(Dropdown.Item)`
   padding: 0 !important;
-`
+`;
 
 const ItemWrapper = styled.div`
   height: 64px;
@@ -50,43 +54,65 @@ const ItemWrapper = styled.div`
   img {
     margin-right: 16px;
   }
-  ${p => p.header && css`
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 99;
-    pointer-events: none;
-  `}
-`
+  ${(p) =>
+    p.header &&
+    css`
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 99;
+      pointer-events: none;
+    `}
+`;
 
-const AccountItem = ({header}) => {
+const AccountItem = ({ header, accountName, accountAddress }) => {
   return (
     <ItemWrapper header={header}>
       <Image src="/imgs/avatar.png" width="40px" />
       <div>
-        <Text>Address Name</Text>
-        <TextMinor>HUfzjs...yCprsX</TextMinor>
+        <Text>{accountName}</Text>
+        <TextMinor>
+          <Address>{accountAddress}</Address>
+        </TextMinor>
       </div>
     </ItemWrapper>
-  )
-}
+  );
+};
 
-const AccountSelector = () => {
+const AccountSelector = ({ accounts, onSelect = () => {} }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  useEffect(() => {
+    onSelect(accounts[selectedIndex]);
+  }, [selectedIndex]);
   return (
     <Wrapper>
       <Label>Choose linked account</Label>
       <DropdownWrapper>
         <StyledDropdown selection labeled>
           <Dropdown.Menu>
-            <StyledDropdownItem>
-              <AccountItem />
-            </StyledDropdownItem>
+            {(accounts || []).map((account, index) => (
+              <StyledDropdownItem
+                key={index}
+                onClick={() => {
+                  setSelectedIndex(index);
+                }}
+              >
+                <AccountItem
+                  accountName={account.name}
+                  accountAddress={account.address}
+                />
+              </StyledDropdownItem>
+            ))}
           </Dropdown.Menu>
         </StyledDropdown>
-        <AccountItem header />
+        <AccountItem
+          accountName={accounts?.[selectedIndex]?.name}
+          accountAddress={accounts?.[selectedIndex]?.address}
+          header
+        />
       </DropdownWrapper>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default AccountSelector;
--- packages/site/src/pages/Login/index.js
+++ packages/site/src/pages/Login/index.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, Redirect } from 'react-router-dom';
-import queryString from 'query-string';
-import { useForm } from 'react-hook-form';
+import { Link, Redirect } from "react-router-dom";
+import queryString from "query-string";
+import { useForm } from "react-hook-form";
 import styled, { css } from "styled-components";
 import { Form, Divider } from "semantic-ui-react";
 
-import { loggedInUserSelector, setLoggedInUser } from "../../store/reducers/userSlice";
+import {
+  loggedInUserSelector,
+  setLoggedInUser,
+} from "../../store/reducers/userSlice";
 import scanApi from "../../services/scanApi";
 import Card from "../../components/Card";
 import Text from "../../components/Text";
-import { TEXT_DARK_MAJOR, TEXT_DARK_MINOR, PRIMARY_THEME_COLOR } from "../../constants";
+import {
+  TEXT_DARK_MAJOR,
+  TEXT_DARK_MINOR,
+  PRIMARY_THEME_COLOR,
+} from "../../constants";
 import FormInput from "../../components/FormInput";
 import FormPasswordWrapper from "../../components/FormPasswordWrapper";
 import ButtonPrimary from "../../components/ButtonPrimary";
@@ -19,6 +26,15 @@ import GrayImage from "../../components/GrayImage";
 import TextMinor from "../../components/TextMinor";
 import DownloadPolkadot from "../../components/DownloadPolkadot";
 import AccountSelector from "../../components/AccountSelector";
+
+import api from "../../services/scanApi";
+import { signMessage } from "../../services/chainApi";
+import { useIsMounted } from "../../utils/hooks";
+import {
+  isWeb3Injected,
+  web3Accounts,
+  web3Enable,
+} from "@polkadot/extension-dapp";
 
 const CardWrapper = styled(Card)`
   max-width: 424px;
@@ -43,7 +59,7 @@ const CardWrapper = styled(Card)`
   @media screen and (max-width: 408px) {
     padding: 32px 16px;
   }
-`
+`;
 
 const Header = styled(Text)`
   font-family: "Montserrat";
@@ -52,14 +68,14 @@ const Header = styled(Text)`
   line-height: 44px;
   margin-bottom: 24px;
   text-align: center;
-`
+`;
 
 const HelperWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-bottom: 24px;
-`
+`;
 
 const RememberMe = styled.div`
   display: flex;
@@ -69,41 +85,43 @@ const RememberMe = styled.div`
     line-height: 24px;
   }
   cursor: pointer;
-`
+`;
 
 const StyledTextMnor = styled(TextMinor)`
   text-decoration: underline;
-`
+`;
 
 const CheckImage = styled(GrayImage)`
   margin-right: 8px;
-  ${p => p.active && css`
-    -webkit-filter: grayscale(0);
-    filter: grayscale(0);
-    opacity: 1;
-  `}
-`
+  ${(p) =>
+    p.active &&
+    css`
+      -webkit-filter: grayscale(0);
+      filter: grayscale(0);
+      opacity: 1;
+    `}
+`;
 
 const StyledButtonPrimary = styled(ButtonPrimary)`
   width: 100%;
   margin-bottom: 16px !important;
-`
+`;
 
 const StyledButton = styled(Button)`
   width: 100%;
   margin-bottom: 24px !important;
-`
+`;
 
 const StyledDivider = styled(Divider)`
   margin: 0 !important;
-`
+`;
 
 const SignUpWrapper = styled.div`
   margin-top: 16px;
   line-height: 24px;
   text-align: center;
   color: ${TEXT_DARK_MINOR};
-`
+`;
 
 const StyledLink = styled(Link)`
   margin-left: 8px;
@@ -111,83 +129,176 @@ const StyledLink = styled(Link)`
   :hover {
     color: ${PRIMARY_THEME_COLOR};
   }
-`
+`;
 
 function Login({ location }) {
   const [web3Login, setWeb3Login] = useState(false);
-  const [hasExtension] = useState(true);
+  const [accounts, setAccounts] = useState([]);
+  const [hasExtension, setHasExtension] = useState(true);
   const [showPassword, setShowPassword] = useState(false);
   const [isRememberMe, setIsRememberMe] = useState(false);
   const loggedInUser = useSelector(loggedInUserSelector);
   const { register, handleSubmit, errors } = useForm();
   const dispatch = useDispatch();
+  const [selectedAccount, setSelectedAccount] = useState(null);
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    if (web3Login) {
+      (async () => {
+        await web3Enable("doTreasury");
+        if (!isWeb3Injected) {
+          if (isMounted.current) {
+            setHasExtension(false);
+          }
+          return;
+        }
+        const extensionAccounts = await web3Accounts();
+        const accounts = extensionAccounts.map(
+          ({ address, meta: { name } }) => {
+            return {
+              address,
+              name,
+            };
+          }
+        );
+
+        if (isMounted.current) {
+          setAccounts(accounts);
+        }
+      })();
+    }
+  }, [web3Login]);
 
   // Redirect out of here if user has already logged in
   if (loggedInUser) {
     const q = queryString.parse(location.search);
-    return <Redirect to={q.returnUrl || '/'} />;
+    return <Redirect to={q.returnUrl || "/"} />;
   }
+
+  const saveLoggedInResult = (loginResult) => {
+    const loggedInUser = {
+      username: loginResult.username,
+      email: loginResult.email,
+    };
+    dispatch(setLoggedInUser(loggedInUser));
+
+    localStorage.setItem(
+      "token",
+      JSON.stringify({
+        accessToken: loginResult.accessToken,
+        refreshToken: loginResult.refreshToken,
+      })
+    );
+  };
 
   // Do login
   const onSubmit = async (formData) => {
-    const loginResult = await scanApi.login(formData.username, formData.password);
+    const loginResult = await scanApi.login(
+      formData.username,
+      formData.password
+    );
     if (loginResult) {
-      const loggedInUser = {
-        username: loginResult.username,
-        email: loginResult.email,
-      };
-      dispatch(setLoggedInUser(loggedInUser));
+      saveLoggedInResult(loginResult);
+    }
+  };
 
-      localStorage.setItem("token", JSON.stringify({
-        accessToken: loginResult.accessToken,
-        refreshToken: loginResult.refreshToken,
-      }));
+  const doWeb3Login = async () => {
+    const { result } = await api.fetch(
+      `/auth/login/${selectedAccount.address}`
+    );
+    if (result?.challenge) {
+      const signature = await signMessage(
+        result?.challenge,
+        selectedAccount.address
+      );
+      const { result: loginResult } = await api.fetch(
+        `/auth/login/${result?.attemptId}`,
+        {},
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ challengeAnswer: signature }),
+        }
+      );
+      if (loginResult) {
+        saveLoggedInResult(loginResult);
+      }
     }
   };
 
   return (
     <CardWrapper>
       <Header>Login</Header>
-      {!web3Login && <Form onSubmit={handleSubmit(onSubmit)}>
+      {!web3Login && (
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Field>
-            <label htmlFor="username">Username
-              { errors.username
-                && <span>
-                    <span>*</span>
-                  </span>
-              }
+            <label htmlFor="username">
+              Username
+              {errors.username && (
+                <span>
+                  <span>*</span>
+                </span>
+              )}
             </label>
-            <FormInput name="username" type="text" ref={register({required: true})} />
+            <FormInput
+              name="username"
+              type="text"
+              ref={register({ required: true })}
+            />
           </Form.Field>
           <Form.Field>
-            <label htmlFor="password">Password
-              { errors.password
-                && <span>
-                    <span>*</span>
-                  </span>
-              }
+            <label htmlFor="password">
+              Password
+              {errors.password && (
+                <span>
+                  <span>*</span>
+                </span>
+              )}
             </label>
-            <FormPasswordWrapper show={showPassword} toggleClick={() => setShowPassword(!showPassword)}>
-              <FormInput name="password" type={showPassword ? "text" : "password"} ref={register({required: true})} />
+            <FormPasswordWrapper
+              show={showPassword}
+              toggleClick={() => setShowPassword(!showPassword)}
+            >
+              <FormInput
+                name="password"
+                type={showPassword ? "text" : "password"}
+                ref={register({ required: true })}
+              />
             </FormPasswordWrapper>
           </Form.Field>
-        <HelperWrapper>
-          <RememberMe onClick={() => setIsRememberMe(!isRememberMe)}>
-            <CheckImage src="/imgs/circle-pass.svg" active={isRememberMe} />
-            <p>Remember me</p>
-          </RememberMe>
-          <Link>
-            <StyledTextMnor>Forgot password?</StyledTextMnor>
-          </Link>
-        </HelperWrapper>
-        <StyledButtonPrimary type="submit" >Login</StyledButtonPrimary>
-        <StyledButton onClick={() => setWeb3Login(true)}>Login with web3 address</StyledButton>
-      </Form>}
-      {web3Login && <div>
-        {hasExtension && <AccountSelector />}
-        {!hasExtension && <DownloadPolkadot />}
-        <StyledButton onClick={() => setWeb3Login(false)}>Login with username</StyledButton>
-      </div>}
+          <HelperWrapper>
+            <RememberMe onClick={() => setIsRememberMe(!isRememberMe)}>
+              <CheckImage src="/imgs/circle-pass.svg" active={isRememberMe} />
+              <p>Remember me</p>
+            </RememberMe>
+            <Link to="?#">
+              <StyledTextMnor>Forgot password?</StyledTextMnor>
+            </Link>
+          </HelperWrapper>
+          <StyledButtonPrimary type="submit">Login</StyledButtonPrimary>
+          <StyledButton onClick={() => setWeb3Login(true)}>
+            Login with web3 address
+          </StyledButton>
+        </Form>
+      )}
+      {web3Login && (
+        <div>
+          {hasExtension && (
+            <AccountSelector
+              accounts={accounts}
+              onSelect={(account) => setSelectedAccount(account)}
+            />
+          )}
+          {!hasExtension && <DownloadPolkadot />}
+          <StyledButtonPrimary onClick={doWeb3Login}>Login</StyledButtonPrimary>
+          <StyledButton onClick={() => setWeb3Login(false)}>
+            Login with username
+          </StyledButton>
+        </div>
+      )}
       <StyledDivider />
       <SignUpWrapper>
         Don't have an account?
