2a564355c5608a12f121027307c7d44bd5eeed30
Yongfeng LI <wliyongfeng@gmail.com>
2021-01-31 20:15:08 +0800

    Fix bounty state by server

--- packages/server/src/features/bounties/bounties.controller.js
+++ packages/server/src/features/bounties/bounties.controller.js
@@ -2,8 +2,19 @@ const { getBountyCollection, getMotionCollection } = require("../../mongo");
 const { extractPage } = require("../../utils");
 const linkService = require("../../services/link.services");
 
-const bountyStatus = (bounty) => bounty.status?.CuratorProposed || bounty.status?.Active || bounty.status?.PendingPayout;
-const bountyStatusName = (bounty) => Object.keys(bounty.status)[0];
+const bountyStatus = (bounty) =>
+  bounty.status?.CuratorProposed ||
+  bounty.status?.Active ||
+  bounty.status?.PendingPayout;
+const bountyStatusName = (meta, stateName) => {
+  if (stateName === "BountyRejected") {
+    return "Rejected";
+  } else if (stateName === "BountyCanceled") {
+    return "Canceled";
+  }
+
+  return Object.keys(meta.status)[0];
+};
 
 class BountiesController {
   async getBounties(ctx) {
@@ -25,7 +36,7 @@ class BountiesController {
     const total = await bountyCol.estimatedDocumentCount();
 
     ctx.body = {
-      items: bounties.map(item => ({
+      items: bounties.map((item) => ({
         bountyIndex: item.bountyIndex,
         proposeTime: item.indexer.blockTime,
         proposeAtBlockHeight: item.indexer.blockHeight,
@@ -36,9 +47,10 @@ class BountiesController {
         title: item.description,
         value: item.meta?.value,
         latestState: {
-          state: bountyStatusName(item.meta) || item.state?.name,
+          state:
+            bountyStatusName(item.meta, item.state?.name) || item.state?.name,
           indexer: item.state?.indexer || item.state?.eventIndexer,
-        }
+        },
       })),
       page,
       pageSize,
@@ -59,7 +71,10 @@ class BountiesController {
     const bounty = await bountyCol.findOne({ bountyIndex });
 
     const motionCol = await getMotionCollection();
-    const bountyMotions = await motionCol.find({ treasuryBountyId: bountyIndex }).sort({ index: 1 }).toArray();
+    const bountyMotions = await motionCol
+      .find({ treasuryBountyId: bountyIndex })
+      .sort({ index: 1 })
+      .toArray();
 
     if (!bounty) {
       ctx.status = 404;
@@ -104,23 +119,29 @@ class BountiesController {
     const bountyIndex = parseInt(ctx.params.bountyIndex);
     const { link, description } = ctx.request.body;
 
-    ctx.body = await linkService.createLink({
-      type: "bounties",
-      indexer: bountyIndex,
-      link,
-      description,
-    }, ctx.request.headers.signature)
+    ctx.body = await linkService.createLink(
+      {
+        type: "bounties",
+        indexer: bountyIndex,
+        link,
+        description,
+      },
+      ctx.request.headers.signature
+    );
   }
 
   async deleteBountyLink(ctx) {
     const bountyIndex = parseInt(ctx.params.bountyIndex);
     const linkIndex = parseInt(ctx.params.linkIndex);
 
-    ctx.body = await linkService.deleteLink({
-      type: "bounties",
-      indexer: bountyIndex,
-      linkIndex,
-    }, ctx.request.headers.signature)
+    ctx.body = await linkService.deleteLink(
+      {
+        type: "bounties",
+        indexer: bountyIndex,
+        linkIndex,
+      },
+      ctx.request.headers.signature
+    );
   }
 }
 
