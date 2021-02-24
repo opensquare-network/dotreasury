bcf4284da393cb3fa05b7867dab013de26d9db1c
Yongfeng LI <wliyongfeng@gmail.com>
2021-02-24 20:37:43 +0800

    Temp fix the break for treasury account balance query

--- packages/scan/src/store/burnt.js
+++ packages/scan/src/store/burnt.js
@@ -6,24 +6,31 @@ const { expandMetadata } = require("@polkadot/metadata");
 
 const ksmMigrateAccountHeight = 1492896;
 
+async function queryAccountFreeWithSystem(blockHash) {
+  const api = await getApi();
+  const account = (
+    await api.query.system.account.at(blockHash, TreasuryAccount)
+  ).toJSON();
+  return account?.data?.free;
+}
+
 async function getTreasuryBalance(blockHash, blockHeight) {
   const api = await getApi();
+  if (blockHeight < 1375086) {
+    const metadata = await api.rpc.state.getMetadata(blockHash);
+    const decorated = expandMetadata(metadata.registry, metadata);
+    const value = await api.rpc.state.getStorage(
+      [decorated.query.balances.freeBalance, TreasuryAccount],
+      blockHash
+    );
 
-  if (blockHeight >= ksmMigrateAccountHeight) {
-    const account = (
-      await api.query.system.account.at(blockHash, TreasuryAccount)
-    ).toJSON();
-    return account?.data?.free;
+    return metadata.registry.createType("Compact<Balance>", value).toJSON();
+  } else if (blockHeight < ksmMigrateAccountHeight) {
+    // TODO: Fix this, account migration has not been applied in this time period, and we need to find the correct way.
+    return await queryAccountFreeWithSystem(blockHash);
+  } else {
+    return await queryAccountFreeWithSystem(blockHash);
   }
-
-  const metadata = await api.rpc.state.getMetadata(blockHash);
-  const decorated = expandMetadata(metadata.registry, metadata);
-  const value = await api.rpc.state.getStorage(
-    [decorated.query.balances.freeBalance, TreasuryAccount],
-    blockHash
-  );
-
-  return metadata.registry.createType("Compact<Balance>", value).toJSON();
 }
 
 async function getBurnPercent(blockHash) {
