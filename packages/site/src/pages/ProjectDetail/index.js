fbeba0a630c653225d155645bb7a814e3b2d54fd
qiyisi <qiyisi@qq.com>
2021-02-26 17:57:24 +0800

    Update project detail

--- packages/site/src/pages/ProjectDetail/index.js
+++ packages/site/src/pages/ProjectDetail/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+import DetailGoBack from "../components/DetailGoBack";
+import RelatedLinks from "../RelatedLinks";
+import Comment from "../Comment";
+import Detail from "./Detail";
+
 const ProjectDetail = () => {
   return (
-    <div>project detail</div>
+    <>
+      <DetailGoBack />
+      <Detail />
+      <RelatedLinks />
+      <Comment />
+    </>
   )
 }
 
