9a43cef4b9121a6790c6c702967eb40d38fd68c1
chaojun <chaojun@opensquare.network>
2021-03-01 11:31:07 +0800

    Refs #482, add divider before big others.

--- packages/site/src/pages/Header/IdentitySlashMenu.js
+++ packages/site/src/pages/Header/IdentitySlashMenu.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import { Label, Menu } from "semantic-ui-react";
 import { useSelector } from "react-redux";
 import { incomeCountSelector } from "../../store/reducers/incomeSlice";
+
+const Divider = styled.div`
+  position: relative;
+  width: 1px;
+  height: 20px;
+  background: #eeeeee;
+  left: 16px;
+`;
 
 function IdentitySlashMenu() {
   const incomeCount = useSelector(incomeCountSelector);
@@ -9,6 +18,7 @@ function IdentitySlashMenu() {
   return (
     <Menu.Item key="IdentitySlash">
       Identity<Label>{incomeCount.identitySlash}</Label>
+      <Divider />
     </Menu.Item>
   );
 }
