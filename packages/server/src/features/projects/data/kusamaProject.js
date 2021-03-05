e05a992817c8688a03d81ed91eb0b35507673749
Yongfeng LI <wliyongfeng@gmail.com>
2021-03-05 21:17:18 +0800

    Add details for kusamaproject.com

--- packages/server/src/features/projects/data/kusamaProject.js
+++ packages/server/src/features/projects/data/kusamaProject.js
@@ -1,13 +1,29 @@
 const id = "kusama-project";
 const name = "Kusama Project";
-const title = "";
-const description = "Kusama Project"
-const logo = "kusama-project-logo.svg"
+const title = "A site for all projects in Kusama ecosystem";
+const description =
+  "KusamaProject provides a directory for people to understand Kusama ecosystem more intuitively. KusamaProject aims to list all projects related to Kusama ecosystem, including tools / wallets / browsers / developers / forums, etc, The project is a website that can be easily accessed from both desktop or mobile.";
+const logo = "kusama-project-logo.svg";
+const startTime = 1588127796000;
+
 const relatedLinks = [
   {
     link: "http://kusamaproject.com/",
-    description: "kusamaproject.com"
-  }
+    description: "Kusamaproject Website",
+  },
+];
+
+const proposals = [
+  {
+    token: "ksm",
+    amount: 200,
+    proposalId: 20,
+    proposeTimePrice: 4.2,
+    title: "Proposal discussion",
+    achievements: [
+      "Polkawallet team list their proposal milestones and have a discussion with councilors",
+    ],
+  },
 ];
 
 module.exports = {
@@ -16,5 +32,7 @@ module.exports = {
   logo,
   title,
   description,
-  relatedLinks
+  relatedLinks,
+  startTime,
+  proposals,
 };
