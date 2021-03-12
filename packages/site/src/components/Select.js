66498f16d26783b8234a1e0db41adf277b3a14e5
888 <songych@163.com>
2021-03-12 21:56:45 +0800

    select style fix

--- packages/site/src/components/Select.js
+++ packages/site/src/components/Select.js
@@ -9,6 +9,21 @@ const CustomSelect = styled(Form.Select)`
     color: rgba(29, 37, 60, 0.24);
     font-size: 13px;
   }
+  .ui.selection.dropdown .menu>.item{
+    padding: 0!important;
+    padding-left: 16px!important;
+    height: 40px;
+    line-height: 40px;
+  }
+  .ui.dropdown.active>.dropdown.icon:before{
+    color: rgba(29, 37, 60, 0.64);
+  }
+  .ui.selection.dropdown{
+    padding-left: 16px!important;
+  }
+  .ui.selection.active.dropdown, .ui.selection.active.dropdown .menu{
+    border-color: #e1e1e1;
+  }
 `
 
 export default CustomSelect;
