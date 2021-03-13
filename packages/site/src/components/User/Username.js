6ea9bf780c6c69aed2ae09a70e68199e0e1bfe6a
qiyisi <qiyisi@qq.com>
2021-03-13 10:09:00 +0800

    Fix address error

--- packages/site/src/components/User/Username.js
+++ packages/site/src/components/User/Username.js
@@ -1,10 +1,10 @@
-import React from "react";
-import styled from "styled-components";
-import {Popup} from "semantic-ui-react";
+import React from 'react'
+import styled from 'styled-components'
+import { Popup } from 'semantic-ui-react'
 
-import TextMinor from "../TextMinor";
-import { TEXT_DARK_MAJOR } from "../../constants";
-import { useDisablePopup } from "../../utils/hooks";
+import TextMinor from '../TextMinor'
+import { TEXT_DARK_MAJOR } from '../../constants'
+import { useDisablePopup } from '../../utils/hooks'
 
 const TextUsername = styled(TextMinor)`
   white-space: nowrap;
@@ -14,23 +14,40 @@ const TextUsername = styled(TextMinor)`
     color: ${TEXT_DARK_MAJOR};
     text-decoration-line: underline;
   }
-`;
+`
 
 const Username = ({ address, name, ellipsis, popup }) => {
-  const disabledPopup = useDisablePopup();
-  let displayAddress = address;
-  if (ellipsis && address) {
-    displayAddress = `${address.substring(0, 6)}...${address.substring(address.length - 6, address.length)}`
+  const disabledPopup = useDisablePopup()
+  let displayAddress
+  if (typeof address === 'string') {
+    if (ellipsis) {
+      displayAddress = `${address.substring(0, 6)}...${address.substring(
+        address.length - 6,
+        address.length
+      )}`
+    } else {
+      displayAddress = address
+    }
+  } else if (typeof address === 'object') {
+    if (ellipsis) {
+      displayAddress = `${address.id.substring(0, 6)}...${address.id.substring(
+        address.id.length - 6,
+        address.id.length
+      )}`
+    } else {
+      displayAddress = address.id
+    }
   }
-  const displayName = name ? name : displayAddress;
+
+  const displayName = name ? name : displayAddress
   return (
     <Popup
       content={name ? `${name} ${address}` : address}
-      size='mini'
+      size="mini"
       disabled={!popup || disabledPopup}
       trigger={<TextUsername>{displayName}</TextUsername>}
     />
-  );
-};
+  )
+}
 
-export default Username;
+export default Username
