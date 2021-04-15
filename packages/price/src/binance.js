d285a04fc9d7783fc62411fef62373e3c7f43782
chaojun <chaojun@opensquare.network>
2021-04-15 18:46:26 +0800

    refs #577, implement price tracker

--- packages/price/src/binance.js
+++ packages/price/src/binance.js
@@ -1,9 +1,9 @@
 const fetch = require("node-fetch");
 const AbortController = require("abort-controller");
 
-async function getKlines(startTime = 0) {
+async function getKlines(symbol, startTime = 0) {
   const url = new URL("api/v3/klines", "https://api.binance.com");
-  url.searchParams.set("symbol", "DOTUSDT");
+  url.searchParams.set("symbol", `${symbol}USDT`);
   url.searchParams.set("interval", "1m");
   url.searchParams.set("limit", "500");
   url.searchParams.set("startTime", `${startTime}`);
@@ -11,12 +11,12 @@ async function getKlines(startTime = 0) {
   const controller = new AbortController();
   const timeout = setTimeout(() => {
     controller.abort();
-  }, 1500);
+  }, 3000);
 
   try {
     const res = await fetch(url, { signal: controller.signal });
     const json = await res.json();
-    console.log(json);
+    return json;
   } catch (error) {
     if (error.name === "AbortError") {
       console.log("request was aborted");
@@ -24,10 +24,10 @@ async function getKlines(startTime = 0) {
   } finally {
     clearTimeout(timeout);
   }
+
+  return [];
 }
 
-async function tick() {}
-
-(async () => {
-  await getKlines();
-})();
+module.exports = {
+  getKlines,
+};
--- packages/price/src/index.js
+++ packages/price/src/index.js
@@ -1,6 +1,113 @@
-async function main() {
-  // TODO: run the ticker
+const minimist = require("minimist");
+const dayjs = require("dayjs");
+const { getKlines } = require("./binance");
+const { getKsmUsdtCollection, getDotUsdtCollection } = require("./mongo");
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function getCollection(symbol) {
+  if (symbol === "DOT") {
+    return getDotUsdtCollection();
+  } else if (symbol === "KSM") {
+    return getKsmUsdtCollection();
+  } else {
+    throw new Error("Unsupport symbol " + symbol);
+  }
 }
 
-// FIXME: log the error
+async function saveKlines(col, klines) {
+  if (klines && klines.length > 0) {
+    const bulk = col.initializeUnorderedBulkOp();
+    for (const item of klines) {
+      const [
+        openTime,
+        open,
+        high,
+        low,
+        close,
+        volume,
+        closeTime,
+        quoteAssetVolume,
+        numberOfTrades,
+        takerBuyBaseAssetVolume,
+        takerBuyQuoteAssetVolume,
+      ] = item;
+
+      bulk.insert({
+        openTime,
+        open,
+        high,
+        low,
+        close,
+        volume,
+        closeTime,
+        quoteAssetVolume,
+        numberOfTrades,
+        takerBuyBaseAssetVolume,
+        takerBuyQuoteAssetVolume,
+      });
+    }
+
+    await bulk.execute();
+  }
+}
+
+async function tick(symbol) {
+  const col = await getCollection(symbol);
+
+  const latestItem = (
+    await col.find({}).sort({ openTime: -1 }).limit(1).toArray()
+  )[0];
+  let klines = null;
+  if (latestItem) {
+    const nextStartTime = latestItem.closeTime + 1;
+    klines = await getKlines(symbol, nextStartTime);
+  } else {
+    klines = await getKlines(symbol);
+  }
+
+  await saveKlines(col, klines);
+
+  return klines?.[klines.length - 1]?.[0] || latestItem?.openTime;
+}
+
+async function main() {
+  const args = minimist(process.argv.slice(2));
+
+  if (!args.symbol) {
+    console.log("Must specify symbol with argument --symbol=[DOT|KSM]");
+    return;
+  }
+
+  if (!["DOT", "KSM"].includes(args.symbol)) {
+    console.log(`Unknown symbol "${args.symbol}"`);
+    return;
+  }
+
+  while (true) {
+    let latestOpenTime = null;
+
+    try {
+      latestOpenTime = await tick(args.symbol);
+      console.log(
+        `${args.symbol} price saved: ${dayjs(latestOpenTime).format(
+          "YYYY-MM-DD HH:mm:ss"
+        )}`
+      );
+    } catch (e) {
+      console.error(e.message);
+    }
+
+    // Reduce the call rate when the latest open time is very close to the current time,
+    if (
+      latestOpenTime &&
+      dayjs(latestOpenTime).add(5, "m").toDate().getTime() > Date.now()
+    ) {
+      await sleep(60 * 1000);
+    } else {
+      await sleep(2 * 1000);
+    }
+  }
+}
+
 main().catch(console.error);
