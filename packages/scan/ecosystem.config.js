ce16149a48f2fbe5f1794a2b5d71a30d602e2b75
Yongfeng LI <wliyongfeng@gmail.com>
2021-04-30 17:23:46 +0800

    Improve scan package scripts

--- packages/scan/ecosystem.config.js
+++ packages/scan/ecosystem.config.js
@@ -32,61 +32,105 @@ const ksmScanConfig = {
   ...getEnvConfig("kusama"),
 };
 
+const calcPriceCommonPart = {
+  script: "src/calcprice/index.js",
+  cron_restart: "* */5 * * *",
+  autorestart: false,
+  log_date_format: "YYYY-MM-DD HH:mm Z",
+  env: {
+    NODE_ENV: "development",
+  },
+  env_production: {
+    NODE_ENV: "production",
+  },
+}
+
+function getCalcPriceEnvConfig(chainName) {
+  return {
+    env: {
+      ...calcPriceCommonPart.env,
+      CHAIN: chainName,
+    },
+    env_production: {
+      ...calcPriceCommonPart.env_production,
+      CHAIN: chainName,
+    },
+  };
+}
+
+const dotCalcPriceConfig = {
+  ...calcPriceCommonPart,
+  ...getCalcPriceEnvConfig("polkadot")
+}
+
+const ksmCalcPriceConfig = {
+  ...calcPriceCommonPart,
+  ...getCalcPriceEnvConfig("kusama")
+}
+
 module.exports = {
   apps: [
+    // prod-scan
     {
-      name: "doTreasury-scan",
-      ...commonPart,
-    },
-    {
-      name: "doTreasury-scan-staging",
-      ...commonPart,
-    },
-    {
-      name: "dot-scan",
+      name: "dot-scan-prod",
       ...dotScanConfig,
     },
+    {
+      name: "ksm-scan-prod",
+      ...ksmScanConfig,
+    },
+
+    // staging-scan
     {
       name: "dot-scan-staging",
       ...dotScanConfig,
     },
     {
-      name: "ksm-scan",
-      ...ksmScanConfig,
-    },
-    {
       name: "ksm-scan-staging",
       ...ksmScanConfig,
     },
+
+    // dev-scan
     {
-      name: "ksm-calc-price",
-      script: "src/calcprice/index.js",
-      cron_restart: "* */5 * * *",
-      autorestart: false,
-      log_date_format: "YYYY-MM-DD HH:mm Z",
-      env: {
-        NODE_ENV: "development",
-        CHAIN: "kusama",
-      },
-      env_production: {
-        NODE_ENV: "production",
-        CHAIN: "kusama",
-      },
+      name: "dot-scan-dev",
+      ...dotScanConfig,
     },
     {
-      name: "dot-calc-price",
-      script: "src/calcprice/index.js",
-      cron_restart: "* */5 * * *",
-      autorestart: false,
-      log_date_format: "YYYY-MM-DD HH:mm Z",
-      env: {
-        NODE_ENV: "development",
-        CHAIN: "polkadot",
-      },
-      env_production: {
-        NODE_ENV: "production",
-        CHAIN: "polkadot",
-      },
+      name: "ksm-scan-dev",
+      ...ksmScanConfig,
+    },
+
+    /**
+     * *****************  calc price  ******************
+     */
+    // prod-calc-price
+    {
+      name: "dot-calc-price-prod",
+      ...dotCalcPriceConfig,
+    },
+    {
+      name: "ksm-calc-price-prod",
+      ...ksmCalcPriceConfig,
+    },
+
+    // staging-calc-price
+    {
+      name: "dot-calc-price-staging",
+      ...dotCalcPriceConfig,
+    },
+    {
+      name: "ksm-calc-price-staging",
+      ...ksmCalcPriceConfig,
+    },
+
+    // dev-calc-price
+    {
+      name: "dot-calc-price-dev",
+      ...dotCalcPriceConfig,
+    },
+    {
+      name: "ksm-calc-price-dev",
+      ...ksmCalcPriceConfig,
     },
   ],
 };
