a4e0544becb68740066aadc15a666b0b916ca05c
qiyisi <qiyisi@qq.com>
2021-05-21 10:51:34 +0800

    Fix jest (#622)

--- packages/scan/src/test/utils/bounty.test.js
+++ packages/scan/src/test/utils/bounty.test.js
@@ -3,21 +3,26 @@ jest.setTimeout(30000);
 const { getBountyMeta, getBountyDescription } = require("../../utils/bounty");
 
 describe("test utils bounty", () => {
-  const TEST_BOUNTY_BLOCK_HASH = "0x6a258eedadb6ddeee7bf5e930ef6a3f859e9339d5dbe09405271e64375e9a726";
+  const TEST_BOUNTY_BLOCK_HASH =
+    "0x6a258eedadb6ddeee7bf5e930ef6a3f859e9339d5dbe09405271e64375e9a726";
   const TEST_BOUNTY_INDEX = 0;
 
   test("get bounty meta", async () => {
-    expect(await getBountyMeta(TEST_BOUNTY_BLOCK_HASH, TEST_BOUNTY_INDEX)).toEqual({
-      proposer: 'GLVeryFRbg5hEKvQZcAnLvXZEXhiYaBjzSDwrXBXrfPF7wj',
+    expect(
+      await getBountyMeta(TEST_BOUNTY_BLOCK_HASH, TEST_BOUNTY_INDEX)
+    ).toEqual({
+      proposer: "GLVeryFRbg5hEKvQZcAnLvXZEXhiYaBjzSDwrXBXrfPF7wj",
       value: 1165000000000000,
       fee: 0,
       curatorDeposit: 0,
       bond: 206666666650,
-      status: { Proposed: null }
+      status: { proposed: null },
     });
   });
 
   test("get bounty description", async () => {
-    expect(await getBountyDescription(TEST_BOUNTY_BLOCK_HASH, TEST_BOUNTY_INDEX)).toBe("Kusama network UI Bounty");
+    expect(
+      await getBountyDescription(TEST_BOUNTY_BLOCK_HASH, TEST_BOUNTY_INDEX)
+    ).toBe("Kusama network UI Bounty");
   });
 });
