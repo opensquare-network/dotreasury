2c48e577e9a33e6899e6a962740b8f3f52b97605
qiyisi <qiyisi@qq.com>
2021-07-05 15:44:27 +0800

    Fix

--- packages/site/src/utils/useUpdateNodesDelay.js
+++ packages/site/src/utils/useUpdateNodesDelay.js
@@ -42,7 +42,7 @@ const useUpdateNodesDelay = () => {
   useEffect(() => {
     const updateNodeDelay = async (url) => {
       try {
-        const api = await getApi(url);
+        const api = await getApi(chain, url);
         const delay = await testNet(api);
         return delay;
       } catch {
