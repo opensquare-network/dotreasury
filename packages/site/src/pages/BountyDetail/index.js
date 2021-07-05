53036c1641fcb12496ac4732dc88e9c7f0e5a7c4
Yongfeng LI <wliyongfeng@gmail.com>
2021-07-05 14:47:06 +0800

    Fix bounty proposeCurator arg

--- packages/site/src/pages/BountyDetail/index.js
+++ packages/site/src/pages/BountyDetail/index.js
@@ -94,7 +94,7 @@ function processTimeline(bountyDetail, scanHeight, symbol) {
                       title: stringToWords(key),
                       value: (() => {
                         if (key === "curator") {
-                          return <User address={val.id} />;
+                          return <User address={val.id || val} />;
                         } else if (key === "fee") {
                           return <Balance value={val} currency={symbol} />;
                         } else {
