436b22dbe3be0c54b21395039e141580b25f237e
qiyisi <qiyisi@qq.com>
2021-08-12 14:25:18 +0800

    Ref #681, update fetch identity

---
+++ packages/site/src/services/identity.js
@@ -0,0 +1,73 @@
+import debounce from "lodash/debounce";
+import axios from "axios";
+
+const cachedIdentities = new Map();
+let pendingQueries = new Map();
+
+const delayQuery = debounce(() => {
+  const pending = pendingQueries;
+  if (pending.size < 1) {
+    return;
+  }
+  pendingQueries = new Map();
+
+  const chainAddresses = {};
+  const idNames = [...pending.keys()];
+  const idNameSplits = idNames.map((item) => item.split("/"));
+  for (const [chain, address] of idNameSplits) {
+    if (!chainAddresses[chain]) {
+      chainAddresses[chain] = [];
+    }
+    chainAddresses[chain].push(address);
+  }
+
+  for (const chain in chainAddresses) {
+    const addresses = chainAddresses[chain];
+
+    axios
+      .post(
+        `${process.env.REACT_APP_IDENTITY_SERVER_HOST}/${chain}/identities`,
+        { addresses }
+      )
+      .then(({ data }) => {
+        const identities = new Map(data.map((item) => [item.address, item]));
+
+        for (const [idName, [, resolve, reject]] of pending) {
+          const [chainOfIdName, addrOfIdName] = idName.split("/");
+          if (chainOfIdName !== chain) {
+            continue;
+          }
+          const identity = identities.get(addrOfIdName) || null;
+          if (identity) {
+            cachedIdentities.set(idName, identity);
+          }
+          if (resolve) {
+            resolve(identity);
+          }
+        }
+      });
+  }
+}, 0);
+
+export function fetchIdentity(chain, address) {
+  const idName = `${chain}/${address}`;
+  if (cachedIdentities.has(idName)) {
+    return Promise.resolve(cachedIdentities.get(idName));
+  }
+
+  const pending = pendingQueries;
+
+  if (!pending.has(idName)) {
+    pending.set(idName, [
+      new Promise((resolve, reject) =>
+        setTimeout(() => {
+          const promise = pending.get(idName);
+          promise.push(resolve, reject);
+          delayQuery();
+        }, 0)
+      ),
+    ]);
+  }
+
+  return pending.get(idName)[0];
+}
--- packages/site/src/utils/hooks.js
+++ packages/site/src/utils/hooks.js
@@ -2,7 +2,7 @@ import { useLayoutEffect, useState, useEffect, useRef } from "react";
 import { useHistory, useLocation, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getIndentity } from "../services/chainApi";
+import { fetchIdentity as getIndentity } from "../services/identity";
 import { setShowMenuTabs } from "../store/reducers/menuSlice";
 import {
   chainSelector,
@@ -39,6 +39,7 @@ export const useIndentity = (address, map) => {
         identity = await getIndentity(chain, address);
         displayCache.set(`identity_${address}`, identity);
       }
+      identity = identity?.info;
       if (isMounted && identity && identity.display) {
         const judgements = identity.judgements.filter(
           ([, judgement]) => !judgement.isFreePaid
