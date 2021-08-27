068bae590f52f774efea0468c56c7e25901a90b6
chaojun <chaojun@opensquare.network>
2021-08-27 11:47:35 +0800

    Refs #679, Implement burnt chart data API

--- packages/server/src/features/burnt/routes.js
+++ packages/server/src/features/burnt/routes.js
@@ -5,5 +5,6 @@ const router = new Router();
 
 router.get("/burnt", burntController.getBurntList);
 router.get("/burnt/count", burntController.getBurntCount);
+router.get("/burnt/chart", burntController.getBurntDataForChart);
 
 module.exports = router;
