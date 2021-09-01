9caf0b060b25a2d663a38cdad387c8551fdb5a3a
chaojun <chaojun@opensquare.network>
2021-09-01 15:21:09 +0800

    Add api to get overview data

--- packages/server/src/features/stats/routes.js
+++ packages/server/src/features/stats/routes.js
@@ -4,5 +4,6 @@ const statsController = require("./stats.controller");
 const router = new Router();
 
 router.get("/stats/weekly", statsController.getWeeklyStatsHistory);
+router.get("/stats/overview", statsController.getOverviewData);
 
 module.exports = router;
--- packages/server/src/features/stats/stats.controller.js
+++ packages/server/src/features/stats/stats.controller.js
@@ -1,4 +1,6 @@
+const { bnToBn } = require("@polkadot/util");
 const { getWeeklyStatsCollection } = require("../../mongo");
+const { getOverview } = require("../../websocket/store");
 
 class StatsController {
   async getWeeklyStatsHistory(ctx) {
@@ -6,6 +8,31 @@ class StatsController {
     const statsCol = await getWeeklyStatsCollection(chain);
     const statsHistory = await statsCol.find({}).toArray();
     ctx.body = statsHistory;
+  }
+
+  async getOverviewData(ctx) {
+    const { chain } = ctx.params;
+    const overview = getOverview(chain);
+
+    const income = overview.income;
+    income.total = bnToBn(income.inflation ?? 0)
+      .add(bnToBn(income.slash ?? 0))
+      .add(bnToBn(income.transfer ?? 0))
+      .add(bnToBn(income.others ?? 0))
+      .toString();
+
+    const output = overview.output;
+    output.total = bnToBn(output.proposal ?? 0)
+      .add(bnToBn(output.tip ?? 0))
+      .add(bnToBn(output.bounty ?? 0))
+      .add(bnToBn(output.burnt ?? 0))
+      .add(bnToBn(output.transfer ?? 0))
+      .toString();
+
+    ctx.body = {
+      income,
+      output,
+    };
   }
 }
 
