c3ffb5c3eac8f038964116efa82bcb8e60d69cc7
qiyisi <qiyisi@qq.com>
2021-09-17 11:23:51 +0800

    Update identity icon (#711)

--- packages/site/src/components/User/Badge.js
+++ packages/site/src/components/User/Badge.js
@@ -9,14 +9,20 @@ const Wrapper = styled.div`
   }
 `;
 
-const Badge = ({ isDisplay, icon }) => {
-  if (!isDisplay) return null;
+const statusIconMap = new Map([
+  ["NOT_VERIFIED", "error-grey"],
+  ["VERIFIED", "auth"],
+  ["ERRONEOUS", "error"],
+  ["VERIFIED_LINKED", "sub"],
+  ["NOT_VERIFIED_LINKED", "sub-grey"],
+  ["ERRONEOUS_LINKED", "sub-red"],
+]);
+
+const Badge = ({ status }) => {
+  if (status === "NO_ID") return null;
+  const icon = statusIconMap.get(status);
   const imgSrc = `/imgs/badge-icons/${icon}.svg`;
-  return (
-    <Wrapper>
-      <Image src={imgSrc} />
-    </Wrapper>
-  );
+  return <Wrapper>{icon && <Image src={imgSrc} />}</Wrapper>;
 };
 
 export default Badge;
