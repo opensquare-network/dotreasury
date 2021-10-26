22b52dae2fe648f6951f466ed74822a71e26055c
Yongfeng LI <wliyongfeng@gmail.com>
2021-10-26 18:07:54 +0800

    Re-order statescan proposal order on project page

--- packages/server/src/features/projects/data/kusama/statescan.js
+++ packages/server/src/features/projects/data/kusama/statescan.js
@@ -20,16 +20,6 @@ const relatedLinks = [
 const proposals = [
   {
     token: "ksm",
-    amount: 69.52,
-    proposalId: 115,
-    proposeTimePrice: 347.2,
-    title:
-      "Statescan NFT business implementation and features polish",
-    achievements: [
-    ],
-  },
-  {
-    token: "ksm",
     amount: 79.48,
     proposalId: 103,
     proposeTimePrice: 194.4,
@@ -41,6 +31,16 @@ const proposals = [
       'Cross chain asset teleports',
       'Make pages SSR',
       'Unfinalized business handling',
+    ],
+  },
+  {
+    token: "ksm",
+    amount: 69.52,
+    proposalId: 115,
+    proposeTimePrice: 347.2,
+    title:
+      "Statescan NFT business implementation and features polish",
+    achievements: [
     ],
   },
 ];
