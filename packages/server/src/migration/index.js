73d67a311c4152b10d67627e1d21d00c7c08fbf6
chaojun <chaojun@opensquare.network>
2021-11-02 13:59:21 +0800

    Refs #737, update migration script

--- packages/server/src/migration/index.js
+++ packages/server/src/migration/index.js
@@ -1,9 +1,9 @@
 const { getProposalCollection } = require("../mongo");
-const { getLinkCollection } = require("../mongo-admin");
+const { getLinkCollection, getDescriptionCollection } = require("../mongo-admin");
 
-const migrate = async (chain) => {
+const migrateLink = async (chain) => {
   try {
-    console.log(`${chain} migration start`);
+    console.log(`${chain} link migration start`);
     const proposalCol = await getProposalCollection(chain);
     const linkCol = await getLinkCollection();
     const proposalLinks = await linkCol
@@ -24,14 +24,45 @@ const migrate = async (chain) => {
         );
       })
     );
-    console.log(`${chain} migration success`);
+    console.log(`${chain} link migration success`);
   } catch (err) {
-    console.log(`${chain} migration error`, err);
+    console.log(`${chain} link migration error`, err);
+  }
+};
+
+const migrateDescription = async (chain) => {
+  try {
+    console.log(`${chain} description migration start`);
+    const proposalCol = await getProposalCollection(chain);
+    const descriptionCol = await getDescriptionCollection();
+    const descriptions = await descriptionCol
+      .find({
+        "indexer.chain": chain,
+        "indexer.type": "proposal",
+      })
+      .toArray();
+    await Promise.all(
+      descriptions.map(async (item) => {
+        await proposalCol.updateOne(
+          {
+            proposalIndex: item.indexer.index,
+          },
+          {
+            $set: { description: item.description },
+          }
+        );
+      })
+    );
+    console.log(`${chain} description migration success`);
+  } catch (err) {
+    console.log(`${chain} description migration error`, err);
   }
 };
 
 (async () => {
-  await migrate("kusama");
-  await migrate("polkadot");
+  await migrateLink("kusama");
+  await migrateLink("polkadot");
+  await migrateDescription("kusama");
+  await migrateDescription("polkadot");
   process.exit(0);
 })();
