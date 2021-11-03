e274917b9082f6c36587ca36b0aafe2461abda0e
chaojun <chaojun@opensquare.network>
2021-11-03 09:41:34 +0800

    Simplify stat point checking

--- packages/input-scan/src/stats/index.js
+++ packages/input-scan/src/stats/index.js
@@ -1,39 +1,13 @@
 const { bnToBn } = require("@polkadot/util");
-const { getWeeklyStatsCollection, getStatusCollection } = require("../mongo/data");
+const { getWeeklyStatsCollection } = require("../mongo/data");
 const { getNowIncomeSeats } = require("../mongo/scanHeight");
 const { getTreasuryBalanceV2 } = require("../utils/freeBalance");
 
-const lastStatsHeight = "last-incomestats-height";
-
 const weeklyBlocks = 3600*24*7 / 6;
 
-async function getNextStatHeight() {
-  const statusCol = await getStatusCollection();
-  const heightInfo = await statusCol.findOne({ name: lastStatsHeight });
-
-  if (!heightInfo) {
-    return weeklyBlocks;
-  } else if (typeof heightInfo.value === "number") {
-    return heightInfo.value + weeklyBlocks;
-  } else {
-    console.error("Stat height value error in DB!");
-    process.exit(1);
-  }
-}
-
-async function updateStatHeight(height) {
-  const statusCol = await getStatusCollection();
-  await statusCol.findOneAndUpdate(
-    { name: lastStatsHeight },
-    { $set: { value: height } },
-    { upsert: true }
-  );
-}
-
 async function tryCreateStatPoint(indexer) {
-  const nextStatHeight = await getNextStatHeight();
 
-  if (indexer.blockHeight !== nextStatHeight) {
+  if (indexer.blockHeight % weeklyBlocks !== 0) {
     return;
   }
 
@@ -52,8 +26,6 @@ async function tryCreateStatPoint(indexer) {
     },
     { upsert: true }
   );
-
-  await updateStatHeight(indexer.blockHeight)
 }
 
 module.exports = {
