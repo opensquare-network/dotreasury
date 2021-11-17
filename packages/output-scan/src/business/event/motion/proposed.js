1b06cdad445f57bf823f0f88cff9bd371c2e0678
Yongfeng LI <wliyongfeng@gmail.com>
2021-11-17 18:56:22 +0800

    Remove the not necessary business data in motion

--- packages/output-scan/src/business/event/motion/proposed.js
+++ packages/output-scan/src/business/event/motion/proposed.js
@@ -3,46 +3,9 @@ const { insertMotion } = require("../../../mongo/service/motion");
 const {
   TimelineItemTypes,
   CouncilEvents,
-  TreasuryProposalMethods,
-  Modules,
-  BountyMethods,
 } = require("../../common/constants");
 const { getVotingFromStorage } = require("../../common/motion/votingStorage");
 const { getMotionCall, getMotionProposalCall } = require("../../common/motion/proposalStorage");
-
-function isBountyMotion(section, method) {
-  return [Modules.Treasury, Modules.Bounties].includes(section) && [
-    BountyMethods.approveBounty,
-    BountyMethods.proposeCurator,
-    BountyMethods.unassignCurator,
-    BountyMethods.closeBounty,
-  ].includes(method)
-}
-
-function extractBusinessFields(proposal = {}) {
-  const { section, method, args } = proposal;
-  if (
-    Modules.Treasury === section &&
-    [
-      TreasuryProposalMethods.approveProposal,
-      TreasuryProposalMethods.rejectProposal,
-    ].includes(method)
-  ) {
-    return {
-      isTreasuryProposal: true,
-      treasuryProposalIndex: args[0].value,
-    };
-  }
-
-  if (isBountyMotion(section, method)) {
-    return {
-      isTreasuryBounty: true,
-      treasuryBountyId: args[0].value,
-    }
-  }
-
-  return {};
-}
 
 async function handleProposed(event, extrinsic, indexer, blockEvents) {
   const eventData = event.data.toJSON();
@@ -76,7 +39,6 @@ async function handleProposed(event, extrinsic, indexer, blockEvents) {
     proposer,
     index: motionIndex,
     threshold,
-    ...extractBusinessFields(proposalCall),
     proposal: proposalCall,
     voting,
     isFinal: false,
