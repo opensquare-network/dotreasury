15a1b302ef9a0fb79ab6b06e208862238d892793
yoshiyuki <brother-zhou@qq.com>
2021-12-27 16:23:21 +0800

    refs #738

---
+++ packages/site/src/components/Tag.js
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+import { Tertiary_Blue_500, Tertiary_Green_500, TEXT_DARK_ACCESSORY } from "../constants";
+
+const Text = styled.span`
+  font-family: "Inter", serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 18px;
+  ${(props) => {
+    return (
+            props &&
+            props.color &&
+            css`
+              color: ${props.color};
+            `
+    );
+  }}
+  margin: 0;
+`;
+
+export default function Tag({text}) {
+  let color = TEXT_DARK_ACCESSORY;
+  if (text === "Working") {
+    color = Tertiary_Green_500;
+  }
+  if (text === "Review") {
+    color = Tertiary_Blue_500;
+  }
+  return <Text color={color}>{text}</Text>;
+}
