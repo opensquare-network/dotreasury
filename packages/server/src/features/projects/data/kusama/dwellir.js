ff661b6a4fef786a93de5fc48782614cfcaeefbf
yoshiyuki <brother-zhou@qq.com>
2022-01-05 16:16:59 +0800

    correct dwellir project

--- packages/server/src/features/projects/data/kusama/dwellir.js
+++ packages/server/src/features/projects/data/kusama/dwellir.js
@@ -2,7 +2,7 @@ const id = "Dwellir";
 const name = "Dwellir";
 const description = "Dwellir is an infrastructure provider for the decentralized web. We provide in the crypto space validator services for networks such as Ethereum, Kusama, Polkadot.";
 const logo = "Dwellir-logo.svg";
-const startTime = 1591848372015;
+const startTime = 1631277348000;
 
 const relatedLinks = [
   {
@@ -16,10 +16,18 @@ const proposals = [
     token: "ksm",
     amount: 136.22,
     proposalId: 125,
-    proposeTimePrice: 475.49,
-    title: "Proposal: Update 1, RPC-node Dwellir",
+    proposeTimePrice: 290.64,
+    title: "Update 1, RPC-node Dwellir",
     achievements: [
-
+    ],
+  },
+  {
+    token: "ksm",
+    amount: 36.1,
+    proposalId: 109,
+    proposeTimePrice: 396.45,
+    title: "Kusama RPC-node Dwellir",
+    achievements: [
     ],
   },
 ];
