d942893cd192c352a10316d15042b7cdffedd600
qiyisi <qiyisi@qq.com>
2022-01-11 11:20:26 +0800

    Fix build

--- packages/site/config-overrides.js
+++ packages/site/config-overrides.js
@@ -4,6 +4,11 @@ module.exports = function override(webpackConfig) {
     include: /node_modules/,
     type: "javascript/auto",
   });
+  webpackConfig.module.rules.push({
+    test: /\.js$/,
+    include: /node_modules/,
+    loader: require.resolve("@open-wc/webpack-import-meta-loader"),
+  });
 
   return webpackConfig;
 };
