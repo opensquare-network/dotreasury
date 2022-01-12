7789ec713f52a90ee52f8c0bd2f64bf955711475
yoshiyuki <brother-zhou@qq.com>
2022-01-12 12:02:19 +0800

    bugfix: show projects count while directly access to the detail page #785

--- packages/site/src/pages/Header/ProjectsMenu.js
+++ packages/site/src/pages/Header/ProjectsMenu.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Label, Menu } from "semantic-ui-react";
-import { useSelector } from "react-redux";
-import { projectsSelector } from "../../store/reducers/projectSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProjects, projectsSelector } from "../../store/reducers/projectSlice";
+import { chainSelector } from "../../store/reducers/chainSlice";
 
 function ProjectsMenu() {
   const projects = useSelector(projectsSelector);
+  const dispatch = useDispatch();
+  const chain = useSelector(chainSelector);
+
+  useEffect(()=>{
+    if(projects?.items?.length === 0){
+      dispatch(fetchProjects(chain,  0, 1));
+    }
+  }, [chain, dispatch, projects])
 
   return (
     <Menu.Item key="Projects">
