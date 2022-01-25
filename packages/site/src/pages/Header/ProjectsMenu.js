287ab270d9b59fdbc6d4194db0b0d488192a130f
Yongfeng LI <wliyongfeng@gmail.com>
2022-01-25 22:52:17 +0800

    Remove projects redundant query

--- packages/site/src/pages/Header/ProjectsMenu.js
+++ packages/site/src/pages/Header/ProjectsMenu.js
@@ -1,24 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Label, Menu } from "semantic-ui-react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProjects, projectsSelector } from "../../store/reducers/projectSlice";
-import { chainSelector } from "../../store/reducers/chainSlice";
+import { useSelector } from "react-redux";
+import { projectsSelector } from "../../store/reducers/projectSlice";
 
 function ProjectsMenu() {
   const projects = useSelector(projectsSelector);
-  const dispatch = useDispatch();
-  const chain = useSelector(chainSelector);
-
-  useEffect(()=>{
-    if(projects?.items?.length === 0){
-      dispatch(fetchProjects(chain,  0, 1));
-    }
-  }, [chain, dispatch, projects])
 
   return (
-    <Menu.Item key="Projects">
-      Projects<Label>{projects?.total ?? 0}</Label>
-    </Menu.Item>
+    <Menu.Item key="Projects" >
+      Projects<Label >{ projects?.total ?? 0 }</Label >
+    </Menu.Item >
   );
 }
 
