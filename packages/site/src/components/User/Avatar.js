a8611441c50603e16ce747447680e2b2ae230607
yoshiyuki <brother-zhou@qq.com>
2022-02-23 15:06:58 +0800

    dependency: upgrade to react-scripts5 & patch legacy packages

--- packages/site/src/components/User/Avatar.js
+++ packages/site/src/components/User/Avatar.js
@@ -1,8 +1,8 @@
 import * as React from "react";
-import Identicon from "@polkadot/react-identicon";
+// import Identicon from "@polkadot/react-identicon";
 
 export default function Avatar({ address, size = 24 }) {
   const theme = "polkadot";
-
-  return <Identicon value={address} size={size} theme={theme} />;
+return theme;
+  // return <Identicon value={address} size={size} theme={theme} />;
 }
