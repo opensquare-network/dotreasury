f8f6fea0f67d28526b261c187a19d1b7df3ae3cd
yoshiyuki <brother-zhou@qq.com>
2022-02-23 15:11:27 +0800

    dependency: @polkadot/react-identicon=> @osn/polkadot-react-identicon

--- packages/site/src/components/User/Avatar.js
+++ packages/site/src/components/User/Avatar.js
@@ -1,8 +1,7 @@
 import * as React from "react";
-// import Identicon from "@polkadot/react-identicon";
+import Identicon from "@osn/polkadot-react-identicon";
 
 export default function Avatar({ address, size = 24 }) {
   const theme = "polkadot";
-return theme;
-  // return <Identicon value={address} size={size} theme={theme} />;
+  return <Identicon value={address} size={size} theme={theme} />;
 }
