a6085018af218e9b4bc1daac7a05e353b69f9d44
Yongfeng LI <wliyongfeng@gmail.com>
2022-04-27 19:06:16 +0800

    Revert polkassembly proposals on project page

--- packages/server/src/features/projects/data/common/polkassembly.js
+++ packages/server/src/features/projects/data/common/polkassembly.js
@@ -20,11 +20,20 @@ const relatedLinks = [
 const proposals = [
   {
     token: "dot",
-    amount: 1849,
-    proposalId: 53,
-    proposeTimePrice: 23.78,
+    amount: 4690,
+    proposalId: 99,
+    proposeTimePrice: 17.12,
     title:
-      "Cover costs for 07.2021 - 09.2021 of the social contract between Polkassembly and Polkadot/Kusama Treasury",
+      "Polkassembly Product Improvement",
+    achievements: [],
+  },
+  {
+    token: "dot",
+    amount: 1581.67,
+    proposalId: 83,
+    proposeTimePrice: 24.14,
+    title:
+      "Polkassembly Social Contract Q1 2022",
     achievements: [],
   },
   {
@@ -38,20 +47,11 @@ const proposals = [
   },
   {
     token: "dot",
-    amount: 1581.67,
-    proposalId: 83,
-    proposeTimePrice: 24.14,
+    amount: 1849,
+    proposalId: 53,
+    proposeTimePrice: 23.78,
     title:
-      "Polkassembly Social Contract Q1 2022",
-    achievements: [],
-  },
-  {
-    token: "dot",
-    amount: 4690,
-    proposalId: 99,
-    proposeTimePrice: 17.12,
-    title:
-      "Polkassembly Product Improvement",
+      "Cover costs for 07.2021 - 09.2021 of the social contract between Polkassembly and Polkadot/Kusama Treasury",
     achievements: [],
   },
 ];
