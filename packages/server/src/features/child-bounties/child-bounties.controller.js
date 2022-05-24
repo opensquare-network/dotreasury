2971ac04cd3d62a1612011d7d2ca6b028e180366
Yongfeng LI <wliyongfeng@gmail.com>
2022-05-24 11:38:57 +0800

    Add apis for child bounty list, #825 (#833)
    * Add api to get child bounty list, #825
    
    * Add api to get child bounties of one parent bounty, #825

--- packages/server/src/features/child-bounties/child-bounties.controller.js
+++ packages/server/src/features/child-bounties/child-bounties.controller.js
@@ -1,4 +1,36 @@
+const { extractPage } = require("../../utils");
 const { getChildBountyCollection } = require("../../mongo");
+
+async function queryChildBounties(ctx, chain, q = {}) {
+  const { page, pageSize } = extractPage(ctx);
+  if (pageSize === 0 || page < 0) {
+    ctx.status = 400;
+    return;
+  }
+
+  const col = await getChildBountyCollection(chain);
+  const bounties = await col
+    .find(q, {
+      projection: {
+        _id: 0,
+        timeline: 0,
+      }
+    })
+    .sort({
+      "indexer.blockHeight": -1,
+    })
+    .skip(page * pageSize)
+    .limit(pageSize)
+    .toArray();
+  const total = await col.estimatedDocumentCount();
+
+  return {
+    items: bounties,
+    page,
+    pageSize,
+    total,
+  }
+}
 
 class ChildBountiesController {
   async getChildBounty(ctx) {
@@ -16,6 +48,24 @@ class ChildBountiesController {
     ctx.body = {
       ...bounty
     }
+  }
+
+  async getBountiesByParent(ctx) {
+    const { chain, bountyIndex } = ctx.params;
+    const parentBountyId = parseInt(bountyIndex);
+    if (isNaN(parentBountyId)) {
+      ctx.status = 400;
+      return;
+    }
+
+    ctx.body = await queryChildBounties(ctx, chain, {
+      parentBountyId: parseInt(bountyIndex),
+    })
+  }
+
+  async getBounties(ctx) {
+    const { chain } = ctx.params;
+    ctx.body = await queryChildBounties(ctx, chain, {});
   }
 }
 
