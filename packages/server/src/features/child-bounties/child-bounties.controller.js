1de73144d5d9f2056040d9bf5343e2a76b9f1c79
Yongfeng LI <wliyongfeng@gmail.com>
2022-05-24 11:09:23 +0800

    Add api to get child bounties of one parent bounty, #825

--- packages/server/src/features/child-bounties/child-bounties.controller.js
+++ packages/server/src/features/child-bounties/child-bounties.controller.js
@@ -1,6 +1,37 @@
 const { extractPage } = require("../../utils");
 const { getChildBountyCollection } = require("../../mongo");
 
+async function queryChildBounties(ctx, chain, q = {}) {
+  const { page, pageSize } = extractPage(ctx);
+  if (pageSize === 0 || page < 0) {
+    ctx.status = 400;
+    return;
+  }
+
+  const col = await getChildBountyCollection(chain);
+  const bounties = await col
+    .find(q, {
+      projection: {
+        _id: 0,
+        timeline: 0,
+      }
+    })
+    .sort({
+      "indexer.blockHeight": -1,
+    })
+    .skip(page * pageSize)
+    .limit(pageSize)
+    .toArray();
+  const total = await col.estimatedDocumentCount();
+
+  return {
+    items: bounties,
+    page,
+    pageSize,
+    total,
+  }
+}
+
 class ChildBountiesController {
   async getChildBounty(ctx) {
     const { chain } = ctx.params;
@@ -19,36 +50,22 @@ class ChildBountiesController {
     }
   }
 
-  async getBounties(ctx) {
-    const { chain } = ctx.params;
-    const { page, pageSize } = extractPage(ctx);
-    if (pageSize === 0 || page < 0) {
+  async getBountiesByParent(ctx) {
+    const { chain, bountyIndex } = ctx.params;
+    const parentBountyId = parseInt(bountyIndex);
+    if (isNaN(parentBountyId)) {
       ctx.status = 400;
       return;
     }
 
-    const col = await getChildBountyCollection(chain);
-    const bounties = await col
-      .find({}, {
-        projection: {
-          _id: 0,
-          timeline: 0,
-        }
-      })
-      .sort({
-        "indexer.blockHeight": -1,
-      })
-      .skip(page * pageSize)
-      .limit(pageSize)
-      .toArray();
-    const total = await col.estimatedDocumentCount();
+    ctx.body = await queryChildBounties(ctx, chain, {
+      parentBountyId: parseInt(bountyIndex),
+    })
+  }
 
-    ctx.body = {
-      items: bounties,
-      page,
-      pageSize,
-      total,
-    }
+  async getBounties(ctx) {
+    const { chain } = ctx.params;
+    ctx.body = await queryChildBounties(ctx, chain, {});
   }
 }
 
