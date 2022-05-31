4f99705d67828f51f31c9c136552e93aaa9c5a82
2nthony <ant.hu@outlook.com>
2022-05-31 15:18:35 +0800

    refactor(site): bounties table

--- packages/site/src/pages/Bounties/BountiesTable.js
+++ packages/site/src/pages/Bounties/BountiesTable.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { NavLink, useHistory } from "react-router-dom";
 import dayjs from "dayjs";
 
-import Table from "../../components/Table";
+import { Table } from "../../components/Table";
 import TableLoading from "../../components/TableLoading";
 import User from "../../components/User";
 import Balance from "../../components/Balance";
@@ -38,16 +38,6 @@ const TableWrapper = styled.div`
   overflow: scroll;
 `;
 
-const StyledTable = styled(Table)`
-  .short-padding {
-    padding-top: 4px !important;
-    padding-bottom: 4px !important;
-  }
-  .no-data {
-    height: 120px !important;
-  }
-`;
-
 const ProposeTimeWrapper = styled.div`
   display: flex;
   align-items: flex-start;
@@ -77,27 +67,6 @@ const CapText = styled(Text)`
   white-space: nowrap;
 `;
 
-const TableRow = styled(Table.Row)`
-  height: 50px;
-
-  &.child {
-    background-color: rgba(250, 250, 250, 1);
-  }
-`;
-
-const ExpandToggleButton = styled.button`
-  border: 1px solid rgba(204, 204, 204, 1);
-  background-color: transparent;
-  border-radius: 4px;
-  cursor: pointer;
-  padding: 0;
-  width: 20px;
-  height: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 const HeaderWrapper = styled.div`
   padding: 20px 24px;
   display: flex;
@@ -118,102 +87,91 @@ const getStateWithVotingAyes = (item) => {
   return state;
 };
 
-function TableExpandableRow({
-  type = "",
-  item = {},
-  isChild = false,
-  symbol = "",
-}) {
+const BountiesTable = ({ data, loading, header, footer }) => {
+  const symbol = useSelector(chainSymbolSelector);
   const history = useHistory();
-  const [expanded, setExpanded] = useState(false);
 
-  const detailRoute = `/${symbol.toLowerCase()}/${type}/${item.bountyIndex}`;
-  const onClickRow = () => {
+  const getDetailRoute = (row) => {
+    const type = row.parentBountyId >= 0 ? "child-bounties" : "bounties";
+    return `/${symbol.toLowerCase()}/${type}/${row.bountyIndex}`;
+  };
+  const onRowClick = (row) => {
     if (window.innerWidth < 1140) {
+      const detailRoute = getDetailRoute(row);
       history.push(detailRoute);
     }
   };
 
-  useEffect(() => {
-    setExpanded(false);
-  }, [item, setExpanded]);
-
-  return (
-    <>
-      <TableRow className={isChild ? "child" : ""} onClick={onClickRow}>
-        <Table.Cell className="">
-          {!!item.childBounties?.length && (
-            <ExpandToggleButton
-              onClick={(event) => {
-                event.stopPropagation();
-                setExpanded(!expanded);
-              }}
-            >
-              <img
-                src={`/imgs/${expanded ? "subtract" : "add"}.svg`}
-                alt="toggle"
-              />
-            </ExpandToggleButton>
-          )}
-        </Table.Cell>
-        <Table.Cell className="index-cell">
-          <TextMinor>{`#${item.bountyIndex}`}</TextMinor>
-        </Table.Cell>
-        <Table.Cell className="propose-time-cell">
-          <ProposeTimeWrapper>
-            <TextMinor>
-              {dayjs(parseInt(item.proposeTime)).format("YYYY-MM-DD HH:mm:ss")}
-            </TextMinor>
-            <ExplorerLink href={`/block/${item.proposeAtBlockHeight}`}>
-              <PolygonLabel fontSize={12} value={item.proposeAtBlockHeight} />
-            </ExplorerLink>
-          </ProposeTimeWrapper>
-        </Table.Cell>
-        <Table.Cell className="user-cell">
-          {item.curator ? <User address={item.curator} /> : "--"}
-        </Table.Cell>
-        <Table.Cell className="title-cell">
-          <Text>{item.title}</Text>
-        </Table.Cell>
-        <Table.Cell className="balance-cell" textAlign={"right"}>
-          <Balance
-            value={item.value}
-            currency={symbol}
-            usdt={item.symbolPrice}
-          />
-        </Table.Cell>
-        <Table.Cell textAlign={"right"}>
-          <CapText>{getStateWithVotingAyes(item)}</CapText>
-        </Table.Cell>
-        <Table.Cell className="link-cell hidden">
-          <NavLink to={detailRoute}>
+  const columns = [
+    {
+      key: "index",
+      title: "Index",
+      dataIndex: "bountyIndex",
+      cellClassName: "index-cell",
+      cellRender: (value) => <TextMinor>#{value}</TextMinor>,
+    },
+    {
+      key: "proposeTime",
+      title: "Propose Time",
+      cellClassName: "propose-time-cell",
+      cellRender: (_, item) => (
+        <ProposeTimeWrapper>
+          <TextMinor>
+            {dayjs(parseInt(item.proposeTime)).format("YYYY-MM-DD HH:mm:ss")}
+          </TextMinor>
+          <ExplorerLink href={`/block/${item.proposeAtBlockHeight}`}>
+            <PolygonLabel fontSize={12} value={item.proposeAtBlockHeight} />
+          </ExplorerLink>
+        </ProposeTimeWrapper>
+      ),
+    },
+    {
+      key: "curator",
+      title: "Curator",
+      dataIndex: "curator",
+      cellClassName: "user-cell",
+      cellRender: (_, item) =>
+        item.curator ? <User address={item.curator} /> : "--",
+    },
+    {
+      key: "title",
+      title: "Title",
+      dataIndex: "title",
+      cellClassName: "title-cell",
+    },
+    {
+      key: "value",
+      title: "Value",
+      headerCellProps: { textAlign: "right" },
+      cellProps: { textAlign: "right" },
+      cellClassName: "balance-cell",
+      cellRender: (_, item) => (
+        <Balance value={item.value} currency={symbol} usdt={item.symbolPrice} />
+      ),
+    },
+    {
+      key: "status",
+      title: "Status",
+      headerCellProps: { textAlign: "right" },
+      cellProps: { textAlign: "right" },
+      cellClassName: "status-cell",
+      cellRender: (_, item) => (
+        <CapText>{getStateWithVotingAyes(item)}</CapText>
+      ),
+    },
+    {
+      key: "navigation",
+      title: "",
+      cellClassName: "link-cell hidden",
+      cellRender: (_, item) => {
+        return (
+          <NavLink to={getDetailRoute(item)}>
             <RightButton />
           </NavLink>
-        </Table.Cell>
-      </TableRow>
-
-      {/* child bounties */}
-      {expanded &&
-        item.childBounties?.map((childItem, childIndex) => {
-          const item = compatChildBountyData(childItem);
-
-          return (
-            <TableExpandableRow
-              type="child-bounties"
-              key={childIndex}
-              item={item}
-              symbol={symbol}
-              isChild
-            />
-          );
-        })}
-    </>
-  );
-}
-
-const BountiesTable = ({ data, loading, header, footer, rowProps = {} }) => {
-  const { isChild = false } = rowProps;
-  const symbol = useSelector(chainSymbolSelector);
+        );
+      },
+    },
+  ];
 
   return (
     <CardWrapper>
@@ -221,35 +179,16 @@ const BountiesTable = ({ data, loading, header, footer, rowProps = {} }) => {
       <Wrapper>
         <TableWrapper>
           <TableLoading loading={loading}>
-            <StyledTable unstackable>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell />
-                  <Table.HeaderCell>Index</Table.HeaderCell>
-                  <Table.HeaderCell>Propose Time</Table.HeaderCell>
-                  <Table.HeaderCell>Curator</Table.HeaderCell>
-                  <Table.HeaderCell>Title</Table.HeaderCell>
-                  <Table.HeaderCell textAlign={"right"}>Value</Table.HeaderCell>
-                  <Table.HeaderCell textAlign={"right"}>
-                    Status
-                  </Table.HeaderCell>
-                  <Table.HeaderCell className="hidden" />
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {(data &&
-                  data.length > 0 &&
-                  data.map((item, index) => (
-                    <TableExpandableRow
-                      isChild={isChild}
-                      type="bounties"
-                      key={index}
-                      item={item}
-                      symbol={symbol}
-                    />
-                  ))) || <TableNoDataCell />}
-              </Table.Body>
-            </StyledTable>
+            {(data && data.length > 0 && (
+              <Table
+                tree
+                treeKey="childBounties"
+                treeDataTransform={compatChildBountyData}
+                columns={columns}
+                data={data}
+                onRowClick={onRowClick}
+              />
+            )) || <TableNoDataCell />}
           </TableLoading>
         </TableWrapper>
       </Wrapper>
