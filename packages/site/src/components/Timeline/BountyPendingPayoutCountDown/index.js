28baf37a872824d4aa14fbc336d7cc43a07c8067
2nthony <ant.hu@outlook.com>
2022-08-29 16:41:47 +0800

    refactor: use estimate time countdown

--- packages/site/src/components/Timeline/BountyPendingPayoutCountDown/index.js
+++ packages/site/src/components/Timeline/BountyPendingPayoutCountDown/index.js
@@ -1,15 +1,4 @@
-import CountDown from "../../../components/OsnCountDown";
-import { useSelector } from "react-redux";
-import { scanHeightSelector } from "../../../store/reducers/chainSlice";
-import {
-  EstimateTime,
-  Gap,
-  Flex,
-  TooltipDiffHeight,
-  TooltipInfoHeight,
-} from "./styled";
-import { useMemo } from "react";
-import { useEstimateTime } from "../../../utils/useEstimateTime";
+import EstimateTimeCountDown from "../../EstimateTimeCountdown";
 
 export default function BountyPendingPayoutCountDown({ bountyDetail }) {
   const { timeline = [] } = bountyDetail ?? {};
@@ -18,38 +7,10 @@ export default function BountyPendingPayoutCountDown({ bountyDetail }) {
   const unlockAt = awardTimeline?.args?.unlockAt;
   const awardBlockHeight = awardTimeline?.indexer?.blockHeight;
 
-  const scanHeight = useSelector(scanHeightSelector);
-
-  const { estimatedTimeString } = useEstimateTime(awardBlockHeight);
-
-  const diffHeight = useMemo(() => {
-    if (scanHeight > unlockAt) {
-      return 0;
-    }
-
-    return unlockAt - scanHeight;
-  }, [scanHeight, unlockAt]);
-
   return (
-    <Flex>
-      <CountDown
-        numerator={scanHeight}
-        denominator={unlockAt}
-        tooltipContent={
-          <div>
-            <TooltipDiffHeight>{diffHeight}</TooltipDiffHeight>
-            <TooltipInfoHeight>
-              {scanHeight} / {unlockAt}
-            </TooltipInfoHeight>
-          </div>
-        }
-      />
-
-      <Gap />
-
-      <EstimateTime>
-        {awardBlockHeight > scanHeight ? estimatedTimeString : 0}
-      </EstimateTime>
-    </Flex>
+    <EstimateTimeCountDown
+      blockHeight={awardBlockHeight}
+      denominator={unlockAt}
+    />
   );
 }
