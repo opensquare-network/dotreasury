344c65ee24c972d37700bf713112b8ccaddb722c
2nthony <ant.hu@outlook.com>
2022-09-06 14:46:35 +0800

    feat: text styles

---
+++ packages/site/src/styles/text.js
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+export const p_14_medium = css`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const p_14_normal = css`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+`;
