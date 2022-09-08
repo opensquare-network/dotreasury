5cb92487374ea28a0c6ef316c4ec2d743e6d51d5
2nthony <ant.hu@outlook.com>
2022-09-08 14:05:10 +0800

    feat: participants 2

---
+++ packages/site/src/pages/Participants/ParticipantsTable.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { Table } from "../../components/Table";
+import TableLoading from "../../components/TableLoading";
+import Card from "../../components/Card";
+
+const CardWrapper = styled(Card)`
+  overflow-x: hidden;
+  padding: 0;
+  table {
+    border-radius: 0 !important;
+    border: none !important;
+  }
+  @media screen and (max-width: 600px) {
+    border-radius: 0;
+  }
+`;
+
+const Wrapper = styled.div`
+  overflow: hidden;
+`;
+
+const TableWrapper = styled.div`
+  overflow: scroll;
+
+  .proposal-beneficiary-header,
+  .curator-header {
+    cursor: pointer !important;
+    color: rgba(0, 0, 0, 0.65) !important;
+  }
+`;
+
+const HeaderWrapper = styled.div`
+  padding: 20px 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default function ParticipantsTable({ data, loading, header, footer }) {
+  return (
+    <CardWrapper>
+      {header && <HeaderWrapper>{header}</HeaderWrapper>}
+
+      <Wrapper>
+        <TableWrapper>
+          <TableLoading loading={loading}>{data && <Table />}</TableLoading>
+        </TableWrapper>
+      </Wrapper>
+
+      {footer}
+    </CardWrapper>
+  );
+}
--- packages/site/src/pages/Participants/index.js
+++ packages/site/src/pages/Participants/index.js
@@ -1,3 +1,78 @@
+import styled from "styled-components";
+import ParticipantsTable from "./ParticipantsTable";
+import { h4_16_semibold } from "../../styles/text";
+import ResponsivePagination from "../../components/ResponsivePagination";
+import { DEFAULT_PAGE_SIZE, DEFAULT_QUERY_PAGE } from "../../constants";
+import { useChainRoute, useQuery, useLocalStorage } from "../../utils/hooks";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { chainSelector } from "../../store/reducers/chainSlice";
+import {
+  fetchParticipants,
+  loadingSelector,
+  participantsSelector,
+} from "../../store/reducers/participantsSlice";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+const Title = styled.h4`
+  ${h4_16_semibold};
+`;
+
 export default function Participants() {
-  return <div>participants</div>;
+  useChainRoute();
+
+  const searchPage = parseInt(useQuery().get("page"));
+  const queryPage =
+    searchPage && !isNaN(searchPage) && searchPage > 0
+      ? searchPage
+      : DEFAULT_QUERY_PAGE;
+  const [tablePage, setTablePage] = useState(queryPage);
+  const [pageSize, setPageSize] = useLocalStorage(
+    "projectsPageSize",
+    DEFAULT_PAGE_SIZE
+  );
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { items: tableData, total } = useSelector(participantsSelector);
+  const loading = useSelector(loadingSelector);
+  const chain = useSelector(chainSelector);
+
+  const totalPages = Math.ceil(total / pageSize);
+
+  useEffect(() => {
+    dispatch(fetchParticipants(chain, tablePage - 1, pageSize));
+  }, [dispatch, chain, tablePage, pageSize]);
+
+  return (
+    <ParticipantsTable
+      data={tableData}
+      loading={loading}
+      header={<Title>Participants</Title>}
+      footer={
+        <ResponsivePagination
+          activePage={tablePage}
+          totalPages={totalPages}
+          pageSize={pageSize}
+          setPageSize={(size) => {
+            setTablePage(DEFAULT_QUERY_PAGE);
+            setPageSize(size);
+            history.push({
+              search: null,
+            });
+          }}
+          onPageChange={(_, { activePage }) => {
+            history.push({
+              search:
+                activePage === DEFAULT_QUERY_PAGE
+                  ? null
+                  : `?page=${activePage}`,
+            });
+            setTablePage(activePage);
+          }}
+        />
+      }
+    />
+  );
 }
