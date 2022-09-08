395d425b597157129fe38d0a22d91cc46cf989ee
2nthony <ant.hu@outlook.com>
2022-09-08 15:23:35 +0800

    feat: new tag component

---
+++ packages/site/src/components/Tag/Tag.js
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+import { p_12_medium } from "../../styles/text";
+import { TEXT_DARK_MAJOR, Greyscale_Grey_100 } from "../../constants";
+
+const TagWrapper = styled.span`
+  ${p_12_medium};
+  color: ${TEXT_DARK_MAJOR};
+  background-color: ${Greyscale_Grey_100};
+  padding: 2px 12px;
+  border-radius: 4px;
+  ${(p) =>
+    p.rounded &&
+    css`
+      border-radius: 9999px;
+    `}
+`;
+
+/**
+ * @description Tag for categorizing or markup.
+ */
+export default function Tag({ children, rounded }) {
+  return <TagWrapper rounded={rounded}>{children}</TagWrapper>;
+}
