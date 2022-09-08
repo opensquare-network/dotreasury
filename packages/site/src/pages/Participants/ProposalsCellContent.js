3d680df7f8073a73b8e52e36bcffa32c8a100ad5
2nthony <ant.hu@outlook.com>
2022-09-08 16:25:17 +0800

    feat: proposals cell content

---
+++ packages/site/src/pages/Participants/ProposalsCellContent.js
@@ -0,0 +1,54 @@
+import { Image } from "semantic-ui-react";
+import styled from "styled-components";
+import { TEXT_DARK_ACCESSORY, TEXT_DARK_MAJOR } from "../../constants";
+import { p_14_normal } from "../../styles/text";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${p_14_normal};
+`;
+
+const PlusWrapper = styled.span`
+  margin: 0 4px;
+  color: ${TEXT_DARK_ACCESSORY};
+  &:first-child {
+    display: none;
+  }
+  &:last-child {
+    display: none;
+  }
+`;
+
+const SymbolWrapper = styled.span`
+  color: ${TEXT_DARK_MAJOR};
+  margin-right: 4px;
+`;
+
+export default function ProposalsCellContent({ data }) {
+  const { proposals, bounties, tips } = data ?? {};
+
+  const symbols = [];
+  const makeSymbol = (count, symbolIconUrl) => {
+    return (
+      <>
+        <SymbolWrapper>{count}</SymbolWrapper>
+        <Image src={symbolIconUrl} />
+        <PlusWrapper>+</PlusWrapper>
+      </>
+    );
+  };
+
+  if (proposals) {
+    symbols.push(makeSymbol(proposals, "/imgs/symbol-proposals.svg"));
+  }
+  if (bounties) {
+    symbols.push(makeSymbol(bounties, "/imgs/symbol-bounties.svg"));
+  }
+  if (tips) {
+    symbols.push(makeSymbol(tips, "/imgs/symbol-tips.svg"));
+  }
+
+  return <Wrapper>{symbols}</Wrapper>;
+}
