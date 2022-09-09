f46f2663a0b34449759e94add165791fd2fb1053
2nthony <ant.hu@outlook.com>
2022-09-09 17:31:04 +0800

    feat: page users detail 2

--- packages/site/src/pages/UsersDetail/index.js
+++ packages/site/src/pages/UsersDetail/index.js
@@ -1,5 +1,16 @@
 // page `/:symbol/users/:address`
+import { useParams } from "react-router";
+import DetailGoBack from "../components/DetailGoBack";
+import UserInfo from "./UserInfo";
 
 export default function UsersDetail() {
-  return <div>page users detail</div>;
+  const { address } = useParams();
+
+  return (
+    <div>
+      <DetailGoBack />
+
+      <UserInfo />
+    </div>
+  );
 }
