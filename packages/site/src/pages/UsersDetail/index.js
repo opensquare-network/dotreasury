5d08bba2945ea183395cf93833f86709236b6da8
2nthony <ant.hu@outlook.com>
2022-09-09 18:30:50 +0800

    feat: compose user info

--- packages/site/src/pages/UsersDetail/index.js
+++ packages/site/src/pages/UsersDetail/index.js
@@ -1,13 +1,13 @@
 // page `/:symbol/users/:address`
-import { useParams } from "react-router";
 import DetailGoBack from "../components/DetailGoBack";
+import UserInfo from "./UserInfo";
 
 export default function UsersDetail() {
-  const { address } = useParams();
-
   return (
     <div>
       <DetailGoBack />
+
+      <UserInfo />
     </div>
   );
 }
