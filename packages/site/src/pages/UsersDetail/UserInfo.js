986b433daccc9b497f49ea3157c7aa871a5bd565
2nthony <ant.hu@outlook.com>
2022-09-09 20:20:25 +0800

    feat: user counts

--- packages/site/src/pages/UsersDetail/UserInfo.js
+++ packages/site/src/pages/UsersDetail/UserInfo.js
@@ -5,10 +5,28 @@ import Badge from "../../components/User/Badge";
 import Tag from "../../components/Tag/Tag";
 import { useIdentity } from "../../utils/hooks";
 import { ellipsis } from "../../utils/ellipsis";
+import ProposalsCount from "../../components/ProposalsCount";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  countsSelector,
+  fetchUsersCounts,
+  resetUsersCounts,
+} from "../../store/reducers/usersDetailSlice";
+import { useEffect } from "react";
+import { chainSelector } from "../../store/reducers/chainSlice";
 
 export default function UserInfo({ role, setRole = () => {} }) {
   const { address } = useParams();
   const { name, badgeData } = useIdentity(address);
+  const dispatch = useDispatch();
+  const counts = useSelector(countsSelector);
+  const chain = useSelector(chainSelector);
+
+  useEffect(() => {
+    dispatch(fetchUsersCounts(chain, address, role));
+
+    return () => dispatch(resetUsersCounts());
+  }, [dispatch, role, address]);
 
   return (
     <InfoCard
@@ -26,6 +44,17 @@ export default function UserInfo({ role, setRole = () => {} }) {
             <Tag rounded>Beneficiary</Tag>
             <Tag rounded>Proposer</Tag>
           </InfoCardExtraItem>
+
+          {/* FIXME: condition to display counts */}
+          {role === "beneficiary" && (
+            <InfoCardExtraItem label="Proposals">
+              <ProposalsCount
+                proposals={counts?.proposalsCount}
+                bounties={counts?.bountiesCount + counts?.childBountiesCount}
+                tips={counts?.tipsCount}
+              />
+            </InfoCardExtraItem>
+          )}
         </>
       }
     />
--- packages/site/src/pages/UsersDetail/index.js
+++ packages/site/src/pages/UsersDetail/index.js
@@ -1,13 +1,16 @@
 // page `/:symbol/users/:address`
+import { useState } from "react";
 import DetailGoBack from "../components/DetailGoBack";
 import UserInfo from "./UserInfo";
 
 export default function UsersDetail() {
+  const [role, setRole] = useState("beneficiary");
+
   return (
     <div>
       <DetailGoBack />
 
-      <UserInfo />
+      <UserInfo role={role} setRole={setRole} />
     </div>
   );
 }
