ca7891f6113d4dfbe01347e78b716ee837446c69
2nthony <ant.hu@outlook.com>
2022-09-16 18:39:52 +0800

    fix: page details reset all table data

--- packages/site/src/pages/UsersDetail/index.js
+++ packages/site/src/pages/UsersDetail/index.js
@@ -5,9 +5,11 @@ import DetailGoBack from "../components/DetailGoBack";
 import UserInfo from "./UserInfo";
 import ProposalsTables from "./ProposalsTables";
 import { useEnsureUsersCount } from "./useEnsureUsersCount";
+import { useResetTableData } from "./useResetTableData";
 
 export default function UsersDetail() {
   useEnsureUsersCount();
+  useResetTableData();
 
   // FIXME: default should be Councilor or from api
   const [role, setRole] = useState(USER_ROLES.Beneficiary);
