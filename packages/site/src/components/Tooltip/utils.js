946b3d9769f6642a81f2631d595829c857246373
2nthony <ant.hu@outlook.com>
2022-09-23 15:21:09 +0800

    refactor: tooltip show tooltip fn

---
+++ packages/site/src/components/Tooltip/utils.js
@@ -0,0 +1,21 @@
+import { createPopper } from "@popperjs/core";
+
+export function showTooltip(target, popper, options) {
+  const { placement = "top", offset = [0, 8] } = options ?? {};
+
+  let instance;
+
+  instance = createPopper(target, popper, {
+    placement,
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset,
+        },
+      },
+    ],
+  });
+
+  return instance;
+}
