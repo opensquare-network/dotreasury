cb3e3e7f0508f7e07b078c82fa59238ad30f4dc4
2nthony <ant.hu@outlook.com>
2022-09-26 19:27:47 +0800

    feat: tip attendance hover tooltip content

--- packages/site/src/pages/UsersDetail/Councilor/TipAttendance.js
+++ packages/site/src/pages/UsersDetail/Councilor/TipAttendance.js
@@ -1,4 +1,7 @@
-import { chainSelector } from "../../../store/reducers/chainSlice";
+import {
+  chainSelector,
+  chainSymbolSelector,
+} from "../../../store/reducers/chainSlice";
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../../components/TableLoading";
@@ -8,11 +11,24 @@ import {
   tipAttendanceSelector,
 } from "../../../store/reducers/usersDetailSlice";
 import { useEffect } from "react";
-import { Card, CardTitle } from "./styled";
+import {
+  Card,
+  CardTitle,
+  TooltipContentDetail,
+  TooltipContentDetailItem,
+  TooltipContentDetailItemLabel,
+  TooltipContentDetailItemValue,
+} from "./styled";
 import AttendanceHeatMap from "../../../components/AttendanceHeatMap";
+import { Link } from "react-router-dom";
+import { makeInSiteTipLink } from "../../../utils/url";
+import { ellipsis } from "../../../utils/ellipsis";
+import Balance from "../../../components/Balance";
+import { toPrecision, getPrecision } from "../../../utils";
 
 export default function TipAttendance() {
   const chain = useSelector(chainSelector);
+  const chainSymbol = useSelector(chainSymbolSelector);
   const dispatch = useDispatch();
   const { address } = useParams();
 
@@ -23,6 +39,11 @@ export default function TipAttendance() {
     dispatch(fetchTipAttendance(chain, address));
   }, [dispatch, chain, address]);
 
+  function parseValue(value) {
+    const precision = toPrecision(value, getPrecision(chainSymbol), false);
+    return Number(precision).toLocaleString();
+  }
+
   return (
     <Loading loading={loading}>
       <Card>
@@ -31,6 +52,44 @@ export default function TipAttendance() {
         <AttendanceHeatMap
           data={compatAttendanceHeatMapData(tips)}
           activeColor="#B0D1F9"
+          showTooltip={(data) => data.type !== "inActive"}
+          tooltipContentRender={(data) => (
+            <TooltipContentDetail gap={35}>
+              <TooltipContentDetailItem>
+                <TooltipContentDetailItemLabel>
+                  Created height
+                </TooltipContentDetailItemLabel>
+                <TooltipContentDetailItemValue>
+                  #{data.meta.tipHeight}
+                </TooltipContentDetailItemValue>
+              </TooltipContentDetailItem>
+              <TooltipContentDetailItem>
+                <TooltipContentDetailItemLabel>
+                  Tip hash
+                </TooltipContentDetailItemLabel>
+                <TooltipContentDetailItemValue>
+                  <Link
+                    to={makeInSiteTipLink(
+                      chainSymbol.toLowerCase(),
+                      "tips",
+                      data.meta.tipHeight,
+                      data.meta.tipHash
+                    )}
+                  >
+                    {ellipsis(data.meta.tipHash)}
+                  </Link>
+                </TooltipContentDetailItemValue>
+              </TooltipContentDetailItem>
+              <TooltipContentDetailItem>
+                <TooltipContentDetailItemLabel>
+                  Value
+                </TooltipContentDetailItemLabel>
+                <TooltipContentDetailItemValue>
+                  {parseValue(data.meta.tips?.[0]?.value)} {chainSymbol}
+                </TooltipContentDetailItemValue>
+              </TooltipContentDetailItem>
+            </TooltipContentDetail>
+          )}
           legendActiveText="Tip"
           legendInactiveText="No tip"
         />
