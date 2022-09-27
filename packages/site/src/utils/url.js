e0c028f467011f796f016189332cb1debbb4d86c
2nthony <ant.hu@outlook.com>
2022-09-27 18:56:24 +0800

    feat: add more to url util

--- packages/site/src/utils/url.js
+++ packages/site/src/utils/url.js
@@ -1,20 +1,63 @@
-export function makeSubsquareLink(chain, category, page, id) {
-  const domain = `https://${chain}.subsquare.io`;
+/**
+ * @description compose subscan link
+ * @example makeSubscanLink("kusama", "user", :address) -> "https://kusama.subscan.io/user/:address"
+ */
+export function makeSubscanLink(chain, ...pathnames) {
+  const hostname = `${chain}.subscan.io`;
 
-  const pathnames = [category, page];
+  return makeUrl({
+    hostname,
+    pathname: makeUrlPathname(...pathnames),
+  });
+}
 
-  if (id) {
-    pathnames.push(id);
-  }
+export function makeSubsquareLink(chain, ...pathnames) {
+  const hostname = `${chain}.subsquare.io`;
 
-  const url = new URL(makeUrlPathname(...pathnames), domain);
-  return url.toString();
+  return makeUrl({
+    hostname,
+    pathname: makeUrlPathname(...pathnames),
+  });
 }
 
 export function makeInSiteTipLink(symbol, page, tipHeight, tipHash) {
   return makeUrlPathname(symbol, page, `${tipHeight}_${tipHash}`);
 }
 
+/**
+ * @description ensure link protocol
+ * @example google.com -> https://google.com
+ */
+export function ensureLinkProtocol(link = "", protocol = "https:") {
+  const isExternal = isExternalLink(link);
+
+  const value = isExternal
+    ? link
+    : {
+        hostname: link,
+        protocol,
+      };
+
+  return makeUrl(value);
+}
+
 function makeUrlPathname(...path) {
   return "/" + path.join("/");
+}
+
+function isExternalLink(link = "") {
+  return /^https?:\/\//.test(link);
+}
+
+/**
+ * @param {URL | string} options
+ */
+function makeUrl(options) {
+  if (typeof options === "string") {
+    return options;
+  }
+
+  const { protocol = "https:", pathname = "/", hostname = "" } = options ?? {};
+
+  return `${protocol}//${hostname}${pathname}`;
 }
