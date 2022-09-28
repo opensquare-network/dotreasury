987771629c9f0f035ea3af8d4964b1f0580a20a2
2nthony <ant.hu@outlook.com>
2022-09-28 17:37:59 +0800

    feat: collection for all api urls

---
+++ packages/site/src/services/urls.js
@@ -0,0 +1,18 @@
+// NOTE: collection for all api urls
+// TODO: move all to here
+import { makeUrlPathname } from "../utils/url";
+
+// page user detail
+// route: /:symbol/users/:address
+export function userDetailProposalCounts(chain, address, role) {
+  return makeUrlPathname(chain, "account", address, role, "counts");
+}
+export function userDetailCouncilorTerms(chain, address) {
+  return makeUrlPathname(chain, "account", address, "councilor", "terms");
+}
+export function userDetailCouncilorMotions(chain, address) {
+  return makeUrlPathname(chain, "account", address, "councilor", "motions");
+}
+export function userDetailCouncilrTips(chain, address) {
+  return makeUrlPathname(chain, "account", address, "councilor", "tippers");
+}
