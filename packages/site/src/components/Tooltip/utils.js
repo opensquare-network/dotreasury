644da1b5ba83ebea45d464a51c1eeb575bfff5b3
2nthony <ant.hu@outlook.com>
2022-09-30 11:45:56 +0800

    fix: tooltip force placement

--- packages/site/src/components/Tooltip/utils.js
+++ packages/site/src/components/Tooltip/utils.js
@@ -3,12 +3,22 @@ import { createPopper } from "@popperjs/core";
 export function showTooltip(target, popper, options) {
   const { placement = "top", offset = [0, 8] } = options ?? {};
 
+  /** @type {import("@popperjs/core").Instance | undefined} */
   let instance;
 
+  if (instance) {
+    instance.destroy();
+  }
+
   instance = createPopper(target, popper, {
-    strategy: "fixed",
     placement,
     modifiers: [
+      {
+        name: "flip",
+        options: {
+          fallbackPlacements: [placement],
+        },
+      },
       {
         name: "offset",
         options: {
