a12abb9ca4897ac593244291924520c5f50cd012
2nthony <ant.hu@outlook.com>
2022-12-07 17:02:50 +0800

    fix: react 18 mount

--- packages/site/src/index.js
+++ packages/site/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "semantic-ui-css/semantic.min.css";
 
@@ -7,15 +7,16 @@ import App from "./App";
 import store from "./store";
 import GlobalStyle from "./GlobalStyle";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.Fragment>
     <GlobalStyle />
     <Provider store={store}>
       {/*<Maintenance />*/}
       <App />
     </Provider>
-  </React.Fragment>,
-  document.getElementById("root")
+  </React.Fragment>
 );
 
 // deploy trigger 25
