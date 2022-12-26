3546cde0823f7af3dbd7a0e6bccbafba16a6d915
Yongfeng LI <wliyongfeng@gmail.com>
2022-12-26 21:42:23 +0800

    Remove useless method, #1074

--- packages/output-scan/src/business/event/referenda/submitted/index.js
+++ packages/output-scan/src/business/event/referenda/submitted/index.js
@@ -12,28 +12,13 @@ function extractProposalHash(proposal) {
   return null;
 }
 
-async function queryPreimageByProposal(proposal, indexer) {
-  let hash;
-  if (proposal?.lookup) {
-    hash = proposal?.lookup?.hash;
-  } else if (proposal?.legacy) {
-    hash = proposal?.legacy?.hash;
-  }
-
-  if (!hash) {
-    return null;
-  }
-
-  return await queryPreimage(hash, indexer.blockHash);
-}
-
 async function handleSubmitted(event, indexer) {
   const referendumIndex = event.data[0].toNumber();
   const track = event.data[1].toNumber();
 
   const proposal = event.data[2].toJSON();
   const proposalHash = extractProposalHash(proposal);
-  const call = await queryPreimageByProposal(proposal, indexer);
+  const call = await queryPreimage(proposalHash, indexer.blockHash);
   await handleTreasurySpend(referendumIndex, track, proposalHash, call, indexer);
 
   // todo: we will also handle other calls by referenda
