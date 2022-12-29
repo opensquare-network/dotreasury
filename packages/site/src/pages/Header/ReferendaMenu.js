3a4a24bb8981d93d3b941021b4e00797f5d62eaa
chaojun <chaojun@opensquare.network>
2022-12-29 11:32:00 +0800

    fix style

--- packages/site/src/pages/Header/ReferendaMenu.js
+++ packages/site/src/pages/Header/ReferendaMenu.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import { Label, Menu } from "semantic-ui-react";
 import { useSelector } from "react-redux";
 import { totalOpenGovApplicationCountSelector } from "../../store/reducers/overviewSlice";
+
+const Divider = styled.div`
+  position: relative;
+  width: 1px;
+  height: 20px;
+  background: #eeeeee;
+  left: 16px;
+`;
 
 function ReferendaMenu() {
   const applicationCount = useSelector(totalOpenGovApplicationCountSelector);
@@ -9,6 +18,7 @@ function ReferendaMenu() {
   return (
     <Menu.Item key="Referenda">
       OpenGov Applications<Label>{applicationCount}</Label>
+      <Divider />
     </Menu.Item>
   );
 }
