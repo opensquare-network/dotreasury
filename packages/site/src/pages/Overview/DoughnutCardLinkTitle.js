a612fc531e3112fa066942d19e5d2a8ef185d9cc
2nthony <ant.hu@outlook.com>
2023-01-16 16:20:58 +0800

    doughnut card link title

---
+++ packages/site/src/pages/Overview/DoughnutCardLinkTitle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import ExternalLink from "../../components/ExternalLink";
+
+const LinkTitle = styled(ExternalLink)`
+  display: inline-flex;
+  align-items: center;
+  color: inherit;
+
+  &:hover {
+    color: inherit;
+    text-decoration: underline;
+  }
+`;
+
+export default function DoughnutCardLinkTitle({ children, href = "" }) {
+  return (
+    <LinkTitle href={href}>
+      {children}
+      <img src="/imgs/caret-up-right.svg" />
+    </LinkTitle>
+  );
+}
