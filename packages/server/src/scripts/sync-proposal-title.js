6b63c086b6accfe97cfb28ec6ad0abacf16be906
hyifeng <chaojun@opensquare.network>
2023-01-31 10:18:44 +0800

    Sync subsquare.io proposal title (#1114)
    * Sync subsquare.io proposal title
    
    * Update
    
    * rename

---
+++ packages/server/src/scripts/sync-proposal-title.js
@@ -0,0 +1,73 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const chunk = require("lodash.chunk");
+const negate = require("lodash.negate");
+const isNil = require("lodash.isnil");
+const { getReferendaReferendumCollection } = require("../mongo");
+const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args));
+
+async function fetchTitle(apiUrl) {
+  try {
+    const resp = await fetch(apiUrl);
+    if (!resp.ok) {
+      return;
+    }
+
+    const data = await resp.json();
+    return data?.title;
+  } catch (e) {
+    return;
+  }
+}
+
+async function syncTitle({ col, fetchTitle, updateTitle }) {
+  const allItems = await col.find({}).toArray();
+
+  // Process in batches that 10 items for each
+  for (const items of chunk(allItems, 10)) {
+    // Fetch titles from subsquare.io
+    const titles = await Promise.all(
+      items.map(item => fetchTitle(item))
+    );
+
+    // Make sure has titles
+    if (titles.filter(negate(isNil)).length === 0) {
+      continue;
+    }
+
+    const bulk = col.initializeUnorderedBulkOp();
+    for (let i = 0; i < items.length; i++) {
+      // Skip nil
+      if (isNil(titles[i])) {
+        continue;
+      }
+
+      updateTitle(bulk, items[i], titles[i]);
+    }
+    await bulk.execute();
+  }
+}
+
+const fetchGov2ReferendaTitle = (chain, referendumIndex) =>
+  fetchTitle(`https://${chain}.subsquare.io/api/gov2/referendums/${referendumIndex}`);
+
+async function syncGov2ReferendaTitle(chain) {
+  await syncTitle({
+    col: await getReferendaReferendumCollection(chain),
+    fetchTitle: (item) => fetchGov2ReferendaTitle(chain, item.referendumIndex),
+    updateTitle: (bulk, item, title) =>
+      bulk
+        .find({ referendumIndex: item.referendumIndex })
+        .updateOne({
+          $set: { description: title }
+        })
+  });
+}
+
+async function main() {
+  console.log(`Sync OpenGov referenda title`);
+  await syncGov2ReferendaTitle("kusama");
+}
+
+module.exports = main;
