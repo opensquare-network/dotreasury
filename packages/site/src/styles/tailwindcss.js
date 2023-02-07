65c878d6ec7f3f4a7260092ddec466870ccd978d
2nthony <ant.hu@outlook.com>
2023-02-07 17:56:37 +0800

    fix: tailwind remove css fn

--- packages/site/src/styles/tailwindcss.js
+++ packages/site/src/styles/tailwindcss.js
@@ -2,8 +2,6 @@
 // https://tailwindcss.com
 // `inline-block` => `inline_block`
 
-import { css } from "styled-components";
-
 // utility functions
 // ---
 
@@ -14,113 +12,113 @@ export function px(n) {
   return `${n}px`;
 }
 export function bg_theme(key) {
-  return css`
+  return `
     background-color: ${theme(key)};
   `;
 }
 export function text_theme(key) {
-  return css`
+  return `
     color: ${theme(key)};
   `;
 }
 export function m(n) {
-  return css`
+  return `
     margin: ${px(n)};
   `;
 }
 export function m_t(n) {
-  return css`
+  return `
     margin-top: ${px(n)};
   `;
 }
 export function m_r(n) {
-  return css`
+  return `
     margin-right: ${px(n)};
   `;
 }
 export function m_b(n) {
-  return css`
+  return `
     margin-bottom: ${px(n)};
   `;
 }
 export function m_l(n) {
-  return css`
+  return `
     margin-left: ${px(n)};
   `;
 }
 export function m_x(n) {
-  return css`
+  return `
     ${m_l(n)};
     ${m_r(n)};
   `;
 }
 export function m_y(n) {
-  return css`
+  return `
     ${m_t(n)};
     ${m_b(n)};
   `;
 }
 export function p(n) {
-  return css`
+  return `
     padding: ${px(n)};
   `;
 }
 export function p_t(n) {
-  return css`
+  return `
     padding-top: ${px(n)};
   `;
 }
 export function p_r(n) {
-  return css`
+  return `
     padding-right: ${px(n)};
   `;
 }
 export function p_b(n) {
-  return css`
+  return `
     padding-bottom: ${px(n)};
   `;
 }
 export function p_l(n) {
-  return css`
+  return `
     padding-left: ${px(n)};
   `;
 }
 export function p_x(n) {
-  return css`
+  return `
     ${p_l(n)};
     ${p_r(n)};
   `;
 }
 export function p_y(n) {
-  return css`
+  return `
     ${p_t(n)};
     ${p_b(n)};
   `;
 }
 export function gap_x(n) {
-  return css`
+  return `
     column-gap: ${px(n)};
   `;
 }
 export function gap_y(n) {
-  return css`
+  return `
     row-gap: ${px(n)};
   `;
 }
 export function gap(n) {
-  return css`
+  return `
     gap: ${px(n)};
   `;
 }
 export function space_x(n) {
-  return css`
+  return `
     & > * + * {
       ${m_l(n)}
     }
   `;
 }
 export function space_y(n) {
-  return css`
+  return `
     & > * + * {
       ${m_t(n)}
     }
@@ -131,20 +129,25 @@ export function space_y(n) {
  * @example hover(text_theme("theme100"))
  */
 export function hover(atom) {
-  return css`
+  return `
     &:hover {
       ${atom}
     }
   `;
 }
 export function w(n) {
-  return css`
+  return `
     width: ${px(n)};
   `;
 }
 export function h(n) {
-  return css`
+  return `
     height: ${px(n)};
+  `;
+}
+export function grid_cols(repeat, min = 0) {
+  return `
+    grid-template-columns: repeat(${repeat}, minmax(${min}px, 1fr));
   `;
 }
 
@@ -152,91 +155,97 @@ export function h(n) {
 // ---
 
 // display
-export const flex = css`
+export const flex = `
   display: flex;
 `;
-export const inline_flex = css`
+export const inline_flex = `
   display: inline-flex;
 `;
-export const block = css`
+export const block = `
   display: block;
 `;
-export const inline_block = css`
+export const inline_block = `
   display: inline-block;
 `;
-export const inline = css`
+export const inline = `
   display: inline;
+`;
+export const grid = `
+  display: grid;
+`;
+export const hidden = `
+  display: none;
 `;
 
 // flex
-export const flex_col = css`
+export const flex_col = `
   flex-direction: column;
 `;
-export const flex_1 = css`
+export const flex_1 = `
   flex: 1 1 0%;
 `;
-export const justify_center = css`
+export const justify_center = `
   justify-content: center;
 `;
-export const justify_between = css`
+export const justify_between = `
   justify-content: space-between;
 `;
-export const justify_start = css`
+export const justify_start = `
   justify-content: flex-start;
 `;
-export const justify_end = css`
+export const justify_end = `
   justify-content: flex-end;
 `;
-export const items_center = css`
+export const items_center = `
   align-items: center;
 `;
-export const items_start = css`
+export const items_start = `
   align-items: flex-start;
 `;
-export const items_end = css`
+export const items_end = `
   align-items: flex-end;
 `;
-export const items_stretch = css`
+export const items_stretch = `
   align-items: stretch;
 `;
-export const flex_wrap = css`
+export const flex_wrap = `
   flex-wrap: wrap;
 `;
-export const flex_nowrap = css`
+export const flex_nowrap = `
   flex-wrap: nowrap;
 `;
 
 // sizing
 // ---
 
-export const w_full = css`
+export const w_full = `
   width: 100%;
 `;
-export const w_screen = css`
+export const w_screen = `
   width: 100vw;
 `;
-export const min_w_full = css`
+export const min_w_full = `
   min-width: 100%;
 `;
-export const max_w_full = css`
+export const max_w_full = `
   max-width: 100%;
 `;
-export const h_full = css`
+export const h_full = `
   height: 100%;
 `;
-export const min_h_full = css`
+export const min_h_full = `
   min-height: 100%;
 `;
-export const max_h_full = css`
+export const max_h_full = `
   max-height: 100%;
 `;
-export const h_screen = css`
+export const h_screen = `
   height: 100vh;
 `;
-export const min_h_screen = css`
+export const min_h_screen = `
   min-height: 100vh;
 `;
-export const max_h_screen = css`
+export const max_h_screen = `
   max-height: 100vh;
 `;
 
@@ -248,7 +257,7 @@ export const p_24 = p(24);
 export const m_0 = m(0);
 export const m_16 = m(16);
 export const m_24 = m(24);
-export const m_x_auto = css`
+export const m_x_auto = `
   margin-left: auto;
   margin-right: auto;
 `;
@@ -264,17 +273,18 @@ export const rounded = make_rounded(2);
 export const rounded_4 = make_rounded(4);
 export const rounded_8 = make_rounded(8);
 export const rounded_full = make_rounded(9999);
+export const rounded_none = make_rounded(0);
 
 // border color
-export const border_theme500 = css`
+export const border_theme500 = `
   border-color: ${theme("theme500")};
 `;
-export const border_theme100 = css`
+export const border_theme100 = `
   border-color: ${theme("theme100")};
 `;
 
 // border style
-export const border_hidden = css`
+export const border_hidden = `
   border-style: hidden;
 `;
 
@@ -282,19 +292,19 @@ export const border_hidden = css`
 // ---
 
 // overflow
-export const overflow_auto = css`
+export const overflow_auto = `
   overflow: auto;
 `;
-export const overflow_hidden = css`
+export const overflow_hidden = `
   overflow: hidden;
 `;
-export const overflow_scroll = css`
+export const overflow_scroll = `
   overflow: scroll;
 `;
-export const overflow_x_scroll = css`
+export const overflow_x_scroll = `
   overflow-x: scroll;
 `;
-export const overflow_y_scroll = css`
+export const overflow_y_scroll = `
   overflow-y: scroll;
 `;
 
@@ -308,18 +318,18 @@ export const z_50 = make_z(50);
 export const z_9999 = make_z(9999);
 
 // position
-export const relative = css`
+export const relative = `
   position: relative;
 `;
-export const absolute = css`
+export const absolute = `
   position: absolute;
 `;
-export const fixed = css`
+export const fixed = `
   position: fixed;
 `;
 
 // top right bottom left
-export const inset_0 = css`
+export const inset_0 = `
   top: 0px;
   right: 0px;
   bottom: 0px;
@@ -330,15 +340,15 @@ export const inset_0 = css`
 // ---
 
 // whitespace
-export const whitespace_nowrap = css`
+export const whitespace_nowrap = `
   white-space: nowrap;
 `;
 
 // text overflow
-export const text_ellipsis = css`
+export const text_ellipsis = `
   text-overflow: ellipsis;
 `;
-export const truncate = css`
+export const truncate = `
   ${overflow_hidden};
   ${whitespace_nowrap};
   ${text_ellipsis};
@@ -355,7 +365,7 @@ export const text_quaternary = text_theme("fontQuaternary");
 // ---
 
 // cursor
-export const cursor_pointer = css`
+export const cursor_pointer = `
   cursor: pointer;
 `;
 
@@ -370,18 +380,18 @@ export const bg_theme500 = bg_theme("theme500");
 // ---
 
 function make_border(n) {
-  return css`
+  return `
     border-width: ${px(n)};
     border-style: solid;
   `;
 }
 function make_rounded(n) {
-  return css`
+  return `
     border-radius: ${px(n)};
   `;
 }
 function make_z(n) {
-  return css`
+  return `
     z-index: ${n};
   `;
 }
