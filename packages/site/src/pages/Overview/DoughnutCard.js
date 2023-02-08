9dc80511e356fb9846528290e6f5c54712288f11
2nthony <ant.hu@outlook.com>
2023-02-08 17:10:07 +0800

    refactor: use base chart card

--- packages/site/src/pages/Overview/DoughnutCard.js
+++ packages/site/src/pages/Overview/DoughnutCard.js
@@ -1,58 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-import Card from "../../components/Card";
 import Doughnut from "../../components/CustomDoughnut";
-import List from "./CustomList";
 import Total from "./Total";
-import Text from "../../components/Text";
 import { abbreviateBigNumber } from "../../utils";
-import {
-  flex,
-  flex_col,
-  items_center,
-  justify_between,
-} from "../../styles/tailwindcss";
-
-const Title = styled(Text)`
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 700;
-`;
-const TitleGroup = styled.div`
-  ${flex};
-  ${items_center};
-  ${justify_between};
-`;
-
-const CardWrapper = styled(Card)`
-  position: relative;
-  padding: 24px;
-  @media screen and (max-width: 600px) {
-    border-radius: 0;
-  }
-`;
-
-const ContentWrapper = styled.div`
-  /* position: relative; */
-  display: flex;
-  ${flex_col};
-  /* padding: 32px; */
-  @media screen and (max-width: 556px) {
-    & > :first-child {
-      margin-bottom: 24px;
-    }
-  }
-`;
+import OverviewBaseChartCard from "./ChartCard";
 
 const CanvasWrapper = styled.div`
-  height: 264px;
   position: relative;
 `;
 
 const DoughnutWrapper = styled.div`
-  width: 214px;
-  height: 214px;
   margin: 0 auto;
   position: absolute;
 `;
@@ -83,13 +41,16 @@ const DoughnutCard = ({
     return acc + (findDisabled(current.name) ? 0 : current.value ?? 0);
   };
   const total = abbreviateBigNumber(data.labels?.reduce(totalReduce, 0));
+
   return (
-    <CardWrapper>
-      <TitleGroup>
-        <Title>{title}</Title>
-        {titleExtra}
-      </TitleGroup>
-      <ContentWrapper>
+    <OverviewBaseChartCard
+      title={title}
+      titleExtra={titleExtra}
+      data={data}
+      status={status}
+      clickEvent={clickEvent}
+      children={children}
+      chart={
         <CanvasWrapper>
           <Total total={total}>
             <DoughnutWrapper>
@@ -97,10 +58,8 @@ const DoughnutCard = ({
             </DoughnutWrapper>
           </Total>
         </CanvasWrapper>
-        <List data={data} status={status} clickEvent={clickEvent}></List>
-        {children}
-      </ContentWrapper>
-    </CardWrapper>
+      }
+    />
   );
 };
 
