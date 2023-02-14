1e779cc5675fec4704bd969f3d28a63393b0ae4c
2nthony <ant.hu@outlook.com>
2023-02-14 14:40:08 +0800

    fix: open gov spend hover data (#1131)
    * fix: open gov spend hover data
    
    * tweak

--- packages/site/src/pages/Overview/OpenGovSpend.js
+++ packages/site/src/pages/Overview/OpenGovSpend.js
@@ -28,49 +28,91 @@ export default function OpenGovSpend() {
     big_spender,
   } = overview.output?.referendaSpent ?? {};
 
+  const treasurerValue = toPrecision(treasurer?.value ?? 0, precision, false);
+  const treasurerFiatValue = treasurer?.fiatValue ?? 0;
+  const smallTipperValue = toPrecision(
+    small_tipper?.value ?? 0,
+    precision,
+    false
+  );
+  const smallTipperFiatValue = small_tipper?.fiatValue ?? 0;
+  const bigTipperValue = toPrecision(big_tipper?.value ?? 0, precision, false);
+  const bigTipperFiatValue = big_tipper?.fiatValue ?? 0;
+  const smallSpendValue = toPrecision(
+    small_spender?.value ?? 0,
+    precision,
+    false
+  );
+  const smallSpendFiatValue = small_spender?.fiatValue ?? 0;
+  const mediumSpendValue = toPrecision(
+    medium_spender?.value ?? 0,
+    precision,
+    false
+  );
+  const mediumSpendFiatValue = medium_spender?.fiatValue ?? 0;
+  const bigSpendValue = toPrecision(big_spender?.value ?? 0, precision, false);
+  const bigSpendFiatValue = big_spender?.fiatValue ?? 0;
+
+  const totalValue = sum([
+    treasurerValue,
+    smallTipperValue,
+    bigTipperValue,
+    smallSpendValue,
+    mediumSpendValue,
+    bigSpendValue,
+  ]);
+  const totalFiatValue = sum([
+    treasurerFiatValue,
+    smallTipperFiatValue,
+    bigTipperFiatValue,
+    smallSpendFiatValue,
+    mediumSpendFiatValue,
+    bigSpendFiatValue,
+  ]);
+
   const chartData = useMemo(() => {
     return {
       icon: "circle",
       labels: [
         {
           name: "Treasurer",
-          value: toPrecision(treasurer?.value ?? 0, precision, false),
-          fiatValue: treasurer?.fiatValue ?? 0,
+          value: treasurerValue,
+          fiatValue: treasurerFiatValue,
           count: treasurer?.count,
           color: OVERVIEW_TREASURER_COLOR,
         },
         {
           name: "Small Tipper",
-          value: toPrecision(small_tipper?.value ?? 0, precision, false),
-          fiatValue: small_tipper?.fiatValue ?? 0,
+          value: smallTipperValue,
+          fiatValue: smallTipperFiatValue,
           count: small_tipper?.count,
           color: OVERVIEW_SMALL_TIPPER_COLOR,
         },
         {
           name: "Big Tipper",
-          value: toPrecision(big_tipper?.value ?? 0, precision, false),
-          fiatValue: big_tipper?.fiatValue ?? 0,
+          value: bigTipperValue,
+          fiatValue: bigTipperFiatValue,
           count: big_tipper?.count,
           color: OVERVIEW_BIG_TIPPER_COLOR,
         },
         {
           name: "Small Spender",
-          value: toPrecision(small_spender?.value ?? 0, precision, false),
-          fiatValue: small_spender?.fiatValue ?? 0,
+          value: smallSpendValue,
+          fiatValue: smallSpendFiatValue,
           count: small_spender?.count,
           color: OVERVIEW_SMALL_SPENDER_COLOR,
         },
         {
           name: "Medium Spender",
-          value: toPrecision(medium_spender?.value ?? 0, precision, false),
-          fiatValue: medium_spender?.fiatValue ?? 0,
+          value: mediumSpendValue,
+          fiatValue: mediumSpendFiatValue,
           count: medium_spender?.count,
           color: OVERVIEW_MEDIUM_SPENDER_COLOR,
         },
         {
           name: "Big Spender",
-          value: toPrecision(big_spender?.value ?? 0, precision, false),
-          fiatValue: big_spender?.fiatValue ?? 0,
+          value: bigSpendValue,
+          fiatValue: bigSpendFiatValue,
           count: big_spender?.count,
           color: OVERVIEW_BIG_SPENDER_COLOR,
         },
@@ -83,7 +125,18 @@ export default function OpenGovSpend() {
     small_spender,
     medium_spender,
     big_spender,
-    precision,
+    treasurerValue,
+    smallTipperValue,
+    bigTipperValue,
+    smallSpendValue,
+    mediumSpendValue,
+    bigSpendValue,
+    treasurerFiatValue,
+    smallTipperFiatValue,
+    bigTipperFiatValue,
+    smallSpendFiatValue,
+    mediumSpendFiatValue,
+    bigSpendFiatValue,
   ]);
 
   const [chartStatus, setChartStatus] = useState({
@@ -115,17 +168,29 @@ export default function OpenGovSpend() {
           data={chartData}
           status={chartStatus}
           tooltipLabelCallback={(tooltipItem) => {
-            const { dataset, raw: currentValue } = tooltipItem;
-            const total = sum(dataset.data);
-            const percentage = parseFloat(
-              ((currentValue / total) * 100).toFixed(2)
+            const { raw: currentValue, label } = tooltipItem;
+            const currentFiatValue = chartData.labels.find(
+              (i) => i.name === label
+            )?.fiatValue;
+            const valuePercentage = parseFloat(
+              ((currentValue / totalValue) * 100).toFixed(2)
+            );
+            const fiatValuePercentage = parseFloat(
+              ((currentFiatValue / totalFiatValue) * 100).toFixed(2)
             );
 
-            return ` ${
+            const token = `${
               Math.round(currentValue) === currentValue ? "" : "≈"
             }${Math.round(
               currentValue
-            ).toLocaleString()} ${symbol} ${percentage}%`;
+            ).toLocaleString()} ${symbol} ${valuePercentage}%`;
+            const fiat = `${
+              Math.round(currentFiatValue) === currentFiatValue ? "" : "≈"
+            }$${Math.round(
+              currentFiatValue
+            ).toLocaleString()} ${fiatValuePercentage}%`;
+
+            return `${token}  ${fiat}`;
           }}
         />
       }
