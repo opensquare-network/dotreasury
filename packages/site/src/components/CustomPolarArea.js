fe3f819a94eac8ef37531ed2ad508905dd85c59c
2nthony <ant.hu@outlook.com>
2023-02-14 15:45:50 +0800

    fix: opengov spend card polar chart size

--- packages/site/src/components/CustomPolarArea.js
+++ packages/site/src/components/CustomPolarArea.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { PolarArea } from "react-chartjs-2";
+import { sumBy } from "../utils/math";
 
 export default function PolarAreaChart({ data, status, tooltipLabelCallback }) {
   const filteredData = status.labels
     .filter((i) => !i.disabled)
     .map((i) => data.labels.find((d) => d.name === i.name));
+  const totalValue = sumBy(filteredData, "value");
+  const enabledCount = filteredData.length;
+
+  const unit = 360 / enabledCount;
+  const calcAngle = (val) => {
+    const ratio = val / totalValue;
+    const value = ratio * unit * enabledCount;
+    return value || 0.001;
+  };
+  const angles = filteredData.map((i) => calcAngle(i.value));
 
   return (
     <PolarArea
@@ -21,6 +32,11 @@ export default function PolarAreaChart({ data, status, tooltipLabelCallback }) {
       }}
       options={{
         maintainAspectRatio: false,
+        elements: {
+          arc: {
+            angle: angles,
+          },
+        },
         scales: {
           r: {
             ticks: {
