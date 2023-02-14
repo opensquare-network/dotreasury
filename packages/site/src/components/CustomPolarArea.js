8d5765688c6aac1d98392957af036abc663157a1
2nthony <ant.hu@outlook.com>
2023-02-14 16:35:37 +0800

    fix: polar area active size (#1132)
    * fix: opengov spend card polar chart size
    
    * maxby
    
    * fix: opengov spend card polar size

--- packages/site/src/components/CustomPolarArea.js
+++ packages/site/src/components/CustomPolarArea.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { PolarArea } from "react-chartjs-2";
+import { maxBy, sumBy } from "../utils/math";
 
 export default function PolarAreaChart({ data, status, tooltipLabelCallback }) {
   const filteredData = status.labels
     .filter((i) => !i.disabled)
     .map((i) => data.labels.find((d) => d.name === i.name));
+  const totalValue = sumBy(filteredData, "value");
+  const max = maxBy(filteredData, "value");
+  const enabledCount = filteredData.length;
+
+  const unit = 360 / enabledCount;
+  const calcAngle = (val) => {
+    const ratio = val / totalValue;
+    const value = ratio * unit * enabledCount;
+    return value || 0.001;
+  };
+  const angles = filteredData.map((i) => calcAngle(i.value));
 
   return (
     <PolarArea
@@ -21,11 +33,20 @@ export default function PolarAreaChart({ data, status, tooltipLabelCallback }) {
       }}
       options={{
         maintainAspectRatio: false,
+        elements: {
+          arc: {
+            angle: angles,
+          },
+        },
         scales: {
           r: {
             ticks: {
               display: false,
             },
+            grid: {
+              color: "#f4f4f4",
+            },
+            max,
           },
         },
         plugins: {
