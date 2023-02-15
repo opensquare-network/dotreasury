7471361a0cbcc39d0202948e39df70fea40a5ce3
Yongfeng LI <wliyongfeng@gmail.com>
2023-02-15 12:01:16 +0800

    Improve overview v2 room name

--- packages/server/src/websocket/constants.js
+++ packages/server/src/websocket/constants.js
@@ -1,11 +1,11 @@
 const chainStatusRoom = "CHAIN_STATUS_ROOM";
 const overviewRoom = "OVERVIEW_ROOM";
-const overviewRoomV2 = "OVERVIEW_ROOM_V2";
+const overviewV2Room = "OVERVIEW_V2_ROOM";
 
 module.exports = {
   chainStatusRoom,
   FEED_INTERVAL: 6000,
   overviewRoom,
-  overviewRoomV2,
+  overviewV2Room,
   OVERVIEW_FEED_INTERVAL: 30 * 1000, // 30 seconds
 };
