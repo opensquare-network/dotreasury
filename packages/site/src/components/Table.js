e0dc5267ecac70a6280dfa7a816223b634d0256f
chaojun <chaojun@opensquare.network>
2023-04-03 11:57:39 +0800

    fix: time cell width

--- packages/site/src/components/Table.js
+++ packages/site/src/components/Table.js
@@ -66,8 +66,8 @@ const CustomTable = styled(SemanticTable)`
     min-width: 100px !important;
   }
   .propose-time-cell {
-    width: 200px !important;
-    max-width: 200px !important;
+    width: 280px !important;
+    max-width: 280px !important;
   }
   .related-links-cell {
     min-width: 120px !important;
