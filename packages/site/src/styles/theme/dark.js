74824d217c5c30f6b6a311bbd2dde523a3d35a74
2nthony <ant.hu@outlook.com>
2023-04-12 18:45:56 +0800

    add colors

--- packages/site/src/styles/theme/dark.js
+++ packages/site/src/styles/theme/dark.js
@@ -3,4 +3,9 @@ import { light } from "./light";
 export const dark = {
   ...light,
   dark: true,
+
+  major: "#ffffff",
+  minor: "rgba(255, 255, 255, 0.8)",
+  accessory: "rgba(255, 255, 255, 0.45)",
+  shadow100: "0px 2px 8px rgba(0, 0, 0, 0.02), 0px 1px 4px rgba(0, 0, 0, 0.02)",
 };
--- packages/site/src/styles/theme/light.js
+++ packages/site/src/styles/theme/light.js
@@ -1,5 +1,12 @@
 export const light = {
   dark: false,
-  shadow100:
-    "0px 2px 8px rgba(0, 0, 0, 0.02), 0px 1px 4px rgba(0, 0, 0, 0.02);",
+
+  accessory: "rgba(0, 0, 0, 0.3)",
+  major: "rgba(0, 0, 0, 0.9)",
+  minor: "rgba(0, 0, 0, 0.65)",
+  grey100: "#fafafa",
+  grey200: "#f4f4f4",
+  pink100: "#fff0f3",
+  pink500: "#f23252",
+  shadow100: "0px 2px 8px rgba(0, 0, 0, 0.02), 0px 1px 4px rgba(0, 0, 0, 0.02)",
 };
