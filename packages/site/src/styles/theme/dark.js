279dbb8f0de34c7e12f49bb48b5e6e7d08d775f7
2nthony <ant.hu@outlook.com>
2023-04-13 15:42:49 +0800

    tweak dark

--- packages/site/src/styles/theme/dark.js
+++ packages/site/src/styles/theme/dark.js
@@ -2,10 +2,8 @@ import { light } from "./light";
 
 export const dark = {
   ...light,
-  dark: true,
 
-  major: "#ffffff",
-  minor: "rgba(255, 255, 255, 0.8)",
-  accessory: "rgba(255, 255, 255, 0.45)",
+  textPrimary: "rgba(255, 255, 255, 0.9)",
+
   shadow100: "0px 2px 8px rgba(0, 0, 0, 0.02), 0px 1px 4px rgba(0, 0, 0, 0.02)",
 };
