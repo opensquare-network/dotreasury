1723b3a2dd929d7e63e1a95c541a3ab27fd5fa27
2nthony <ant.hu@outlook.com>
2023-04-14 13:26:13 +0800

    improve theme context

--- packages/site/src/context/theme.js
+++ packages/site/src/context/theme.js
@@ -1,12 +1,20 @@
-import { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext } from "react";
 import {
   ThemeProvider as StyledThemeProvider,
   createGlobalStyle,
 } from "styled-components";
 import { light, dark } from "../styles/theme";
 
+/**
+ * @typedef {'light'|'dark'} Mode
+ * @typedef {(mode: Mode) => void} SetMode
+ * @typedef {typeof light} Theme
+ * @typedef {{mode: Mode, setMode: SetMode, theme: Theme} ThemeContext
+ */
+
 // TODO: read from cookie
 const defaultThemeMode = "light";
+/** @type {React.Context<ThemeContext>} */
 const ThemeContext = createContext({});
 
 const GlobalThemeVars = createGlobalStyle`
@@ -30,7 +38,7 @@ export function ThemeProvider({ children }) {
 }
 
 /**
- * @returns {typeof light}
+ * @returns {Theme}
  * @description type friendly alternative to styled `useTheme`
  */
 export function useTheme() {
@@ -38,7 +46,30 @@ export function useTheme() {
 }
 
 // TODO: read preferred color scheme
+/**
+ * @description shortcut for `useThemeMode().mode === 'dark'`
+ */
 export function useDark() {
   const { mode } = useContext(ThemeContext);
   return mode === "dark";
+}
+
+/**
+ * @returns {[mode, setMode]}
+ */
+export function useThemeMode() {
+  const { mode, setMode } = useContext(ThemeContext);
+  return [mode, setMode];
+}
+
+// TODO: 1. preferred color scheme, 2. light, 3. dark
+export function useToggleThemeMode() {
+  const [, setMode] = useThemeMode();
+  const dark = useDark();
+
+  function toggleThemeMode() {
+    setMode(dark ? "light" : "dark");
+  }
+
+  return toggleThemeMode;
 }
