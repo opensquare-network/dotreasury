d9a66af12d1a94189c0022f9b1528d1278df9872
2nthony <ant.hu@outlook.com>
2023-04-23 18:40:36 +0800

    feat: theme new proposal color

--- packages/site/src/components/Input.js
+++ packages/site/src/components/Input.js
@@ -3,10 +3,21 @@ import { Input } from "semantic-ui-react";
 
 const CustomInput = styled(Input)`
   input {
+    background-color: var(--neutral100) !important;
     padding: 8px 16px !important;
     min-height: 40px !important;
-    :hover, :focus {
-      border-color: #CCCCCC !important;
+    color: var(--textPrimary) !important;
+    border-color: var(--neutral300) !important;
+    :hover,
+    :focus {
+      border-color: var(--neutral400) !important;
+    }
+
+    ::placeholder {
+      color: var(--textSecondary) !important;
+    }
+    &:focus::placeholder {
+      color: var(--textTertiary) !important;
     }
   }
 `;
