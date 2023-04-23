66f6b202cfec2592e7ce8f061d7d0b4ec869a957
2nthony <ant.hu@outlook.com>
2023-04-23 18:07:44 +0800

    fix: link to subsquare icon

--- packages/site/src/pages/Referenda/Link.js
+++ packages/site/src/pages/Referenda/Link.js
@@ -3,6 +3,15 @@ import { ReactComponent as LinkSVG } from "./link.svg";
 
 const Icon = styled(LinkSVG)`
   cursor: pointer;
+  g {
+    path {
+      stroke: var(--textTertiary);
+    }
+  }
+
+  rect {
+    stroke: var(--textDisable);
+  }
 `;
 
 export default function JumpToLink({ href }) {
