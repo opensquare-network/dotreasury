e4af1a8f8a9d4bc3bde81196f0a3c0ea80ab603d
2nthony <ant.hu@outlook.com>
2023-04-24 11:22:42 +0800

    missed import

--- packages/site/src/pages/Overview/OpenGovSpend.js
+++ packages/site/src/pages/Overview/OpenGovSpend.js
@@ -14,6 +14,7 @@ import { overviewSelector } from "../../store/reducers/overviewSlice";
 import { getPrecision, toPrecision } from "../../utils";
 import { sumBy } from "../../utils/math";
 import OverviewBaseChartCard from "./ChartCard";
+import OpengovSpendTitle from "./OpengovSpendTitle";
 
 export default function OpenGovSpend() {
   const overview = useSelector(overviewSelector);
