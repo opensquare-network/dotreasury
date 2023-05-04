97ae96f22ab3fb21661e8561d8a3d7de96380d87
2nthony <ant.hu@outlook.com>
2023-05-04 14:50:13 +0800

    avatar background color

--- packages/site/src/components/User/Avatar.js
+++ packages/site/src/components/User/Avatar.js
@@ -1,7 +1,14 @@
 import * as React from "react";
 import Identicon from "@osn/polkadot-react-identicon";
+import styled from "styled-components";
+
+const Wrapper = styled(Identicon)`
+  svg circle:first-child {
+    fill: var(--neutral200);
+  }
+`;
 
 export default function Avatar({ address, size = 24 }) {
   const theme = "polkadot";
-  return <Identicon value={address} size={size} theme={theme} />;
+  return <Wrapper value={address} size={size} theme={theme} />;
 }
