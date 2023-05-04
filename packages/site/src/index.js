3834b99c33106745876a854a6523a5426a351dbb
2nthony <ant.hu@outlook.com>
2023-05-04 17:33:11 +0800

    fix: override semantic ui css popup
    global effects

--- packages/site/src/index.js
+++ packages/site/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "semantic-ui-css/semantic.min.css";
+import "./styles/override-semantic-ui-css.css";
 
 import App from "./App";
 import store from "./store";
