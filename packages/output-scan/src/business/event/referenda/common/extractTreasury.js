6f77a52cbabd994bfa573410bdba49d0512594c9
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-09 00:39:37 +0800

    Handle referenda whose image is uploaded after submission, #1252 (#1253)
    * Save submitted referendum without preimage to pending collection, #1252
    
    * Save treasury referenda when preimage noted, #1252

---
+++ packages/output-scan/src/business/event/referenda/common/extractTreasury.js
@@ -0,0 +1,36 @@
+const { handleWrappedCall } = require("../../../common/call");
+const {
+  utils: { bigAdd },
+} = require("@osn/scan-common");
+
+async function extractTreasuryCalls(call, indexer) {
+  const spendCalls = [];
+  await handleWrappedCall(call, null, indexer, [], innerCall => {
+    const { section, method } = innerCall;
+    if ("treasury" === section || "spend" === method) {
+      spendCalls.push(innerCall);
+    }
+  });
+
+  if (spendCalls.length <= 0) {
+    return {};
+  }
+
+  const amount = spendCalls.reduce((result, call) => {
+    const amount = call.args[0].toString();
+    return bigAdd(result, amount);
+  }, 0);
+  const beneficiary = spendCalls[0].args[1].toString();
+  const beneficiaries = spendCalls.map(call => call.args[1].toString());
+
+  return {
+    calls: spendCalls,
+    amount,
+    beneficiary,
+    beneficiaries,
+  }
+}
+
+module.exports = {
+  extractTreasuryCalls,
+}
--- packages/output-scan/src/business/event/referenda/submitted/spend.js
+++ packages/output-scan/src/business/event/referenda/submitted/spend.js
@@ -1,32 +1,18 @@
+const { extractTreasuryCalls } = require("../common/extractTreasury");
 const { toDecimal128 } = require("../../../../utils");
-const { handleWrappedCall } = require("../../../common/call");
 const { insertReferendaReferendum } = require("../../../../mongo/service/referendaReferendum");
 const { getCommonData } = require("./common");
 const {
   call: { normalizeCall },
-  utils: { bigAdd },
 } = require("@osn/scan-common");
 
 async function handleTreasurySpend(referendumIndex, trackId, proposalHash, call, indexer) {
-  const spendCalls = [];
-  await handleWrappedCall(call, null, indexer, [], innerCall => {
-    const { section, method } = innerCall;
-    if ("treasury" === section || "spend" === method) {
-      spendCalls.push(innerCall);
-    }
-  });
-  if (spendCalls.length <= 0) {
+  const { calls, amount, beneficiary, beneficiaries } = await extractTreasuryCalls(call, indexer);
+  if (!calls) {
     return
   }
 
-  const amount = spendCalls.reduce((result, call) => {
-    const amount = call.args[0].toString();
-    return bigAdd(result, amount);
-  }, 0);
-  const beneficiary = spendCalls[0].args[1].toString();
-
   const commonData = await getCommonData(referendumIndex, trackId, indexer);
-
   await insertReferendaReferendum({
     ...commonData,
     proposalHash,
@@ -35,6 +21,7 @@ async function handleTreasurySpend(referendumIndex, trackId, proposalHash, call,
     value: amount,
     dValue: toDecimal128(amount),
     beneficiary,
+    beneficiaries,
   });
 }
 
