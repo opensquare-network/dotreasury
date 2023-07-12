0f9b21ce38252d05fa4598ddad03c13214622246
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-13 00:23:15 +0800

    Fix treasury output statistics

--- packages/output-scan/src/stats/index.js
+++ packages/output-scan/src/stats/index.js
@@ -1,14 +1,9 @@
 const { getBlockIndexerByHeight } = require("./chain");
-const { stringUpperFirst } = require("@polkadot/util");
 const {
   getStatusCollection,
-  getProposalCollection,
-  getBountyCollection,
-  getTipCollection,
-  getBurntCollection,
   getWeeklyStatsCollection,
 } = require("../mongo");
-const { utils: { bigAdd } } = require("@osn/scan-common");
+const { calcOutputStatsAt } = require("./output");
 
 const lastStatsHeight = "last-outputstats-height";
 
@@ -39,102 +34,20 @@ async function updateStatHeight(height) {
   );
 }
 
-async function calcOutputStats() {
-  // fixme: 1. should calculate output by indexer.blockHeight
-  // fixme: 2. should calculate transfers too. https://www.dotreasury.com/ksm/transfers
-  const proposalCol = await getProposalCollection();
-  const proposals = await proposalCol
-    .find({}, { value: 1, beneficiary: 1, meta: 1, state: 1 })
-    .toArray();
-
-  const tipCol = await getTipCollection();
-  const tips = await tipCol
-    .find({}, { finder: 1, medianValue: 1, state: 1 })
-    .toArray();
-
-  const bountyCol = await getBountyCollection();
-  const bounties = await bountyCol.find({}, { meta: 1, state: 1 }).toArray();
-
-  const burntCol = await getBurntCollection();
-  const burntList = await burntCol.find({}, { balance: 1 }).toArray();
-
-  const output = await calcOutput(proposals, tips, bounties, burntList);
-
-  return output;
-}
-
-const bountyStatuses = [
-  "Proposed",
-  "Approved",
-  "Funded",
-  "CuratorProposed",
-  "Active",
-  "PendingPayout",
-];
-
-async function calcOutput(
-  proposals = [],
-  tips = [],
-  bounties = [],
-  burntList = []
-) {
-  const spentProposals = proposals.filter(
-    ({ state: { name, state } }) => (name || state) === "Awarded"
-  );
-  const proposalSpent = spentProposals.reduce(
-    (result, { value }) => bigAdd(result, value),
-    0
-  );
-
-  const tipSpent = tips.reduce((result, { state: { state }, medianValue }) => {
-    if (state !== "TipClosed") {
-      return result;
-    }
-
-    if (!medianValue) {
-      return result;
-    }
-
-    return bigAdd(result, medianValue);
-  }, 0);
-
-  const bountySpent = bounties.reduce((result, { meta: { status, value } }) => {
-    const statusKey = stringUpperFirst(Object.keys(status)[0]);
-
-    const index = bountyStatuses.findIndex((item) => item === statusKey);
-    return index >= 2 ? bigAdd(result, value) : result;
-  }, 0);
-
-  const burntTotal = burntList.reduce((result, { balance }) => {
-    return bigAdd(result, balance);
-  }, 0);
-
-  return {
-    proposal: proposalSpent,
-    tip: tipSpent,
-    bounty: bountySpent,
-    burnt: burntTotal,
-  };
-}
-
-async function createStatAt(indexer, isWeekPoint = true) {
-
-}
-
-async function tryCreateStatPoint(indexer) {
+async function tryCreateStatPoint(blockIndexer) {
   while (true) {
     const nextStatHeight = await getNextStatHeight();
-    if (nextStatHeight >= indexer.blockHeight) {
+    if (nextStatHeight > blockIndexer.blockHeight) {
       return;
     }
 
-    const indexer = await getBlockIndexerByHeight(nextStatHeight);
-    const output = await calcOutputStats();
+    const statIndexer = await getBlockIndexerByHeight(nextStatHeight);
+    const output = await calcOutputStatsAt(statIndexer);
 
     // Go on create one stat point
     const weeklyStatsCol = await getWeeklyStatsCollection();
     await weeklyStatsCol.updateOne(
-      { indexer },
+      { indexer: statIndexer },
       {
         $set: {
           output,
