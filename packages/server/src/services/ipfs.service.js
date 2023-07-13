a930d043ba00d348e352c35d9c42b0f9849166b4
chaojun <chaojun@opensquare.network>
2023-07-13 12:38:43 +0800

    Upgrade ipfs dependency

--- packages/server/src/services/ipfs.service.js
+++ packages/server/src/services/ipfs.service.js
@@ -8,21 +8,29 @@ const {
   USE_LOCAL_IFPS_NODE,
 } = require("../env");
 
-const [ipfsClient] = getClients(
-  USE_LOCAL_IFPS_NODE ? ClientMode.Local : ClientMode.Infura,
-  new ClientOptions(
-    INFURA_PROJECT_ID,
-    INFURA_PROJECT_SECRET,
-    LOCAL_IPFS_NODE_URL
-  )
-);
+let ipfsClient = null;
+
+async function initIpfsClient() {
+  if (!ipfsClient) {
+    [ipfsClient] = await getClients(
+      USE_LOCAL_IFPS_NODE ? ClientMode.Local : ClientMode.Infura,
+      new ClientOptions(
+        INFURA_PROJECT_ID,
+        INFURA_PROJECT_SECRET,
+        LOCAL_IPFS_NODE_URL
+      )
+    );
+  }
+}
 
 async function ipfsAdd(data) {
+  await initIpfsClient();
   const added = await ipfsClient.add(JSON.stringify(data));
   return added;
 }
 
 async function ipfsAddBuffer(data) {
+  await initIpfsClient();
   const added = await ipfsClient.add(data);
   return added;
 }
