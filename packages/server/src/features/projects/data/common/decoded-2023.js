f263e083cc57bdd4d7ffa175957fe3e5ca78cd19
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-30 21:35:27 +0800

    Add decoded 2023 to projects, #771

---
+++ packages/server/src/features/projects/data/common/decoded-2023.js
@@ -0,0 +1,255 @@
+const id = "decoded-2023";
+const name = "Polkadot Decoded 2023";
+const title =
+  "Various side events around decoded 2023 funded by dotsama treasury";
+const logo = "decoded.svg";
+const description = `Various side events around decoded 2023 funded by polkadot/kusama treasury`;
+
+const proposals = [
+  {
+    token: "dot",
+    proposalId: 291,
+  },
+  {
+    token: "dot",
+    proposalId: 278,
+  },
+  {
+    token: "dot",
+    proposalId: 255,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 315,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 314,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 312,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 311,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 310,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 309,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 308,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 307,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 306,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 305,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 304,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 303,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 302,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 301,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 300,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 299,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 298,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 297,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 297,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 296,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 295,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 294,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 293,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 292,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 291,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 290,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 289,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 288,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 287,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 286,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 285,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 284,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 283,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 282,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 281,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 280,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 279,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 278,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 277,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 276,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 275,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 274,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 241,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 240,
+  },
+  {
+    type: "child-bounty",
+    token: "dot",
+    id: 239,
+  },
+];
+
+module.exports = {
+  id,
+  name,
+  logo,
+  title,
+  description,
+  proposals,
+}
