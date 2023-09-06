9565836c47ce91b2c42865f69cef360b0999ca97
hyifeng <chaojun@opensquare.network>
2023-09-06 12:15:49 +0800

    Add stats of opengov applications, #1260 (#1284)
    * Add stats of opengov applications, #1260
    
    * fix
    
    * Add all tracks summary, #1260

--- packages/server/src/features/referenda/referenda.controller.js
+++ packages/server/src/features/referenda/referenda.controller.js
@@ -1,7 +1,5 @@
 const { HttpError } = require("../../exc");
-const {
-  getReferendaReferendumCollection,
-} = require("../../mongo");
+const { getReferendaReferendumCollection } = require("../../mongo");
 const { extractPage } = require("../../utils");
 const { getRangeCondition } = require("../common/getRangeCondition");
 const { ReferendaQueryFieldsMap } = require("../common/query");
@@ -22,7 +20,7 @@ const ReferendaStateSort = {
 function getCondition(ctx) {
   const { status, track } = ctx.request.query;
 
-  const condition = {}
+  const condition = {};
   if (status) {
     condition["state.name"] = status;
   }
@@ -61,24 +59,22 @@ class ReferendaController {
         $addFields: {
           sort: {
             $switch: {
-              branches: Object.keys(ReferendaStateSort).map(state => (
-                {
-                  case: {
-                    $eq: ["$state.name", state]
-                  },
-                  then: ReferendaStateSort[state],
-                }
-              )),
+              branches: Object.keys(ReferendaStateSort).map((state) => ({
+                case: {
+                  $eq: ["$state.name", state],
+                },
+                then: ReferendaStateSort[state],
+              })),
               default: 0,
-            }
-          }
-        }
+            },
+          },
+        },
       },
       {
         $sort: {
           sort: -1,
           "indexer.blockHeight": -1,
-        }
+        },
       },
     ];
 
@@ -94,23 +90,25 @@ class ReferendaController {
           $sort: {
             [fieldName]: sortDirection === "desc" ? -1 : 1,
             "indexer.blockHeight": -1,
-          }
-        }
+          },
+        },
       ];
     }
 
-    const referendaQuery = referendumCol.aggregate([
-      { $match: condition },
-      ...sortPipeline,
-      { $skip: page * pageSize },
-      { $limit: pageSize },
-      {
-        $project: {
-          sort: 0,
-          timeline: 0,
-        }
-      }
-    ]).toArray();
+    const referendaQuery = referendumCol
+      .aggregate([
+        { $match: condition },
+        ...sortPipeline,
+        { $skip: page * pageSize },
+        { $limit: pageSize },
+        {
+          $project: {
+            sort: 0,
+            timeline: 0,
+          },
+        },
+      ])
+      .toArray();
 
     const [total, referenda] = await Promise.all([totalQuery, referendaQuery]);
 
@@ -125,52 +123,68 @@ class ReferendaController {
   async getSummary(ctx) {
     const { chain } = ctx.params;
     const referendumCol = await getReferendaReferendumCollection(chain);
-    const totalByStack = await referendumCol
-      .aggregate([
-        {
-          $group: {
-            _id: "$trackInfo.name",
-            count: { $sum: 1 },
-          },
-        },
-      ])
-      .toArray();
-    const activeByStack = await referendumCol
-      .aggregate([
-        {
-          $match: {
-            "state.name": {
-              $in: [
-                "Confirming",
-                "Deciding",
-                "Queueing",
-                "Submitted",
-              ]
-            }
-          }
-        },
-        {
-          $group: {
-            _id: "$trackInfo.name",
-            count: { $sum: 1 },
-          },
-        },
-      ])
-      .toArray();
-
+    const referendums = await referendumCol.find().toArray();
     const result = {};
-    for (const item of totalByStack) {
-      result[item._id] = result[item._id] || { total: 0, active: 0 };
-      result[item._id].total = item.count;
+    for (const referendum of referendums) {
+      const { name } = referendum.trackInfo || {};
+      const { name: state } = referendum.state || {};
+      if (!name) {
+        continue;
+      }
+
+      let {
+        total = 0,
+        voting = 0,
+        passing = 0,
+        approved = 0,
+        rejected = 0,
+      } = result[name] || {};
+
+      total++;
+      if (["Confirming", "Deciding", "Queueing", "Submitted"].includes(state)) {
+        voting++;
+      }
+      if ("Confirming" === state) {
+        passing++;
+      }
+      if ("Approved" === state) {
+        approved++;
+      }
+      if ("Rejected" === state) {
+        rejected++;
+      }
+
+      result[name] = {
+        total,
+        active: voting,
+        voting,
+        passing,
+        approved,
+        rejected,
+      };
     }
-    for (const item of activeByStack) {
-      result[item._id] = result[item._id] || { total: 0, active: 0 };
-      result[item._id].active = item.count;
-    }
+
+    result.all = Object.values(result).reduce(
+      (acc, cur) => ({
+        total: acc.total + cur.total,
+        active: acc.active + cur.active,
+        voting: acc.voting + cur.voting,
+        passing: acc.passing + cur.passing,
+        approved: acc.approved + cur.approved,
+        rejected: acc.rejected + cur.rejected,
+      }),
+      {
+        total: 0,
+        voting: 0,
+        active: 0,
+        passing: 0,
+        approved: 0,
+        rejected: 0,
+      },
+    );
 
     ctx.body = result;
   }
-
 }
 
 module.exports = new ReferendaController();
