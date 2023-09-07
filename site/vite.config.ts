c4fd0afe3b8b29ab5e7aab5007e95a88d7567eb6
2nthony <ant.hu@outlook.com>
2023-09-07 11:31:03 +0800

    fix: build

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
-import type { PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills() as PluginOption],
+  plugins: [react(), svgr()],
   assetsInclude: ["**/*.md"],
   server: {
     port: 3000,
   },
-  resolve: {
-    alias: {
-      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [NodeModulesPolyfillPlugin()],
     },
   },
   build: {
