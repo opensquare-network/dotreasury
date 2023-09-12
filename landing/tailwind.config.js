56e1637fdce4ec5526849c5f124d97afe1495992
2nthony <ant.hu@outlook.com>
2023-09-12 18:59:13 +0800

    tailwind extend colors

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -1,9 +1,23 @@
+const { light } = require("../site/src/styles/theme");
+
+/**
+ * `light.neutral100` -> `{ neutral100: 'var(--neutral100)' }`
+ */
+const twThemeVariables = Object.keys(light).reduce((value, key) => {
+  value[key] = `var(--${key})`;
+  return value;
+}, {});
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...twThemeVariables,
+      },
+    },
   },
   plugins: [],
 };
