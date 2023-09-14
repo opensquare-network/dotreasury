63494e97eece9e9cc14c65e00faca278841cf1f8
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-14 21:09:56 +0800

    Support more chains

--- packages/eco-data-server/src/apis/endpoints.js
+++ packages/eco-data-server/src/apis/endpoints.js
@@ -9,6 +9,26 @@ const endpoints = Object.freeze({
     "wss://kusama.api.onfinality.io/public-ws",
     "wss://kusama-rpc.dwellir.com"
   ],
+  centrifuge: [
+    "wss://fullnode.centrifuge.io/",
+    "wss://rpc-centrifuge.luckyfriday.io/",
+  ],
+  khala: [
+    "wss://khala-api.phala.network/ws",
+    "wss://khala-rpc.dwellir.com/",
+  ],
+  phala: [
+    "wss://api.phala.network/ws",
+    "wss://phala-rpc.dwellir.com/",
+  ],
+  hydradx: [
+    "wss://hydradx-rpc.dwellir.com/",
+    "wss://rpc.hydradx.cloud/"
+  ],
+  basilisk: [
+    "wss://basilisk-rpc.dwellir.com/",
+    "wss://rpc.basilisk.cloud/"
+  ],
 });
 
 module.exports = {
