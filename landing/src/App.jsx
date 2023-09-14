cb147d88b4598dfbb833eef78c65629de08fdef5
2nthony <ant.hu@outlook.com>
2023-09-14 18:38:27 +0800

    stash dotsama section

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -4,12 +4,14 @@ import Footer from "../../site/src/pages/Footer";
 import { ThemeProvider } from "../../site/src/context/theme";
 import Header from "./components/header";
 import Hero from "./components/hero";
+import DotsamaTreasuryOverview from "./components/dotsamaTreasuryOverview";
 
 function App() {
   return (
     <ThemeProvider>
       <Header />
       <Hero />
+      <DotsamaTreasuryOverview />
       <Footer />
 
       <div className="absolute top-0 max-h-screen overflow-hidden -z-10 pointer-events-none">
---
+++ landing/src/components/dotsamaTreasuryOverview/index.jsx
@@ -0,0 +1,12 @@
+import Container from "../../../../site/src/components/Container";
+
+export default function DotsamaTreasuryOverview() {
+  return (
+    <Container className="py-10">
+      <h2 className="h2-32-bold-montserrat mb-10 px-6">
+        Dotsama Treasury Overview
+      </h2>
+      <div>TODO content</div>
+    </Container>
+  );
+}
