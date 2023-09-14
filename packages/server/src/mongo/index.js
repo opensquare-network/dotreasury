022fbb9aebac2e08e9b2ab6fbd54d7d667487bb6
chaojun <chaojun@opensquare.network>
2023-09-14 11:23:16 +0800

    fix

--- packages/server/src/mongo/index.js
+++ packages/server/src/mongo/index.js
@@ -1,5 +1,7 @@
 if (process.env.CHAIN === "polkadot") {
   module.exports = require("./polkadot");
-} else {
+} else if (process.env.CHAIN === "kusama") {
   module.exports = require("./kusama");
+} else {
+  throw new Error("Invalid CHAIN env variable");
 }
