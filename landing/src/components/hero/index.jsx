7c293af7996f75dfb48640d24d2a9bd695b4c261
2nthony <ant.hu@outlook.com>
2023-09-14 18:38:13 +0800

    refactor

--- landing/src/components/hero/index.jsx
+++ landing/src/components/hero/index.jsx
@@ -10,29 +10,27 @@ export default function Hero() {
   const [bubblesRef, bubblesSize] = useElementSize();
 
   return (
-    <div className="py-20">
-      <Container className="grid grid-cols-2 h-[480px] max-md:grid-cols-1">
-        <div className="px-6 flex items-center">
-          <HeroContent />
-        </div>
+    <Container className="grid grid-cols-2 h-[480px] py-20 max-md:grid-cols-1">
+      <div className="px-6 flex items-center">
+        <HeroContent />
+      </div>
 
-        <div
-          ref={bubblesRef}
-          className={cn(
-            "sm:px-4 max-md:pt-4",
-            "h-[480px] max-md:h-[315px] max-sm:h-[235px]",
-          )}
-        >
-          <ProjectBubbleGroup
-            width={bubblesSize.width}
-            height={bubblesSize.height}
-            data={BUBBLE_DATA}
-            renderBubbleToHTMLString={(node) => {
-              return <ProjectBubble project={node.name} size={node.r * 2} />;
-            }}
-          />
-        </div>
-      </Container>
-    </div>
+      <div
+        ref={bubblesRef}
+        className={cn(
+          "sm:px-4 max-md:pt-4",
+          "h-[480px] max-md:h-[315px] max-sm:h-[235px]",
+        )}
+      >
+        <ProjectBubbleGroup
+          width={bubblesSize.width}
+          height={bubblesSize.height}
+          data={BUBBLE_DATA}
+          renderBubbleToHTMLString={(node) => {
+            return <ProjectBubble project={node.name} size={node.r * 2} />;
+          }}
+        />
+      </div>
+    </Container>
   );
 }
