a39f2683046d0d0c1aa94fd646b68c5774c1c695
chaojun <chaojun@opensquare.network>
2023-09-18 17:21:02 +0800

    fix: user detail, bounties and child-bounties tab header margin

--- site/src/pages/UsersDetail/ProposalsTables/ChildBountiesTable.jsx
+++ site/src/pages/UsersDetail/ProposalsTables/ChildBountiesTable.jsx
@@ -10,6 +10,7 @@ import {
 import { useEffect, useMemo } from "react";
 import { compatChildBountyData } from "../../ChildBounties/utils";
 import { resolveFilterData } from "./resolveFilterData";
+import { TableHeaderWrapper } from "./styled";
 
 export default function ChildBountiesTable({
   header,
@@ -34,8 +35,8 @@ export default function ChildBountiesTable({
         chain,
         tablePage - 1,
         pageSize,
-        resolveFilterData(filterData, { role, address })
-      )
+        resolveFilterData(filterData, { role, address }),
+      ),
     );
   }, [dispatch, chain, tablePage, pageSize, filterData, role, address]);
 
@@ -43,7 +44,7 @@ export default function ChildBountiesTable({
 
   return (
     <ChildBountiesTableOrigin
-      header={header}
+      header={<TableHeaderWrapper>{header}</TableHeaderWrapper>}
       loading={loading}
       data={items}
       footer={!!items.length && footer(totalPages)}
