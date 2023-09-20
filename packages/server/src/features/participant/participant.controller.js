1900e3b33d46206d4a0d388ff89dd50a2ed4f062
chaojun <chaojun@opensquare.network>
2023-09-20 18:34:04 +0800

    fix: participant api

--- packages/server/src/features/participant/participant.controller.js
+++ packages/server/src/features/participant/participant.controller.js
@@ -1,12 +1,24 @@
 const { getParticipantCollection } = require("../../mongo");
 const { extractPage } = require("../../utils");
 
-async function getParticipants(ctx) {
+async function getParticipant(ctx) {
   const { address } = ctx.params;
+
+  const participantCol = await getParticipantCollection();
+  const data = await participantCol.findOne({ address });
+  if (!data) {
+    ctx.throw(404, "Participant not found");
+    return;
+  }
+
+  ctx.body = data;
+}
+
+async function getParticipants(ctx) {
   const { page, pageSize } = extractPage(ctx);
   const { role } = ctx.request.query;
 
-  const q = { proposer: address };
+  const q = {};
   if (role === "beneficiary") {
     q.isBeneficiary = true;
   } else if (role === "proposer") {
@@ -33,5 +45,6 @@ async function getParticipants(ctx) {
 }
 
 module.exports = {
+  getParticipant,
   getParticipants,
 };
