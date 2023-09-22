a1bc80cd0808cbb6841175bd889c629f4e3a3834
2nthony <ant.hu@outlook.com>
2023-09-22 16:10:38 +0800

    fix: dedupe all dependencies from `site`

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -4,6 +4,7 @@ import url from "url";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import siteConfig from "../site/vite.config";
+import sitePkgJson from "../site/package.json";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,6 +12,14 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: siteConfig.optimizeDeps,
+  resolve: {
+    dedupe: [
+      // dedupe all dependencies from `site`
+      "@polkadot/keyring",
+      "@osn/polkadot-react-identicon",
+      ...Object.keys(sitePkgJson.dependencies),
+    ],
+  },
   plugins: [
     react(),
     viteStaticCopy({
