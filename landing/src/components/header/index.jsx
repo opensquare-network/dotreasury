bead5ccf1ee59418ab9b2f9e37198916d1a06521
2nthony <ant.hu@outlook.com>
2023-09-22 15:14:12 +0800

    fix: header layout

--- landing/src/components/header/index.jsx
+++ landing/src/components/header/index.jsx
@@ -5,6 +5,8 @@ import { flip, offset, shift, useFloating } from "@floating-ui/react";
 import { useOnClickOutside } from "usehooks-ts";
 import HeaderExplorer from "./explorer";
 import { cn } from "../../utils";
+import IconMask from "../../../../site/src/components/Icon/Mask";
+import ExternalLink from "../../../../site/src/components/ExternalLink";
 
 const CONTACT_LINK = "https://t.me/dotreasury";
 
@@ -29,7 +31,13 @@ export default function Header() {
 
   return (
     <header className="py-5 max-md:px-6">
-      <Container className="flex justify-between items-center">
+      <Container
+        className={cn(
+          "flex justify-between items-center",
+          "max-md:!m-0",
+          "max-w-[inherit] max-md:!w-full",
+        )}
+      >
         <div>
           <a href="/">
             <img
@@ -45,24 +53,38 @@ export default function Header() {
           </a>
         </div>
 
-        <div className={cn("flex items-center gap-x-8", "max-sm:hidden")}>
-          <a href={CONTACT_LINK} className="text-textPrimary">
-            Contact Us
-          </a>
-          <Button
-            ref={refs.setReference}
-            className="font-medium"
-            onClick={show}
-          >
-            Explore Treasury
-            {visible && (
-              <HeaderExplorer
-                ref={refs.setFloating}
-                style={floatingStyles}
-                className="z-10"
-              />
-            )}
-          </Button>
+        <div className="flex">
+          <div className={cn("flex items-center gap-x-8", "max-sm:hidden")}>
+            <ExternalLink
+              href={CONTACT_LINK}
+              className="text-textPrimary hover:text-textPrimary"
+            >
+              Contact Us
+            </ExternalLink>
+            <Button
+              ref={refs.setReference}
+              className="font-medium"
+              onClick={show}
+            >
+              Explore Treasury
+              {visible && (
+                <HeaderExplorer
+                  ref={refs.setFloating}
+                  style={floatingStyles}
+                  className="z-10"
+                />
+              )}
+            </Button>
+          </div>
+
+          <div className="hidden max-sm:block">
+            <IconMask
+              src="/imgs/icon-ham-black.svg"
+              color="textPrimary"
+              size={24}
+              alt="menu"
+            />
+          </div>
         </div>
       </Container>
     </header>
