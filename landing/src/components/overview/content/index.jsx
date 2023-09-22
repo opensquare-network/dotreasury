cace7576014cc75a48559ba19f183f631dee6150
2nthony <ant.hu@outlook.com>
2023-09-22 18:49:22 +0800

    fix: overview layout

--- landing/src/components/overview/content/index.jsx
+++ landing/src/components/overview/content/index.jsx
@@ -8,8 +8,7 @@ export default function OverviewContent({ chain = "" }) {
   return (
     <div
       className={cn(
-        "grid grid-rows-3 grid-cols-3 grid-flow-col gap-4",
-        "md:min-h-[944px]",
+        "grid grid-cols-3 gap-x-4",
         "max-md:block max-md:space-y-4",
       )}
     >
@@ -17,18 +16,20 @@ export default function OverviewContent({ chain = "" }) {
         <OverviewSummary chain={chain} />
       </div>
 
-      <div className="col-span-2">
-        <OverviewTotalStacked chain={chain} />
-      </div>
+      <div className="space-y-4 col-span-2">
+        <div className="col-span-2">
+          <OverviewTotalStacked chain={chain} />
+        </div>
 
-      <div
-        className={cn(
-          "row-span-2 col-span-2 grid grid-cols-2 gap-4",
-          "max-md:block max-md:space-y-4",
-        )}
-      >
-        <OverviewAmount chain={chain} />
-        <OverviewTopBeneficiaries chain={chain} />
+        <div
+          className={cn(
+            "grid grid-cols-2 gap-4",
+            "max-md:block max-md:space-y-4",
+          )}
+        >
+          <OverviewAmount chain={chain} />
+          <OverviewTopBeneficiaries chain={chain} />
+        </div>
       </div>
     </div>
   );
