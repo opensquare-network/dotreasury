c9c46e3530ec66e8e523341ad6316dfdb1c29f3a
2nthony <ant.hu@outlook.com>
2023-09-25 14:48:44 +0800

    feat: countup hero value

--- landing/src/components/hero/content.jsx
+++ landing/src/components/hero/content.jsx
@@ -1,31 +1,57 @@
 import { abbreviateBigNumber } from "../../../../site/src/utils";
 import { useTreasuriesData } from "../../hooks/useTreasuriesData";
 import { cn } from "../../utils";
+import CountUp from "react-countup";
 
 export default function HeroContent() {
   const { data, totalValue } = useTreasuriesData();
+
+  const projects = data.length || 0;
+
+  const displayTotalValue = abbreviateBigNumber(totalValue);
+  const displayTotalValueFiat = Number(displayTotalValue.slice(0, -1));
+  const displayTotalValueDecimals = totalValue ? 2 : 0;
+  const displayTotalValueUnit = totalValue ? displayTotalValue.slice(-1) : "";
 
   return (
     <div>
       <div className="h2-32-bold-montserrat text-textPrimary max-sm:leading-10">
         <div>
-          <span className={cn("text-yellow500", "sm:h2-48-bold-montserrat")}>
-            {data.length || 0}
-          </span>{" "}
-          <span>Projects</span>
+          <div
+            className={cn(
+              "text-yellow500",
+              "sm:h2-48-bold-montserrat",
+              !projects && "animate-pulse",
+            )}
+          >
+            <CountUp end={projects} />
+          </div>
+          <div className="h2-32-bold-montserrat leading-[32px]">Projects</div>
         </div>
-        <div>
-          <span className="text-pink500 sm:h2-48-bold-montserrat">
-            ${abbreviateBigNumber(totalValue)}
-          </span>{" "}
-          <span>Treasury Assets</span>{" "}
-          <span className="sm:h2-48-bold-montserrat inline-block max-sm:inline">
-            In Dotsama Ecosystem
-          </span>
+        <div className="mt-4">
+          <div
+            className={cn(
+              "text-pink500 sm:h2-48-bold-montserrat",
+              !totalValue && "animate-pulse",
+            )}
+          >
+            <CountUp
+              prefix="$"
+              end={displayTotalValueFiat}
+              decimals={displayTotalValueDecimals}
+              suffix={displayTotalValueUnit}
+            />
+          </div>
+          <div className="h2-32-bold-montserrat leading-[32px]">
+            Treasury Assets
+          </div>
+        </div>
+        <div className="mt-4 sm:h2-48-bold-montserrat inline-block max-sm:inline">
+          In Dotsama Ecosystem
         </div>
       </div>
 
-      <div className="text-textTertiary h3-18-medium sm:mt-2">
+      <div className="text-textTertiary h3-18-medium sm:mt-2 mt-4">
         dotTreasury is a platform that aggregates treasury data on the Substrate
         ecological network.
       </div>
