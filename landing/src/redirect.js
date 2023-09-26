c7e02cb433e05b1d30562a616c50e15178b94691
2nthony <ant.hu@outlook.com>
2023-09-26 16:45:36 +0800

    fix: redirect to correct site
    from old url

---
+++ landing/src/redirect.js
@@ -0,0 +1,19 @@
+import { networkFromSymbol } from "@site/src/utils";
+
+export function redirect() {
+  const pathname = location.pathname;
+  const paths = pathname.split("/").filter(Boolean);
+  const symbol = paths[0]?.toLowerCase?.();
+  const routes = paths.slice(1);
+
+  if (symbol) {
+    const chain = networkFromSymbol(symbol);
+
+    // if matched chian, then redirect
+    if (chain) {
+      window.location.replace(
+        `https://${chain}.dotreasury.com/#/${routes.join("/")}`,
+      );
+    }
+  }
+}
