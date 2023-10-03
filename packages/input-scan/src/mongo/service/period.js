0d97f5ac2008eb7c20e5662f8dc4ad68e656c95c
Yongfeng LI <wliyongfeng@gmail.com>
2023-10-03 18:42:00 +0800

    Save period info in input-scan package

---
+++ packages/input-scan/src/mongo/service/period.js
@@ -0,0 +1,21 @@
+const { getPeriodCol } = require("../data");
+
+async function insertPeriod(obj = {}, indexer) {
+  if (!obj.startIndexer || !obj.endIndexer) {
+    throw new Error(`No start or end indexer when insert period at ${ indexer.blockHeight }`)
+  }
+
+  const col = await getPeriodCol();
+  await col.insertOne(obj);
+}
+
+async function getLastPeriod(height) {
+  const col = await getPeriodCol();
+  const periods = await col.find({ endHeight: { $lt: height } }).sort({ endHeight: -1 }).limit(1).toArray();
+  return periods.length > 0 ? periods[0] : null;
+}
+
+module.exports = {
+  getLastPeriod,
+  insertPeriod,
+}
