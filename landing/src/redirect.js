23f52037166e289b52e2d517f27ac1aff347a786
2nthony <ant.hu@outlook.com>
2023-10-07 11:18:16 +0800

    typo

--- landing/src/redirect.js
+++ landing/src/redirect.js
@@ -9,7 +9,7 @@ export function redirect() {
   if (symbol) {
     const chain = networkFromSymbol(symbol);
 
-    // if matched chian, then redirect
+    // if matched chain, then redirect
     if (chain) {
       window.location.replace(
         `https://${chain}.dotreasury.com/#/${routes.join("/")}`,
