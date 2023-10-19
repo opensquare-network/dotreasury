c0ad1d822a3fd7970b69dcb525868f69a1a67e91
2nthony <ant.hu@outlook.com>
2023-10-19 17:33:34 +0800

    update chain configs

--- site/src/utils/env.js
+++ site/src/utils/env.js
@@ -1,11 +1,10 @@
 import { CHAINS } from "../constants";
+import { getChainSettings } from "./chains";
 
 export const CHAIN = import.meta.env.VITE_APP_CHAIN;
+
+export const CHAIN_SETTINGS = getChainSettings(CHAIN);
 
 export const IS_KUSAMA = CHAIN === CHAINS.KUSAMA;
 export const IS_POLKADOT = CHAIN === CHAINS.POLKADOT;
 export const IS_CENTRIFUGE = CHAIN === CHAINS.CENTRIFUGE;
-
-export const IS_SUPPORT_OPEN_GOV = [CHAINS.KUSAMA, CHAINS.POLKADOT].includes(
-  CHAIN,
-);
