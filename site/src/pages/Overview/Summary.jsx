796de236d196763b3d893f9bb3bedf9019f8b604
2nthony <ant.hu@outlook.com>
2023-10-25 11:55:43 +0800

    fix: overview summary gap

--- site/src/pages/Overview/Summary.jsx
+++ site/src/pages/Overview/Summary.jsx
@@ -30,7 +30,7 @@ import {
   p,
   rounded_none,
 } from "../../styles/tailwindcss";
-import { breakpoint, smcss, mdcss } from "../../styles/responsive";
+import { breakpoint, smcss, mdcss, lgcss } from "../../styles/responsive";
 import { useSupportOpenGov } from "../../utils/hooks/chain";
 import { extractTime } from "@polkadot/util";
 import { parseEstimateTime } from "../../utils/parseEstimateTime";
@@ -44,12 +44,13 @@ const Wrapper = styled(Card)`
 
   ${p(24)};
   ${grid};
-  ${gap_x(16)};
+  ${gap_x(128)};
   ${gap_y(8)};
   ${grid_cols(4)};
 
   ${mdcss(grid_cols(3))};
   ${smcss(grid_cols(2))};
+  ${lgcss(gap_x(16))};
   ${breakpoint(600, rounded_none)};
 `;
 
