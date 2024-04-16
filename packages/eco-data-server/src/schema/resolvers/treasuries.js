dfa43299ef3347ade3b268776d1ce9e7cbf1497b
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-16 09:50:50 +0800

    Enable darwinia and integritee on landing page

--- packages/eco-data-server/src/schema/resolvers/treasuries.js
+++ packages/eco-data-server/src/schema/resolvers/treasuries.js
@@ -9,8 +9,8 @@ async function treasuries(_, _args) {
   }
 
   const treasuries = await col.find(q, { projection: { _id: 0 } }).toArray();
-  const filtered = treasuries.filter(item => !["darwinia", "integritee"].includes(item.chain));
-  return filtered.map(treasury => {
+  // const filtered = treasuries.filter(item => !["darwinia", "integritee"].includes(item.chain));
+  return treasuries.map(treasury => {
     return {
       ...treasury,
       balanceUpdateAt: treasury.balanceUpdateAt.getTime(),
