eb22ee1f8799ba7d1af5a098f0149570ebd60617
chaojun <chaojun@opensquare.network>
2024-05-11 16:56:56 +0800

    Add script to insert failed proposals, #1357

---
+++ packages/server/src/scripts/update-failed-proposals/index.js
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { getFailedProposalCollection } = require("../../mongo");
+
+const getFailedProposalData = () => {
+  const chain = process.env.CHAIN;
+  if (chain === "kusama") {
+    return require("./kusama.json");
+  } else if (chain === "polkadot") {
+    return require("./polkadot.json");
+  } else if (chain === "centrifuge") {
+    return require("./centrifuge.json");
+  } else {
+    throw new Error("Invalid chain");
+  }
+};
+
+async function main() {
+  const failedProposalCol = await getFailedProposalCollection();
+  const failedProposals = getFailedProposalData();
+
+  const bulk = failedProposalCol.initializeUnorderedBulkOp();
+  for (const proposal of failedProposals) {
+    console.log(`Updating proposal ${proposal.proposalIndex}`);
+    bulk
+      .find({ proposalIndex: proposal.proposalIndex })
+      .upsert()
+      .updateOne({
+        $set: {
+          reason: proposal.reason,
+        },
+      });
+  }
+  await bulk.execute();
+}
+
+main()
+  .catch(console.error)
+  .finally(() => process.exit(0));
