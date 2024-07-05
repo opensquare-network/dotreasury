88b1be7b593d4946d8400cb9e79e9dd191d93c27
2nthony <ant.hu@outlook.com>
2024-07-05 15:55:06 +0800

    asshub account link

--- site/src/constants/assetHub.js
+++ site/src/constants/assetHub.js
@@ -2,3 +2,4 @@ export const ASSET_HUB_ACCOUNT =
   "14xmwinmCEz6oRrFdczHKqHgWNMiCysE2KrA4jXXAAM1Eogk";
 export const ASSET_HUB_USDT_ASSET_ID = 1984;
 export const ASSET_HUB_USDC_ASSET_ID = 1337;
+export const ASSET_HUB_ACCOUNT_LINK = `https://statemint.statescan.io/#/accounts/${ASSET_HUB_ACCOUNT}`;
