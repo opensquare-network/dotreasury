ad6eb5d2040283ff75d8f35c01d67e194387cdb5
2nthony <ant.hu@outlook.com>
2024-11-13 10:10:55 +0800

    rename file to assets

--- site/src/pages/Overview/polkadot/assets/index.jsx
+++ site/src/pages/Overview/polkadot/assets/index.jsx
@@ -1,12 +1,12 @@
 // site/src/pages/Overview/AssetHub.jsx
 
 import styled from "styled-components";
-import Card from "../../../components/Card";
+import Card from "../../../../components/Card";
 
 const Wrapper = styled(Card)`
   padding: 24px;
 `;
 
-export default function OverviewAssethub() {
+export default function OverviewAssets() {
   return <Wrapper>assethub</Wrapper>;
 }
--- site/src/pages/Overview/polkadot/index.jsx
+++ site/src/pages/Overview/polkadot/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import OverviewAssethub from "./assethub";
+import OverviewAssets from "./assets";
 import OverviewTotalTreasury from "./totalTreasury";
 import { space_y } from "../../../styles/tailwindcss";
 
@@ -12,7 +12,7 @@ export default function OverviewPolkadot() {
     <Wrapper>
       <OverviewTotalTreasury />
 
-      <OverviewAssethub />
+      <OverviewAssets />
     </Wrapper>
   );
 }
