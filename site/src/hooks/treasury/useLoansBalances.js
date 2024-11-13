6136237805406a8c41ae7a5263d93a0b7f9dc39d
leocs2417 <leochen.sat24@gmail.com>
2024-11-13 15:10:07 +0800

    Add useLoansBalances, #1410

---
+++ site/src/hooks/treasury/useLoansBalances.js
@@ -0,0 +1,20 @@
+export function useLoansCentrifugeUsdcBalance() {
+  return {
+    balance: 3000000000000,
+    isLoading: false,
+  };
+}
+
+export function useLoansBifrostDotBalance() {
+  return {
+    balance: 5000000000000000,
+    isLoading: false,
+  };
+}
+
+export function useLoansPendulumDotBalance() {
+  return {
+    balance: 500000000000000,
+    isLoading: false,
+  };
+}
