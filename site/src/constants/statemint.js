a141a8f0778c030fc93a8b5d771ae2e0d7ca3894
leocs2417 <leochen.sat24@gmail.com>
2024-11-13 14:30:29 +0800

    common constants & utils, #1407

--- site/src/constants/statemint.js
+++ site/src/constants/statemint.js
@@ -3,3 +3,16 @@ export const STATEMINT_FELLOWSHIP_SALARY_ACCOUNT =
 
 export const STATEMINT_FELLOWSHIP_TREASURY_ACCOUNT =
   "16VcQSRcMFy6ZHVjBvosKmo7FKqTb8ZATChDYo8ibutzLnos";
+
+export const STATEMINT_ASSETS = [
+  {
+    id: 1984,
+    symbol: "USDt",
+    decimals: 6,
+  },
+  {
+    id: 1337,
+    symbol: "USDC",
+    decimals: 6,
+  },
+];
--- site/src/hooks/treasury/useTreasuryFree.js
+++ site/src/hooks/treasury/useTreasuryFree.js
@@ -1,22 +1,7 @@
 import { isPolkadot } from "../../utils/chains";
 import { useAssetHubAsset } from "../assetHub/useAssetHubAsset";
 import { STATEMINT_FELLOWSHIP_SALARY_ACCOUNT } from "../../constants/statemint";
-
-export const StatemintAssets = [
-  {
-    id: 1984,
-    symbol: "USDt",
-    decimals: 6,
-  },
-  {
-    id: 1337,
-    symbol: "USDC",
-    decimals: 6,
-  },
-];
-
-export const getAssetBySymbol = (symbol) =>
-  StatemintAssets.find((asset) => asset.symbol === symbol);
+import { getAssetBySymbol } from "../../utils";
 
 export default function useQueryFellowshipSalaryBalance(symbol) {
   let salaryAccount = null;
