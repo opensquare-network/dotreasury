c2c0edb9e3726fe4c8c2fc0231caf09037f2f300
chaojun <chaojun@opensquare.network>
2024-11-14 09:25:44 +0800

    Remove unused code

--- packages/price/src/mongo/index.js
+++ packages/price/src/mongo/index.js
@@ -12,7 +12,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017";
 let ksmUsdtCol = null;
 let dotUsdtCol = null;
 let cfgUsdtCol = null;
-let mythUsdtCol = null;
 
 async function initDb() {
   client = await MongoClient.connect(mongoUrl, {
@@ -23,7 +22,6 @@ async function initDb() {
   ksmUsdtCol = db.collection(ksmUsdtCollectionName);
   dotUsdtCol = db.collection(dotUsdtCollectionName);
   cfgUsdtCol = db.collection("cfgUsdt");
-  mythUsdtCol = db.collection("mythUsdt");
 
   await _createIndexes();
 }
@@ -37,7 +35,6 @@ async function _createIndexes() {
   ksmUsdtCol.createIndex({ openTime: 1 });
   dotUsdtCol.createIndex({ openTime: 1 });
   cfgUsdtCol.createIndex({ openTime: 1 });
-  mythUsdtCol.createIndex({ openTime: 1 });
 }
 
 async function tryInit(col) {
@@ -61,15 +58,9 @@ async function getCfgUsdtCol() {
   return cfgUsdtCol;
 }
 
-async function getMythUsdtCol() {
-  await tryInit(mythUsdtCol);
-  return mythUsdtCol;
-}
-
 module.exports = {
   initDb,
   getKsmUsdtCollection,
   getDotUsdtCollection,
   getCfgUsdtCol,
-  getMythUsdtCol,
 };
