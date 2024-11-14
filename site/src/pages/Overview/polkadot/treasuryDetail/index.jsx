afde095a9e538f3939992f32e2c4b93a454082c4
2nthony <ant.hu@outlook.com>
2024-11-14 10:43:58 +0800

    treasury detail mobile

--- site/src/pages/Overview/polkadot/treasuryDetail/index.jsx
+++ site/src/pages/Overview/polkadot/treasuryDetail/index.jsx
@@ -6,12 +6,15 @@ import TreasuryDetailHydration from "./hydration";
 import { grid, grid_cols } from "../../../../styles/tailwindcss";
 import TreasuryDetailBounties from "./bounties";
 import TreasuryDetailFellowship from "./fellowship";
+import { mdcss, smcss } from "../../../../styles/responsive";
 
 const Wrapper = styled(Card)`
   padding: 24px;
   ${grid}
   ${grid_cols(3)}
   gap: 48px;
+  ${mdcss(grid_cols(2))}
+  ${smcss(grid_cols(1))}
 `;
 
 export default function OverviewTreasuryDetail() {
