ce8ab4ba866dc68c525807138671391dbe0e1462
leocs2417 <leochen.sat24@gmail.com>
2024-11-15 16:12:18 +0800

    re-init polkadot Referenda, #1399

---
+++ site/src/pages/Referenda/polkadot/index.jsx
@@ -0,0 +1,11 @@
+import ReferendaTable from "./table";
+import Summary from "./summary";
+
+export default function Referenda() {
+  return (
+    <div>
+      <Summary />
+      <ReferendaTable />
+    </div>
+  );
+}
---
+++ site/src/pages/Referenda/polkadot/table/index.jsx
@@ -0,0 +1,3 @@
+export default function ReferendaTable() {
+  return <div>ReferendaTable</div>;
+}
