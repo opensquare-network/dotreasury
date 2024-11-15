4e5764a922d41f43e9bd6a4013c048eeba330c44
2nthony <ant.hu@outlook.com>
2024-11-15 18:30:50 +0800

    submenu

---
+++ site/src/pages/Header/SpendsMenu.jsx
@@ -0,0 +1,9 @@
+import { Label, Menu } from "semantic-ui-react";
+
+export default function SpendsMenu() {
+  return (
+    <Menu.Item key="Proposals">
+      Spends<Label>{0}</Label>
+    </Menu.Item>
+  );
+}
