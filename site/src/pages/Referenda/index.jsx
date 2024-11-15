aeb413d809758634fbdd03bc709b656f8f3ea689
leocs2417 <leochen.sat24@gmail.com>
2024-11-15 16:12:18 +0800

    re-init polkadot Referenda, #1399

--- site/src/pages/Referenda/index.jsx
+++ site/src/pages/Referenda/index.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 import ReferendaTable from "./ReferendaTable";
 import Summary from "./Summary";
+import { isPolkadot } from "../../utils/chains";
+import ReferendaPolkadot from "./polkadot";
 
 const Wrapper = styled.div``;
 
 export default function Referenda() {
+  if (isPolkadot) {
+    return <ReferendaPolkadot />
+  }
   return (
     <Wrapper>
       <Summary />
