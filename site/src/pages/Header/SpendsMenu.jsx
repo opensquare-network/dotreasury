254031c98ca292e09a9f11c6694d2c13bab0d6ed
2nthony <ant.hu@outlook.com>
2024-11-18 17:09:19 +0800

    spends count

--- site/src/pages/Header/SpendsMenu.jsx
+++ site/src/pages/Header/SpendsMenu.jsx
@@ -1,9 +1,13 @@
 import { Label, Menu } from "semantic-ui-react";
+import { totalSpendsCountSelector } from "../../store/reducers/overviewSlice";
+import { useSelector } from "react-redux";
 
 export default function SpendsMenu() {
+  const treasurySpendsCount = useSelector(totalSpendsCountSelector);
+
   return (
     <Menu.Item key="Proposals">
-      Spends<Label>{0}</Label>
+      Spends<Label>{treasurySpendsCount}</Label>
     </Menu.Item>
   );
 }
