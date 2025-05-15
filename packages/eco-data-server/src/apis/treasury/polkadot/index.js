60ba13f9ba24db72e4db0ee48ba98b4948fe54cd
Chaojun Huang <chaojun@opensquare.network>
2025-05-15 15:11:07 +0800

    Calc aDOT asset value on the hydration acquistion address, #1511 (#1513)
    * Calc aDOT asset value on the hydration acquistion address, #1511
    
    * Rename, #1511

--- packages/eco-data-server/src/apis/treasury/polkadot/index.js
+++ packages/eco-data-server/src/apis/treasury/polkadot/index.js
@@ -46,37 +46,53 @@ async function getPolkadotTreasuryData() {
     hydrationTreasuryAccount1,
     hydrationTreasuryAccount2,
     hydrationTreasuryAccount3,
+    hydrationTreasuryAccount4,
   } = await getTreasuryOnHydration();
 
   const {
     accountUsdt: hydrationAccount1Usdt,
     accountUsdc: hydrationAccount1Usdc,
     accountDot: hydrationAccount1Dot,
+    accountADot: hydrationAccount1ADot,
   } = hydrationTreasuryAccount1;
 
   const {
     accountUsdt: hydrationAccount2Usdt,
     accountUsdc: hydrationAccount2Usdc,
     accountDot: hydrationAccount2Dot,
+    accountADot: hydrationAccount2ADot,
   } = hydrationTreasuryAccount2;
 
   const {
     accountUsdt: hydrationAccount3Usdt,
     accountUsdc: hydrationAccount3Usdc,
     accountDot: hydrationAccount3Dot,
+    accountADot: hydrationAccount3ADot,
   } = hydrationTreasuryAccount3;
+
+  const {
+    accountUsdt: hydrationAccount4Usdt,
+    accountUsdc: hydrationAccount4Usdc,
+    accountDot: hydrationAccount4Dot,
+    accountADot: hydrationAccount4ADot,
+  } = hydrationTreasuryAccount4;
+
+  const getTotal = (balance) =>
+    new BigNumber(balance?.free || 0).plus(balance?.reserved || 0);
 
   const dot = bountyTreasuryOnRelay
     .reduce((acc, item) => acc.plus(item.data.free || 0), new BigNumber(0))
     .plus(treasuryDotOnRelay?.data.free || 0)
     .plus(fellowshipTreasuryDotOnAssetHub?.data.free || 0)
     .plus(dotTreasuryBalanceOnAssetHub?.data.free || 0)
-    .plus(hydrationAccount1Dot?.free || 0)
-    .plus(hydrationAccount1Dot?.reserved || 0)
-    .plus(hydrationAccount2Dot?.free || 0)
-    .plus(hydrationAccount2Dot?.reserved || 0)
-    .plus(hydrationAccount3Dot?.free || 0)
-    .plus(hydrationAccount3Dot?.reserved || 0)
+    .plus(getTotal(hydrationAccount1Dot))
+    .plus(getTotal(hydrationAccount1ADot))
+    .plus(getTotal(hydrationAccount2Dot))
+    .plus(getTotal(hydrationAccount2ADot))
+    .plus(getTotal(hydrationAccount3Dot))
+    .plus(getTotal(hydrationAccount3ADot))
+    .plus(getTotal(hydrationAccount4Dot))
+    .plus(getTotal(hydrationAccount4ADot))
     .plus(loansBifrostDotBalance || 0)
     .plus(loansPendulumDotBalance || 0)
     .plus(loansHydrationDotBalance || 0)
@@ -85,21 +101,17 @@ async function getPolkadotTreasuryData() {
   const usdt = new BigNumber(fellowshipSalaryUsdtBalance?.balance || 0)
     .plus(usdtTreasuryBalanceOnAssetHub?.balance || 0)
     .plus(ambassadorTreasuryUsdtBalance?.balance || 0)
-    .plus(hydrationAccount1Usdt?.free || 0)
-    .plus(hydrationAccount1Usdt?.reserved || 0)
-    .plus(hydrationAccount2Usdt?.free || 0)
-    .plus(hydrationAccount2Usdt?.reserved || 0)
-    .plus(hydrationAccount3Usdt?.free || 0)
-    .plus(hydrationAccount3Usdt?.reserved || 0)
+    .plus(getTotal(hydrationAccount1Usdt))
+    .plus(getTotal(hydrationAccount2Usdt))
+    .plus(getTotal(hydrationAccount3Usdt))
+    .plus(getTotal(hydrationAccount4Usdt))
     .toFixed();
 
   const usdc = new BigNumber(usdcTreasuryBalanceOnAssetHub?.balance || 0)
-    .plus(hydrationAccount1Usdc?.free || 0)
-    .plus(hydrationAccount1Usdc?.reserved || 0)
-    .plus(hydrationAccount2Usdc?.free || 0)
-    .plus(hydrationAccount2Usdc?.reserved || 0)
-    .plus(hydrationAccount3Usdc?.free || 0)
-    .plus(hydrationAccount3Usdc?.reserved || 0)
+    .plus(getTotal(hydrationAccount1Usdc))
+    .plus(getTotal(hydrationAccount2Usdc))
+    .plus(getTotal(hydrationAccount3Usdc))
+    .plus(getTotal(hydrationAccount4Usdc))
     .plus(loansCentrifugeUsdcBalance || 0)
     .toFixed();
 
--- packages/eco-data-server/src/apis/treasury/polkadot/treasuryOnHydration.js
+++ packages/eco-data-server/src/apis/treasury/polkadot/treasuryOnHydration.js
@@ -1,6 +1,7 @@
 const { multiApiQuery } = require("./common");
 
 const DotTokenId = 5;
+const ADotTokenId = 1001;
 const UsdtTokenIdFromAssetHub = 10;
 const UsdcTokenIdFromAssetHub = 22;
 
@@ -13,22 +14,37 @@ const PolkadotTreasuryOnHydrationAccount2 =
 const PolkadotTreasuryOnHydrationAccount3 =
   "7KATdGaecnKi4zDAMWQxpB2s59N2RE1JgLuugCjTsRZHgP24";
 
+const PolkadotTreasuryOnHydrationAccount4 =
+  "7N4oFqXKgeTXo6CMSY9BVZdHP5J3RhQXY77Fe7qmQwjcxa1w";
+
 async function getHydrationTreasuryBalance(api, treasuryAccount, tokenId) {
   const account = await api.query.tokens.accounts(treasuryAccount, tokenId);
   return account.toJSON();
 }
 
 async function getHydrationTreasuryBalances(api, treasuryAccount) {
-  const [accountUsdt, accountUsdc, accountDot] = await Promise.all([
-    getHydrationTreasuryBalance(api, treasuryAccount, UsdtTokenIdFromAssetHub),
-    getHydrationTreasuryBalance(api, treasuryAccount, UsdcTokenIdFromAssetHub),
-    getHydrationTreasuryBalance(api, treasuryAccount, DotTokenId),
-  ]);
+  const [accountUsdt, accountUsdc, accountDot, accountADot] = await Promise.all(
+    [
+      getHydrationTreasuryBalance(
+        api,
+        treasuryAccount,
+        UsdtTokenIdFromAssetHub,
+      ),
+      getHydrationTreasuryBalance(
+        api,
+        treasuryAccount,
+        UsdcTokenIdFromAssetHub,
+      ),
+      getHydrationTreasuryBalance(api, treasuryAccount, DotTokenId),
+      getHydrationTreasuryBalance(api, treasuryAccount, ADotTokenId),
+    ],
+  );
 
   return {
     accountUsdt,
     accountUsdc,
     accountDot,
+    accountADot,
   };
 }
 
@@ -42,11 +58,15 @@ async function getTreasuryOnHydration() {
   const hydrationTreasuryAccount3 = await multiApiQuery("hydradx", (api) =>
     getHydrationTreasuryBalances(api, PolkadotTreasuryOnHydrationAccount3),
   );
+  const hydrationTreasuryAccount4 = await multiApiQuery("hydradx", (api) =>
+    getHydrationTreasuryBalances(api, PolkadotTreasuryOnHydrationAccount4),
+  );
 
   return {
     hydrationTreasuryAccount1,
     hydrationTreasuryAccount2,
     hydrationTreasuryAccount3,
+    hydrationTreasuryAccount4,
   };
 }
 
