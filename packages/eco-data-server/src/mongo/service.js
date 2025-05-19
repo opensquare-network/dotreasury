75cf129e1c890064d6b99f7fbf9bf573d03cb800
chaojun <chaojun@opensquare.network>
2025-05-19 13:48:32 +0800

    Get token price from db, #1514

--- packages/eco-data-server/src/mongo/service.js
+++ packages/eco-data-server/src/mongo/service.js
@@ -123,4 +123,5 @@ module.exports = {
   upsertTokenPrice,
   batchUpdateTokenPrices,
   normalizeBalancesItem,
+  getTotalFiatValue,
 };
--- packages/eco-data-server/src/scripts/treasuryHistory.js
+++ packages/eco-data-server/src/scripts/treasuryHistory.js
@@ -1,8 +1,11 @@
-const { multiApiQuery } = require("../apis/treasury/polkadot/common");
+require("dotenv").config();
 
 const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
+dayjs.extend(utc);
+
 const { createChainApis } = require("../apis");
+const { multiApiQuery } = require("../apis/treasury/polkadot/common");
 const {
   getTreasuryOnHydrationFromApi,
 } = require("../apis/treasury/polkadot/treasuryOnHydration");
@@ -26,7 +29,12 @@ const {
   getMythTreasuryOnMythosFromApi,
 } = require("../apis/treasury/polkadot/treasuryOnMythos");
 const { calcTotalBalance } = require("../apis/treasury/polkadot");
-dayjs.extend(utc);
+const { getTotalFiatValue, getTreasuryHistoryCol } = require("../mongo");
+const { getTreasuryBalancesArray } = require("../apis/treasury");
+const {
+  getDotUsdtCollection,
+  getMythUsdtCol,
+} = require("@dotreasury/price/src/mongo");
 
 const oneDay = 24 * 60 * 60 * 1000;
 
@@ -121,10 +129,73 @@ async function getTreasuryBalance(daysAgo) {
   };
 }
 
+async function normalizeBalancesItem(balance, timestamp) {
+  if (["USDt", "USDC"].includes(balance.token)) {
+    return {
+      ...balance,
+      price: 1,
+    };
+  }
+
+  if (balance.token === "DOT") {
+    const priceCol = await getDotUsdtCollection();
+    const [tokenPrice] = await priceCol
+      .find({ openTime: { $lte: timestamp } })
+      .sort({ openTime: -1 })
+      .limit(1)
+      .toArray();
+    return {
+      ...balance,
+      price: parseFloat(tokenPrice?.open),
+      priceUpdateAt: tokenPrice?.openTime,
+    };
+  }
+
+  if (balance.token === "MYTH") {
+    const priceCol = await getMythUsdtCol();
+    const [tokenPrice] = await priceCol
+      .find({ openTime: { $lte: timestamp } })
+      .sort({ openTime: -1 })
+      .limit(1)
+      .toArray();
+    return {
+      ...balance,
+      price: tokenPrice?.open,
+      priceUpdateAt: tokenPrice?.openTime,
+    };
+  }
+
+  return balance;
+}
+
 async function generateTreasuryItemAtDaysAgo(daysAgo) {
-  const balances = await getTreasuryBalance(daysAgo);
-  const totalBalances = calcTotalBalance(balances);
-  console.log(daysAgo, totalBalances);
+  const onchainTreasuries = await getTreasuryBalance(daysAgo);
+  const treasuryData = calcTotalBalance(onchainTreasuries);
+  const balances = getTreasuryBalancesArray(treasuryData);
+
+  const timestamp = dayjs().utc().subtract(daysAgo, "day").valueOf();
+  const normalizedBalances = await Promise.all(
+    balances.map((item) => normalizeBalancesItem(item, timestamp)),
+  );
+  if (!normalizedBalances.every((item) => item.price)) {
+    return;
+  }
+
+  const totalValue = getTotalFiatValue(normalizedBalances);
+  const date = dayjs().utc().subtract(daysAgo, "day").startOf("day").valueOf();
+
+  const treasuryHistoryCol = await getTreasuryHistoryCol();
+  await treasuryHistoryCol.updateOne(
+    { chain: "polkadot", date },
+    {
+      $set: {
+        balance: totalValue,
+        balances: normalizedBalances,
+        balanceUpdateAt: new Date(),
+      },
+    },
+    { upsert: true },
+  );
 }
 
 async function generateTreasuryHistory() {
@@ -133,7 +204,6 @@ async function generateTreasuryHistory() {
   }
 }
 
-createChainApis().then(async () => {
-  console.log("createChainApis");
-  await generateTreasuryHistory();
-});
+createChainApis()
+  .then(generateTreasuryHistory)
+  .finally(() => process.exit(0));
