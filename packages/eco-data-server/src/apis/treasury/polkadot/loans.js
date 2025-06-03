054cf4119bd930b7fad81d70140fd3248312e7ff
chaojun <chaojun@opensquare.network>
2025-06-03 11:00:36 +0800

    Update bifrost loan, #1514

--- packages/eco-data-server/src/apis/treasury/polkadot/loans.js
+++ packages/eco-data-server/src/apis/treasury/polkadot/loans.js
@@ -1,12 +1,13 @@
 const loansCentrifugeUsdcBalance = 1500000000000;
-const loansBifrostDotBalance = 0;
+const loansBifrostDotBalance = 100000000000000000;
 const loansPendulumDotBalance = 500000000000000;
 const loansHydrationDotBalance = 10000000000000000;
 
 function getLoansBifrostDotBalanceByTimestamp(timestamp) {
-  // if (timestamp > ?) {
-  //   return 100000000000000000;
-  // }
+  // https://polkadot.subsquare.io/treasury/proposals/1020?tab=timeline
+  if (timestamp > 1748817708000) {
+    return 100000000000000000;
+  }
 
   // https://bifrost.subsquare.io/referenda/110?tab=timeline
   if (timestamp > 1742984040000) {
