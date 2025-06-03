23a622050a879e6ef3f26bda101a181da1143fa3
chaojun <chaojun@opensquare.network>
2025-06-03 11:04:49 +0800

    Update, #1514

--- packages/eco-data-server/src/apis/treasury/polkadot/loans.js
+++ packages/eco-data-server/src/apis/treasury/polkadot/loans.js
@@ -1,12 +1,12 @@
 const loansCentrifugeUsdcBalance = 1500000000000;
-const loansBifrostDotBalance = 100000000000000000;
+const loansBifrostDotBalance = 10000000000000000;
 const loansPendulumDotBalance = 500000000000000;
 const loansHydrationDotBalance = 10000000000000000;
 
 function getLoansBifrostDotBalanceByTimestamp(timestamp) {
   // https://polkadot.subsquare.io/treasury/proposals/1020?tab=timeline
   if (timestamp > 1748817708000) {
-    return 100000000000000000;
+    return loansBifrostDotBalance;
   }
 
   // https://bifrost.subsquare.io/referenda/110?tab=timeline
