6e4c56eb8cd1330707ca3d74ca425aaadb36bd00
leocs2417 <leochen.sat24@gmail.com>
2025-06-09 18:10:01 +0800

    add totalBountyCountSelector by subsquareApi, #1536

--- site/src/pages/Header/BountiesMenu.jsx
+++ site/src/pages/Header/BountiesMenu.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Label, Menu } from "semantic-ui-react";
-import { useSelector } from "react-redux";
-import { totalBountyCountSelector } from "../../store/reducers/overviewSlice";
+import { useSelector, useDispatch } from "react-redux";
 import IconMask from "../../components/Icon/Mask";
 import { chainSelector } from "../../store/reducers/chainSlice";
 import ExternalLink from "../../components/ExternalLink";
 import { isKusama, isPolkadot } from "../../utils/chains";
+import {
+  fetchBountiesTotal,
+  totalBountyCountSelector,
+} from "../../store/reducers/bountySlice";
 
 function BountiesMenuExternalLink() {
   const bountiesCount = useSelector(totalBountyCountSelector);
   const chain = useSelector(chainSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (bountiesCount) {
+      return;
+    }
+
+    dispatch(fetchBountiesTotal());
+  }, [dispatch, bountiesCount]);
 
   return (
     <Menu.Item key="Bounties">
