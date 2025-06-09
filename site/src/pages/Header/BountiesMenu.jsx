c2c0444a2d9c85206a8acc89a0da90dc06de221e
leocs2417 <leochen.sat24@gmail.com>
2025-06-09 17:27:48 +0800

    init BountiesMenuExternalLink, #1536

--- site/src/pages/Header/BountiesMenu.jsx
+++ site/src/pages/Header/BountiesMenu.jsx
@@ -2,6 +2,33 @@ import React from "react";
 import { Label, Menu } from "semantic-ui-react";
 import { useSelector } from "react-redux";
 import { totalBountyCountSelector } from "../../store/reducers/overviewSlice";
+import IconMask from "../../components/Icon/Mask";
+import { chainSelector } from "../../store/reducers/chainSlice";
+import ExternalLink from "../../components/ExternalLink";
+import { isKusama, isPolkadot } from "../../utils/chains";
+
+function BountiesMenuExternalLink() {
+  const bountiesCount = useSelector(totalBountyCountSelector);
+  const chain = useSelector(chainSelector);
+
+  return (
+    <Menu.Item key="Bounties">
+      <ExternalLink
+        href={`https://${chain}.subsquare.io/treasury/bounties`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Bounties
+        <IconMask
+          src="/imgs/caret-up-right.svg"
+          color="textSecondary"
+          size={16}
+        />
+        <Label>{bountiesCount}</Label>
+      </ExternalLink>
+    </Menu.Item>
+  );
+}
 
 function BountiesMenu() {
   const bountiesCount = useSelector(totalBountyCountSelector);
@@ -13,4 +40,10 @@ function BountiesMenu() {
   );
 }
 
-export default BountiesMenu;
+export default function BountiesEntry() {
+  if (isPolkadot || isKusama) {
+    return <BountiesMenuExternalLink />;
+  }
+
+  return <BountiesMenu />;
+}
