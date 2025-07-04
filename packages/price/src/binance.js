7dc3ecaf2c83239cb7d9081d4b673f74b5391377
chaojun <chaojun@opensquare.network>
2025-07-04 16:38:38 +0800

    Check price data availability

--- packages/price/src/binance.js
+++ packages/price/src/binance.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const AbortController = require("abort-controller");
 
 async function getKlines(symbol, startTime = 0) {
-  const url = new URL("api/v3/klines", "https://api.binance.com");
+  const url = new URL("api/v3/klines", "https://data-api.binance.vision");
   url.searchParams.set("symbol", `${symbol}USDT`);
   url.searchParams.set("interval", "1m");
   url.searchParams.set("limit", "500");
@@ -15,8 +15,10 @@ async function getKlines(symbol, startTime = 0) {
 
   try {
     const res = await fetch(url, { signal: controller.signal });
-    const json = await res.json();
-    return json;
+    if (res.ok) {
+      return await res.json();
+    }
+    console.error(`Error fetching klines: ${res.status} ${res.statusText}`);
   } catch (error) {
     if (error.name === "AbortError") {
       console.log("request was aborted");
---
+++ packages/price/src/scripts/checkPriceData.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+
+const minimist = require("minimist");
+const dayjs = require("dayjs");
+const {
+  getDotUsdtCollection,
+  getKsmUsdtCollection,
+  getCfgUsdtCol,
+  getMythUsdtCol,
+} = require("../mongo");
+
+const args = minimist(process.argv.slice(2));
+
+if (!args.symbol) {
+  console.log("Must specify symbol with argument --symbol=[DOT|KSM|CFG|MYTH]");
+  return;
+}
+
+async function getPriceCol() {
+  if (args.symbol === "DOT") {
+    return await getDotUsdtCollection();
+  } else if (args.symbol === "KSM") {
+    return await getKsmUsdtCollection();
+  } else if (args.symbol === "CFG") {
+    return await getCfgUsdtCol();
+  } else if (args.symbol === "MYTH") {
+    return await getMythUsdtCol();
+  } else {
+    throw new Error("Unsupported symbol: " + args.symbol);
+  }
+}
+
+async function main() {
+  const priceCol = await getPriceCol();
+
+  // Get earliest openTime from priceCol
+  const earliestDoc = await priceCol.findOne({}, { sort: { openTime: 1 } });
+  if (!earliestDoc) {
+    console.log(`No data found for symbol: ${args.symbol}`);
+    return;
+  }
+
+  const earliestOpenTime = earliestDoc.openTime;
+
+  let numOfDays = 1;
+  while (true) {
+    const theDay = dayjs().subtract(numOfDays, "day");
+    const timestampStart = theDay.startOf("day").valueOf();
+    if (timestampStart < earliestOpenTime) {
+      console.log(
+        `Finish checking price data. Reached earliest data for ${
+          args.symbol
+        } on ${dayjs(earliestOpenTime).format("YYYY-MM-DD")}`,
+      );
+      break;
+    }
+
+    const timestampEnd = theDay.endOf("day").valueOf();
+    const exists = await priceCol.findOne({
+      openTime: { $gte: timestampStart, $lte: timestampEnd },
+    });
+    if (!exists) {
+      console.log(
+        `No ${args.symbol} data found at ${theDay.format("YYYY-MM-DD")}`,
+      );
+    }
+
+    numOfDays++;
+  }
+}
+
+main()
+  .catch(console.error)
+  .finally(() => {
+    process.exit(0);
+  });
