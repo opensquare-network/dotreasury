fa62bf470f2365ecdccaf9bd27c92f333bb25bd7
leocs2417 <leochen.sat24@gmail.com>
2025-11-07 18:35:18 +0800

    Link the burnt page to SubSquare

--- site/src/pages/Header/BurntMenu.jsx
+++ site/src/pages/Header/BurntMenu.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { Menu, Label } from "semantic-ui-react";
+import { Label, Menu } from "semantic-ui-react";
 import { useSelector } from "react-redux";
+import IconMask from "../../components/Icon/Mask";
+import { chainSelector } from "../../store/reducers/chainSlice";
+import ExternalLink from "../../components/ExternalLink";
+import { isKusama, isPolkadot } from "../../utils/chains";
 import { totalBurntCountSelector } from "../../store/reducers/overviewSlice";
 
-function BurntMenu() {
+function BurntMenuExternalLink() {
   const burntListCount = useSelector(totalBurntCountSelector);
+  const chain = useSelector(chainSelector);
 
   return (
     <Menu.Item key="Burnt">
-      Burnt<Label>{burntListCount}</Label>
+      <ExternalLink href={`https://${chain}.subsquare.io/treasury/burnt`}>
+        Burnt
+        <IconMask
+          src="/imgs/caret-up-right.svg"
+          color="textSecondary"
+          size={16}
+        />
+        <Label>{burntListCount}</Label>
+      </ExternalLink>
     </Menu.Item>
   );
 }
 
-export default BurntMenu;
+export default function BurntEntry() {
+  if (isPolkadot || isKusama) {
+    return <BurntMenuExternalLink />;
+  }
+
+  return null;
+}
