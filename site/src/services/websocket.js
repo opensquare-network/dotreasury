e0475b3fa9edc985580061c47390262ecf9cf077
leocs2417 <leochen.sat24@gmail.com>
2026-01-08 18:23:07 +0800

    socket

--- site/src/services/websocket.js
+++ site/src/services/websocket.js
@@ -1,35 +1,35 @@
-// import io from "socket.io-client";
-// import store from "../store";
-// import { setScanHeight } from "../store/reducers/chainSlice";
-// import { setOverview } from "../store/reducers/overviewSlice";
-// import { currentChainSettings } from "../utils/chains";
+import io from "socket.io-client";
+import store from "../store";
+import { setScanHeight } from "../store/reducers/chainSlice";
+import { setOverview } from "../store/reducers/overviewSlice";
+import { currentChainSettings } from "../utils/chains";
 
-// const chainStatusRoom = "CHAIN_STATUS_ROOM";
-// const overviewRoom = "OVERVIEW_V2_ROOM";
+const chainStatusRoom = "CHAIN_STATUS_ROOM";
+const overviewRoom = "OVERVIEW_V2_ROOM";
 
-// let socket = null;
+let socket = null;
 
-// export function connect() {
-//   if (socket) {
-//     socket.emit("unsubscribe", chainStatusRoom);
-//     socket.emit("unsubscribe", overviewRoom);
-//     socket.disconnect();
-//   }
+export function connect() {
+  if (socket) {
+    socket.emit("unsubscribe", chainStatusRoom);
+    socket.emit("unsubscribe", overviewRoom);
+    socket.disconnect();
+  }
 
-//   const { socketIOUrl } = currentChainSettings?.api || {};
-//   socket = io(socketIOUrl || "api.dotreasury.com");
-//   socket.connect();
+  const { socketIOUrl } = currentChainSettings?.api || {};
+  socket = io(socketIOUrl || "api.dotreasury.com");
+  socket.connect();
 
-//   socket.on("connect", () => {
-//     socket.emit("subscribe", chainStatusRoom);
-//     socket.emit("subscribe", overviewRoom);
+  socket.on("connect", () => {
+    socket.emit("subscribe", chainStatusRoom);
+    socket.emit("subscribe", overviewRoom);
 
-//     socket.on("scanStatus", ({ height }) => {
-//       store.dispatch(setScanHeight(height));
-//     });
+    socket.on("scanStatus", ({ height }) => {
+      store.dispatch(setScanHeight(height));
+    });
 
-//     socket.on("overview_v2", (overview) => {
-//       store.dispatch(setOverview(overview));
-//     });
-//   });
-// }
+    socket.on("overview_v2", (overview) => {
+      store.dispatch(setOverview(overview));
+    });
+  });
+}
