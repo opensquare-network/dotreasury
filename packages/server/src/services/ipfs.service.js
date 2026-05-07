21db128f191ad65e73465ee57b19ea13e927f604
chaojun <chaojun@opensquare.network>
2026-05-07 16:42:48 +0800

    fix: Arbitrary code execution in protobufjs

--- packages/server/src/services/ipfs.service.js
+++ packages/server/src/services/ipfs.service.js
@@ -1,5 +1,3 @@
-const Hash = require("ipfs-only-hash");
-const { CID } = require("multiformats/cid");
 const { getClients, ClientOptions, ClientMode } = require("@osn/ipfs");
 const {
   INFURA_PROJECT_ID,
@@ -17,8 +15,8 @@ async function initIpfsClient() {
       new ClientOptions(
         INFURA_PROJECT_ID,
         INFURA_PROJECT_SECRET,
-        LOCAL_IPFS_NODE_URL
-      )
+        LOCAL_IPFS_NODE_URL,
+      ),
     );
   }
 }
@@ -37,15 +35,24 @@ async function ipfsAddBuffer(data) {
 
 async function cidOf(obj) {
   const data = JSON.stringify(obj);
-  const cidV0 = await Hash.of(Buffer.from(data));
-  const cid = CID.parse(cidV0);
+  const importer = await import("ipfs-unixfs-importer");
+  const { cid } = await importer.importBytes(
+    Buffer.from(data),
+    { put: async () => {} },
+    {
+      cidVersion: 0,
+      rawLeaves: false,
+    },
+  );
   return cid.toV1().toString();
 }
 
 async function pinJsonToIpfsWithTimeout(data, timeout) {
   const errorMsg = "Pin json to ipfs timeout";
   return await Promise.race([
-    new Promise((_, reject) => setTimeout(() => reject(new Error(errorMsg)), timeout)),
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error(errorMsg)), timeout),
+    ),
     ipfsAdd(data),
   ]);
 }
